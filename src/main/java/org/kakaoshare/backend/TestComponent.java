74ab736e2969ae31cdedadf74d548a08d304277f
YeaChan05 <qkenrdl05@gmail.com>
2024-02-02 20:16:54 +0900

    [test]test ci (#${closes})
    github action ci 테스트를 위한 코드 작성

---
+++ src/main/java/org/kakaoshare/backend/TestComponent.java
@@ -0,0 +1,10 @@
+package org.kakaoshare.backend;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestComponent {
+    public Long testMethod(){
+        return 1L;
+    }
+}
