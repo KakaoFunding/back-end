0d46394dff08e4adf969d8bdfc9f3fef475fbf2c
YeaChan05 <qkenrdl05@gmail.com>
2024-02-02 20:56:06 +0900

    Revert "Revert "[test]test ci (#${closes})""
    This reverts commit aa8bdcafa5e02489ea14703509dd86a26070b11e.

--- src/main/java/org/kakaoshare/backend/BackEndApplication.java
+++ src/main/java/org/kakaoshare/backend/BackEndApplication.java
@@ -1,10 +1,13 @@
 package org.kakaoshare.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackEndApplication {
+	@Autowired
+	private TestComponent testComponent;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndApplication.class, args);
