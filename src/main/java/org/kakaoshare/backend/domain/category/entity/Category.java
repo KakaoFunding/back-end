c8f36061b0432d614f92ebba4517ee60f2cdbce8
YeaChan05 <qkenrdl05@gmail.com>
2024-02-14 22:02:12 +0900

    [refactor] 컬럼명 명시 및 생성자 리팩토링 (#12)
    1. 자기 참조 컬럼명을 명시적으로 부여
    2. 정적 팩토리 메서드를 명시적으로 구현
    3. `toString()` 구현

--- src/main/java/org/kakaoshare/backend/domain/category/entity/Category.java
+++ src/main/java/org/kakaoshare/backend/domain/category/entity/Category.java
@@ -1,6 +1,16 @@
 package org.kakaoshare.backend.domain.category.entity;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,21 +23,44 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(staticName = "of")
+@AllArgsConstructor
 public class Category extends BaseTimeEntity {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
     private Long categoryId;
-
+    
     @Column(nullable = false)
     private String name;
-
+    
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent_id")
+    @JoinColumn(name = "parent_id", referencedColumnName = "category_id")
     private Category parent;
-
-    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY)
+    
+    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Category> children;
-
+    
+    public Category(String name, Category parent, List<Category> children) {
+        this.name = name;
+        this.parent = parent;
+        this.children = children;
+    }
+    
+    public static Category of(String name, Category parent, List<Category> children) {
+        return new Category(name, parent, children);
+    }
+    
+    @Override
+    public String toString() {
+        return "Category{" + '\n' +
+                "categoryId=" + categoryId + '\n' +
+                ", name='" + name + '\n' +
+                ", parent id=" + parent.getCategoryId() + '\n' +
+                ", children id=" +
+                children.stream()
+                        .map(Category::getCategoryId)
+                        .toList() +
+                '}';
+    }
 }
