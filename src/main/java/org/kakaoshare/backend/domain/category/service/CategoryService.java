ef68753124f819727f98de388a39f7efc939b8ef
YeaChan05 <qkenrdl05@gmail.com>
2024-02-14 22:10:39 +0900

    [fix]level 관련 로직 제거 및 root 연결 불량 해결 (#12)
    level 관련 로직 제거 및 root에 parent category가 연결되지 않는 문제 해결

--- src/main/java/org/kakaoshare/backend/domain/category/service/CategoryService.java
+++ src/main/java/org/kakaoshare/backend/domain/category/service/CategoryService.java
@@ -2,12 +2,14 @@ package org.kakaoshare.backend.domain.category.service;
 
 import lombok.RequiredArgsConstructor;
 import org.kakaoshare.backend.domain.category.dto.CategoryDto;
+import org.kakaoshare.backend.domain.category.entity.Category;
 import org.kakaoshare.backend.domain.category.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import static java.util.stream.Collectors.groupingBy;
 
@@ -15,37 +17,50 @@ import static java.util.stream.Collectors.groupingBy;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CategoryService {
-    public static final int MAX_LEVEL = 2;
-    public static final String ROOT_CATEGORY_NAME = "Root";
+    private static final String ROOT_CATEGORY_NAME = "Root";
     private final CategoryRepository categoryRepository;
     
     
     public CategoryDto createCategoryRoot() {
-        Map<Long, List<CategoryDto>> groupingByParentId = categoryRepository.findAllWithChildren()
-                .stream()
-                .map(CategoryDto::of)
-                .collect(groupingBy(CategoryDto::getCategoryId));
+        List<Category> categories = categoryRepository.findAllWithChildren();
+        CategoryDto rootCategoryDto = new CategoryDto(0L, ROOT_CATEGORY_NAME, 0L);
         
-        CategoryDto rootCategoryDto = new CategoryDto(0L, ROOT_CATEGORY_NAME, null);
-        rootCategoryDto.setLevel(0);
-        addSubCategories(rootCategoryDto, groupingByParentId);
+        Map<Long, List<CategoryDto>> parentGroup = categories
+                .stream()
+                .filter(category -> Objects.isNull(category.getParent()))
+                .map(category -> new CategoryDto(
+                        category.getCategoryId(),
+                        category.getName(),
+                        rootCategoryDto.getCategoryId()))//부모 ID를 Root로 임의 지정(DB에는 존재하지 않기 때문)
+                .collect(groupingBy(CategoryDto::getParentId));
+        
+        Map<Long, List<CategoryDto>> childGroup = categories
+                .stream()
+                .filter(category -> Objects.nonNull(category.getParent()))
+                .map(CategoryDto::of)
+                .collect(groupingBy(CategoryDto::getParentId));
+        
+        childGroup.forEach((parentId, subCategories) -> {
+            parentGroup.merge(parentId, subCategories, (existingSubCategories, newSubCategories) -> {
+                existingSubCategories.addAll(newSubCategories);
+                return existingSubCategories;
+            });
+        });
+        
+        addSubCategories(rootCategoryDto,parentGroup);
         
         return rootCategoryDto;
     }
     
     private void addSubCategories(CategoryDto parent, Map<Long, List<CategoryDto>> categoryGroup) {
-        int level = parent.getLevel();
-        if(level >= MAX_LEVEL)
-            return;
         List<CategoryDto> subCategories = categoryGroup.get(parent.getCategoryId());
         
-        if (subCategories == null)
+        if (subCategories == null) {
             return;
+        }
+        
         parent.setSubCategories(subCategories);
         
-        subCategories.forEach(s -> {
-            s.setLevel(level+1);
-            addSubCategories(s, categoryGroup);
-        });
+        subCategories.forEach(s -> addSubCategories(s, categoryGroup));
     }
 }
