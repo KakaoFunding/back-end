21d09474c60025fc0d5c06188a0cc2f46bdd7f34
kmw2378 <rlarla677@gmail.com>
2024-02-15 14:24:08 +0900

    [feat]PasswordEncoder 빈 등록 (${#14}) SecurityConfig에 PasswordEncoder 객체 빈 등록 메서드 추가

--- src/main/java/org/kakaoshare/backend/common/config/SecurityConfig.java
+++ src/main/java/org/kakaoshare/backend/common/config/SecurityConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -25,5 +28,19 @@ public class SecurityConfig {
                 .cors();
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern(ORIGIN_PATTERN);
+        configuration.addAllowedHeader(ORIGIN_PATTERN);
+        configuration.addAllowedMethod(ORIGIN_PATTERN);
+        configuration.setAllowCredentials(true);
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration(CORS_CONFIGURATION_PATTERN, configuration);
+
+        return source;
     }
 }
