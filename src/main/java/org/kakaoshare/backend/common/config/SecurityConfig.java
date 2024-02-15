2cfbe8bca3fd514eae8a5c0026ca6363a54cebca
kmw2378 <rlarla677@gmail.com>
2024-02-15 14:23:14 +0900

    [feat]Spring Security에 CORS 설정 추가 (${#14}) Spring Security가 Spring MVC보다 먼저 실행되므로 SecurityConfig에 CORS 설정 추가

--- src/main/java/org/kakaoshare/backend/common/config/SecurityConfig.java
+++ src/main/java/org/kakaoshare/backend/common/config/SecurityConfig.java
@@ -4,13 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -30,24 +25,5 @@ public class SecurityConfig {
                 .cors();
 
         return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOriginPattern(ORIGIN_PATTERN);
-        configuration.addAllowedHeader(ORIGIN_PATTERN);
-        configuration.addAllowedMethod(ORIGIN_PATTERN);
-        configuration.setAllowCredentials(true);
-
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration(CORS_CONFIGURATION_PATTERN, configuration);
-
-        return source;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
