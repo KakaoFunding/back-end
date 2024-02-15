207e2bda56c2be8679561c8e89571586d1b8cd43
YeaChan05 <qkenrdl05@gmail.com>
2024-02-15 23:57:55 +0900

    [feat]부모 카테고리 조회 로직 작성 (#${closes})
    부모 카테고리를 id를 통해 가져오고, 이떄 자식 카테고리들의 정보도 함깨 반환하는 로직 작성

--- src/main/java/org/kakaoshare/backend/domain/category/repository/query/CategoryRepositoryCustom.java
+++ src/main/java/org/kakaoshare/backend/domain/category/repository/query/CategoryRepositoryCustom.java
@@ -3,7 +3,10 @@ package org.kakaoshare.backend.domain.category.repository.query;
 import org.kakaoshare.backend.domain.category.entity.Category;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepositoryCustom {
     List<Category> findAllWithChildren();
+    
+    Optional<Category> findByCategoryIdWithChildren(Long categoryId);
 }
