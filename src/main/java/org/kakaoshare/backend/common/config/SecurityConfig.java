7c67d4e0678f796fe773979a7fd4a29358cbf07a
kmw2378 <rlarla677@gmail.com>
2024-02-15 14:21:08 +0900

    [feat]Spring Security 설정 클래스 추가 (${#14}) Http 요청에 관해 세션 사용 여부, 요청 권한 설정 추가

---
+++ src/main/java/org/kakaoshare/backend/common/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.kakaoshare.backend.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsUtils;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        http.httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .cors();
+
+        return http.build();
+    }
+}
