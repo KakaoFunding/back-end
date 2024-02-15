cc9017c4ac028b707286f27a9e1b33103f4440fa
YeaChan05 <qkenrdl05@gmail.com>
2024-02-15 23:57:55 +0900

    [feat]부모 카테고리 조회 로직 작성 (#${closes})
    부모 카테고리를 id를 통해 가져오고, 이떄 자식 카테고리들의 정보도 함깨 반환하는 로직 작성

--- src/main/java/org/kakaoshare/backend/domain/category/repository/query/CategoryRepositoryCustomImpl.java
+++ src/main/java/org/kakaoshare/backend/domain/category/repository/query/CategoryRepositoryCustomImpl.java
@@ -6,20 +6,30 @@ import org.kakaoshare.backend.domain.category.entity.Category;
 import org.kakaoshare.backend.domain.category.entity.QCategory;
 
 import java.util.List;
+import java.util.Optional;
+
 @RequiredArgsConstructor
 public class CategoryRepositoryCustomImpl implements CategoryRepositoryCustom {
     private final JPAQueryFactory queryFactory;
-
-
+    private static final QCategory category = QCategory.category;
+    private static final QCategory child = new QCategory("child");
+    
+    
     @Override
     public List<Category> findAllWithChildren() {
-        QCategory category = QCategory.category;
-        QCategory child = new QCategory("child");
-
         return queryFactory.selectFrom(category)
                 .leftJoin(category.children, child)
                 .fetchJoin()
                 .distinct()
                 .fetch();
+    }
+    
+    @Override
+    public Optional<Category> findByCategoryIdWithChildren(final Long categoryId) {
+        return Optional.ofNullable(queryFactory.selectFrom(category)
+                .where(category.categoryId.eq(categoryId))
+                .leftJoin(category.children, child)
+                .fetchJoin()
+                .fetchOne());
     }
 }
