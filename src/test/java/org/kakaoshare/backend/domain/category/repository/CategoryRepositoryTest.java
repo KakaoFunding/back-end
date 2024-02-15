e3b1bf15b151d412cc37dbec66e815c18444e3dd
YeaChan05 <qkenrdl05@gmail.com>
2024-02-16 04:06:59 +0900

    [test]ID조회 테스트 로직 추가 (#${closes})
    ID조회를 통한 카테고리에 대해 부모 카테고리, 자식 카테고리 조회기능 검증

--- src/test/java/org/kakaoshare/backend/domain/category/repository/CategoryRepositoryTest.java
+++ src/test/java/org/kakaoshare/backend/domain/category/repository/CategoryRepositoryTest.java
@@ -18,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class CategoryRepositoryTest {
     public static final String ROOT_NAME = "Root";
+    public static final String SUBCATEGORY = "Subcategory";
     @Autowired
     private CategoryRepository categoryRepository;
     
@@ -41,7 +42,7 @@ class CategoryRepositoryTest {
             
             for (int j = 1; j <= 5; j++) {
                 Category subCategory = Category.builder()
-                        .name("Category " + i + " - Subcategory " + j)
+                        .name("Category " + i + " - " + SUBCATEGORY + " " + j)
                         .parent(parentCategory)
                         .children(new ArrayList<>())
                         .build();
@@ -95,6 +96,62 @@ class CategoryRepositoryTest {
                 .forEach(CategoryRepositoryTest::assertChildCategory);
     }
     
+    @Test
+    @DisplayName("단일 ID를 통해 조회한 카테고리들은 부모 카테고리다")
+    void testFindByCategoryId() {
+        // given
+        List<Category> parentCategories = getParentCategories();
+        
+        for (Category parentCategory : parentCategories) {
+            Long categoryId = parentCategory.getCategoryId();
+            Category parent = categoryRepository.findByCategoryIdWithChildren(categoryId).orElseThrow();
+            
+            assertThat(parentCategory.getCategoryId()).isEqualTo(parent.getCategoryId());
+            assertThat(parent.getName()).doesNotContain(SUBCATEGORY);
+            assertThat(parent.getChildren().size()).isEqualTo(5);
+        }
+    }
+    
+    @Test
+    @DisplayName("자식 카테고리도 ID를 통해 조회 가능하다")
+    void testFindByCategoryIdAndSubCategoryId() {
+        // given
+        List<Category> childCategories = getChildCategories();
+        
+        for (Category childCategory : childCategories) {
+            // when
+            Long categoryId = childCategory.getParent().getCategoryId();
+            Category category
+                    = categoryRepository.findByCategoryIdWithChildren(categoryId).orElseThrow();
+            
+            // then
+            assertThat(category.getParent()).isNotNull();
+            assertThat(category.getChildren()).isEmpty();
+            assertThat(category.getName()).contains(SUBCATEGORY);
+        }
+    }
+    
+    private List<Category> getParentCategories() {// 부모 카테고리만 반환
+        return categoryRepository.findAll().stream()
+                .filter(this::haveNoParent)
+                .toList()
+                .stream()
+                .filter(category -> category.getParent().getName().equals(ROOT_NAME))
+                .toList();
+    }
+    
+    private List<Category> getChildCategories() {// 자식 카테고리만 반환
+        return categoryRepository.findAll().stream()
+                .filter(category -> List.of(category.getChildren()).isEmpty())
+                .toList();
+    }
+    
+    
+    private boolean haveNoParent(Category category) {
+        return Objects.nonNull(category.getParent());
+    }
+    
+    
     private static void assertParentCategory(Category parent, Category rootCategory) {
         assertThat(parent.getParent()).isNotNull();
         assertThat(parent.getParent()).isEqualTo(rootCategory);
@@ -103,7 +160,7 @@ class CategoryRepositoryTest {
     
     private static void assertChildCategory(Category child) {
         assertThat(child.getParent().getName()).contains("Category");
-        assertThat(child.getParent().getName()).doesNotContain("SubCategory");
+        assertThat(child.getParent().getName()).doesNotContain(SUBCATEGORY);
         assertThat(child.getChildren()).isEmpty();
     }
 }
