55932c344b0dbb4723bbc186448ac239f0072cd8
hwan2-99 <thh1233123@gmail.com>
2024-02-19 16:38:22 +0900

    [feat] 상품상세조회 컨트롤러 구현 (KakaoFunding#25)

---
+++ src/main/java/org/kakaoshare/backend/domain/product/controller/ProductController.java
@@ -0,0 +1,24 @@
+package org.kakaoshare.backend.domain.product.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.kakaoshare.backend.domain.product.dto.DetailResponse;
+import org.kakaoshare.backend.domain.product.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/app/v1/products")
+public class ProductController {
+    private final ProductService productService;
+    @GetMapping("/{productId}")
+    public ResponseEntity<DetailResponse> getProductDetail(@PathVariable Long productId) {
+        DetailResponse detailResponse = productService.getProductDetail(productId);
+        return ResponseEntity.ok(detailResponse);
+    }
+}
