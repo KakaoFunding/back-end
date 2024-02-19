b8a118764b3d59e893a445dcaf2416e98752f3be
hwan2-99 <thh1233123@gmail.com>
2024-02-19 16:32:19 +0900

    [feat] 상품상세조회 기능 구현 (KakaoFunding#25)

--- src/main/java/org/kakaoshare/backend/domain/product/service/ProductService.java
+++ src/main/java/org/kakaoshare/backend/domain/product/service/ProductService.java
@@ -1,11 +1,27 @@
 package org.kakaoshare.backend.domain.product.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.kakaoshare.backend.domain.product.dto.DetailResponse;
+import org.kakaoshare.backend.domain.product.entity.Product;
+import org.kakaoshare.backend.domain.product.repository.ProductQueryRepository;
 import org.kakaoshare.backend.domain.product.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional(readOnly = true)
 public class ProductService {
     private final ProductRepository productRepository;
+    private final ProductQueryRepository productQueryRepository;
+
+
+    public DetailResponse getProductDetail(Long productId) {
+        Product product = productQueryRepository.findProductWithDetailsAndPhotos(productId);
+        if (product == null) {
+            throw new EntityNotFoundException("Product not found with id: " + productId);
+        }
+        return DetailResponse.from(product);
+    }
 }
