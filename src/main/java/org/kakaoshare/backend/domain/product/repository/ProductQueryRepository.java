08c28696ebe91f22960c88747a44d9f49982af0f
hwan2-99 <thh1233123@gmail.com>
2024-02-21 15:54:20 +0900

    [refactor] 상품 상세조회를 위한 쿼리에 option join 추가 (KakaoFunding#25)

--- src/main/java/org/kakaoshare/backend/domain/product/repository/ProductQueryRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/product/repository/ProductQueryRepository.java
@@ -2,6 +2,7 @@ package org.kakaoshare.backend.domain.product.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.kakaoshare.backend.domain.option.entity.QOption;
 import org.kakaoshare.backend.domain.product.entity.Product;
 import org.kakaoshare.backend.domain.product.entity.QProduct;
 import org.kakaoshare.backend.domain.product.entity.QProductDescriptionPhoto;
@@ -17,11 +18,13 @@ public class ProductQueryRepository {
         QProduct qProduct = QProduct.product;
         QProductDetail qProductDetail = QProductDetail.productDetail;
         QProductDescriptionPhoto qProductDescriptionPhoto = QProductDescriptionPhoto.productDescriptionPhoto;
+        QOption qOption = QOption.option;
 
         return queryFactory
                 .selectFrom(qProduct)
                 .leftJoin(qProduct.productDetail, qProductDetail).fetchJoin()
                 .leftJoin(qProduct.productDescriptionPhotos, qProductDescriptionPhoto).fetchJoin()
+                .leftJoin(qProduct.options, qOption).fetchJoin()
                 .where(qProduct.productId.eq(productId))
                 .fetchOne();
     }
