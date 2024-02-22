69c162f644ae5fa1a547e55b282a53505e37b0ce
kmw2378 <rlarla677@gmail.com>
2024-02-22 15:38:42 +0900

    [feat]MemberRepository.findByEmail() 메서드 추가 ${#21} 이메일을 통해 회원 엔티티를 조회하는 메서드 추가

--- src/main/java/org/kakaoshare/backend/domain/member/repository/MemberRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/member/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package org.kakaoshare.backend.domain.member.repository;
 import org.kakaoshare.backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(final String email);
 }
