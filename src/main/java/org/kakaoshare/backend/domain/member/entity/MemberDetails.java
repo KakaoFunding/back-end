adc22df6769e18c3daa9ffec6536f8021f03a766
kmw2378 <rlarla677@gmail.com>
2024-02-22 15:36:36 +0900

    [feat]MemberDetails 클래스 추가 ${#21} 회원 엔티티 정보를 통해 UserDetails의 추상 메서드을 구현하는 클래스 추가

---
+++ src/main/java/org/kakaoshare/backend/domain/member/entity/MemberDetails.java
@@ -0,0 +1,51 @@
+package org.kakaoshare.backend.domain.member.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class MemberDetails implements UserDetails {
+    private final Member member;
+
+    public MemberDetails(final Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
