862315334599e41fb6ee2050fe8de17bbab0019e
kmw2378 <rlarla677@gmail.com>
2024-02-25 15:31:53 +0900

    [feat]카카오 사용자 정보를 요청할 때 사용되는 DTO 추가  ${#28}

---
+++ src/main/java/org/kakaoshare/backend/domain/member/dto/oauth/profile/KakaoProfile.java
@@ -0,0 +1,47 @@
+package org.kakaoshare.backend.domain.member.dto.oauth.profile;
+
+import java.util.Map;
+
+public class KakaoProfile extends OAuthProfile {
+    public KakaoProfile(final Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) getProfile().get("email");
+    }
+
+    @Override
+    public String getGender() {
+        return (String) getAccount().get("gender");
+    }
+
+    @Override
+    public String getName() {
+        return (String) getAccount().get("nickname");
+    }
+
+    @Override
+    public String getPhoneNumber() {
+        return (String) getAccount().get("phone_number");
+    }
+
+    @Override
+    public String getProvider() {
+        return "KAKAO";
+    }
+
+    @Override
+    public String getProviderId() {
+        return (String) attributes.get("id");
+    }
+
+    private Map<String, Object> getAccount() {
+        return (Map<String, Object>) attributes.get("kakao_account");
+    }
+
+    private Map<String, Object> getProfile() {
+        return (Map<String, Object>) getAccount().get("profile");
+    }
+}
