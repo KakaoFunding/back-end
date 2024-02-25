e291d5a74d44fb01e15e989c8dba8ef9c10fe15e
kmw2378 <rlarla677@gmail.com>
2024-02-25 15:31:36 +0900

    [feat]OAuth2 토큰으로 사용자 정보를 요청할 때 사용되는 DTO 추가  ${#28} 소셜로 부터 얻는 토큰을 통해 사용자 정보를 요청할 때 사용되는 DTO 추가

---
+++ src/main/java/org/kakaoshare/backend/domain/member/dto/oauth/profile/OAuthProfile.java
@@ -0,0 +1,18 @@
+package org.kakaoshare.backend.domain.member.dto.oauth.profile;
+
+import java.util.Map;
+
+public abstract class OAuthProfile {
+    protected final Map<String, Object> attributes;
+
+    protected OAuthProfile(final Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public abstract String getEmail();
+    public abstract String getGender();
+    public abstract String getName();
+    public abstract String getPhoneNumber();
+    public abstract String getProvider();
+    public abstract String getProviderId();
+}
