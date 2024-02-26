3f6437bd50e8594205065d358ce393cbd1fe78a8
hwan2-99 <thh1233123@gmail.com>
2024-02-26 22:18:56 +0900

    [refactor] 상세 정보 조회기능 구현 (KakaoFunding#25)

--- src/main/java/org/kakaoshare/backend/domain/product/repository/query/ProductRepositoryCustomImpl.java
+++ src/main/java/org/kakaoshare/backend/domain/product/repository/query/ProductRepositoryCustomImpl.java
@@ -3,11 +3,9 @@ package org.kakaoshare.backend.domain.product.repository.query;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import org.kakaoshare.backend.domain.option.entity.QOption;
-import org.kakaoshare.backend.domain.product.dto.DetailDescriptionResponse;
-import org.kakaoshare.backend.domain.product.entity.Product;
+import org.kakaoshare.backend.domain.product.dto.DescriptionResponse;
+import org.kakaoshare.backend.domain.product.dto.DetailResponse;
 import org.kakaoshare.backend.domain.product.entity.QProduct;
-import org.kakaoshare.backend.domain.product.entity.QProductDescriptionPhoto;
 import org.kakaoshare.backend.domain.product.entity.QProductDetail;
 import org.springframework.stereotype.Repository;
 
@@ -16,13 +14,14 @@ import org.springframework.stereotype.Repository;
 public class ProductRepositoryCustomImpl implements ProductRepositoryCustom{
     private final JPAQueryFactory queryFactory;
 
-    public DetailDescriptionResponse findProductWithDetailsAndPhotos(Long productId) {
+    public DescriptionResponse findProductWithDetailsAndPhotos(Long productId) {
         return queryFactory
-                .select(Projections.bean(DetailDescriptionResponse.class,
+                .select(Projections.bean(DescriptionResponse.class,
                         QProduct.product.productId.as("productId"),
                         QProduct.product.name,
                         QProduct.product.price,
                         QProduct.product.type,
+                        QProduct.product.photo,
                         QProduct.product.productDetail.description.as("description"),
                         QProduct.product.productDescriptionPhotos.as("descriptionPhotos"),
                         QProduct.product.productDetail.hasPhoto.as("hasPhoto"),
@@ -34,6 +33,32 @@ public class ProductRepositoryCustomImpl implements ProductRepositoryCustom{
                 .leftJoin(QProduct.product.productDescriptionPhotos).fetchJoin()
                 .leftJoin(QProduct.product.options).fetchJoin()
                 .where(QProduct.product.productId.eq(productId))
+                .fetchOne();
+    }
+
+    public DetailResponse findProductDetail(Long productId) {
+        QProduct qProduct = QProduct.product;
+        QProductDetail qProductDetail = QProductDetail.productDetail;
+
+        return queryFactory
+                .select(Projections.constructor(DetailResponse.class,
+                        qProduct.productId,
+                        qProduct.name,
+                        qProduct.price,
+                        qProduct.type,
+                        qProductDetail.hasPhoto,
+                        qProductDetail.productName,
+                        qProductDetail.origin,
+                        qProductDetail.manufacturer,
+                        qProductDetail.tel,
+                        qProductDetail.deliverDescription,
+                        qProductDetail.billingNotice,
+                        qProductDetail.caution,
+                        qProduct.options.any(),
+                        qProduct.brand))
+                .from(qProduct)
+                .leftJoin(qProduct.productDetail, qProductDetail)
+                .where(qProduct.productId.eq(productId))
                 .fetchOne();
     }
 }
