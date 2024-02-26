d8505ac20fab5464e1123ebb4f1415a8cbe4ee72
YeaChan05 <qkenrdl05@gmail.com>
2024-02-26 17:58:58 +0900

    [perf]root 조회 방식의 전체 카테고리 조회 로직 수정 (#12)
    기존의 root 조회 방식의 쿼리가 비이상적으로 많이 발생하여 부모 카테고리를 조회하는 방식으로 수정

--- src/main/java/org/kakaoshare/backend/domain/category/service/CategoryService.java
+++ src/main/java/org/kakaoshare/backend/domain/category/service/CategoryService.java
@@ -7,14 +7,16 @@ import org.kakaoshare.backend.domain.category.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
-    public CategoryDto getRootCategory() {
-        Category rootCategory = categoryRepository.findRoot().orElseThrow();
-        return CategoryDto.from(rootCategory);
+    public List<CategoryDto> getParentCategory() {
+        List<Category> categories = categoryRepository.findAllParentCategories();
+        return categories.stream().map(CategoryDto::from).toList();
     }
     
     public CategoryDto getSpecificCategory(Long categoryId){
