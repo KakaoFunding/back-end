a68080c43b8d34bdde542e65aefb8a96c3e6d651
hwan2-99 <thh1233123@gmail.com>
2024-02-26 22:19:29 +0900

    [refactor] 상세정보 또는 상세 설명 조회 api 구현 (KakaoFunding#25)

--- src/main/java/org/kakaoshare/backend/domain/product/controller/ProductController.java
+++ src/main/java/org/kakaoshare/backend/domain/product/controller/ProductController.java
@@ -2,12 +2,14 @@ package org.kakaoshare.backend.domain.product.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.kakaoshare.backend.domain.product.dto.DetailDescriptionResponse;
+import org.kakaoshare.backend.domain.product.dto.DescriptionResponse;
+import org.kakaoshare.backend.domain.product.dto.DetailResponse;
 import org.kakaoshare.backend.domain.product.service.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -16,9 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/products")
 public class ProductController {
     private final ProductService productService;
+
     @GetMapping("/{productId}")
-    public ResponseEntity<DetailDescriptionResponse> getProductDetail(@PathVariable Long productId) {
-        DetailDescriptionResponse response = productService.getProductDetail(productId);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<?> getProductDetail(@PathVariable Long productId,
+                                                                @RequestParam(name = "tab", required = false, defaultValue = "description") String tab) {
+        if ("description".equals(tab)) {
+            DescriptionResponse response = productService.getProductDescription(productId);
+            return ResponseEntity.ok(response);
+        }
+        if ("detail".equals(tab)){
+            DetailResponse response = productService.getProductDetail(productId);
+            return ResponseEntity.ok(response);
+        }
+        return ResponseEntity.badRequest().body("Invalid tab value");
     }
 }
--- src/main/java/org/kakaoshare/backend/domain/product/service/ProductService.java
+++ src/main/java/org/kakaoshare/backend/domain/product/service/ProductService.java
@@ -2,8 +2,8 @@ package org.kakaoshare.backend.domain.product.service;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.kakaoshare.backend.domain.product.dto.DetailDescriptionResponse;
-import org.kakaoshare.backend.domain.product.entity.Product;
+import org.kakaoshare.backend.domain.product.dto.DescriptionResponse;
+import org.kakaoshare.backend.domain.product.dto.DetailResponse;
 import org.kakaoshare.backend.domain.product.repository.query.ProductRepositoryCustomImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,12 +15,20 @@ public class ProductService {
     private final ProductRepositoryCustomImpl productRepositoryCustomImpl;
 
 
-    public DetailDescriptionResponse getProductDetail(Long productId) {
-        DetailDescriptionResponse detailDescriptionResponse = productRepositoryCustomImpl.findProductWithDetailsAndPhotos(productId);
-        if (detailDescriptionResponse == null) {
+    public DescriptionResponse getProductDescription(Long productId) {
+        DescriptionResponse descriptionResponse = productRepositoryCustomImpl.findProductWithDetailsAndPhotos(
+                productId);
+        if (descriptionResponse == null) {
             throw new EntityNotFoundException("Product not found with id: " + productId);
         }
-        return detailDescriptionResponse;
+        return descriptionResponse;
     }
 
+    public DetailResponse getProductDetail(Long productId) {
+        DetailResponse detailResponse = productRepositoryCustomImpl.findProductDetail(productId);
+        if (detailResponse == null) {
+            throw new EntityNotFoundException("Product not found with id: " + productId);
+        }
+        return detailResponse;
+    }
 }
