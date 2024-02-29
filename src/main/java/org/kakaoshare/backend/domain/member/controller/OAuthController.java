0665b756c06ec22c9c8e0d2c708ef37e0883a10b
kmw2378 <rlarla677@gmail.com>
2024-02-29 13:05:10 +0900

    [feat]OAuthController 추가 ${#28}

---
+++ src/main/java/org/kakaoshare/backend/domain/member/controller/OAuthController.java
@@ -0,0 +1,22 @@
+package org.kakaoshare.backend.domain.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.kakaoshare.backend.domain.member.dto.oauth.authenticate.OAuthAuthenticateRequest;
+import org.kakaoshare.backend.domain.member.dto.oauth.authenticate.OAuthAuthenticateResponse;
+import org.kakaoshare.backend.domain.member.service.oauth.OAuthService;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/oauth")
+@RestController
+public class OAuthController {
+    private final OAuthService oAuthService;
+
+    @PostMapping("/login")
+    public OAuthAuthenticateResponse login(@ModelAttribute final OAuthAuthenticateRequest oAuthAuthenticateRequest) {
+        return oAuthService.authenticate(oAuthAuthenticateRequest);
+    }
+}
