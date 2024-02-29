b54770d7c89b585d3db4e58a6b007b2adeec6f93
kmw2378 <rlarla677@gmail.com>
2024-02-29 12:37:54 +0900

    [refactor]MemberRepository 에 상속 관계 추가 ${#28}

--- src/main/java/org/kakaoshare/backend/domain/member/repository/MemberRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/member/repository/MemberRepository.java
@@ -1,11 +1,11 @@
 package org.kakaoshare.backend.domain.member.repository;
 
 import org.kakaoshare.backend.domain.member.entity.Member;
+import org.kakaoshare.backend.domain.member.repository.query.MemberRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
     Optional<Member> findByProviderId(final String providerId);
 }
