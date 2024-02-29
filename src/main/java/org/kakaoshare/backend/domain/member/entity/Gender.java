e06c958694cd1192ccf366be8aa7104474100e8c
kmw2378 <rlarla677@gmail.com>
2024-02-29 12:37:30 +0900

    [feat]Gender에 정적 팩토리 메서드 추가 ${#28}

--- src/main/java/org/kakaoshare/backend/domain/member/entity/Gender.java
+++ src/main/java/org/kakaoshare/backend/domain/member/entity/Gender.java
@@ -1,5 +1,14 @@
 package org.kakaoshare.backend.domain.member.entity;
 
+import java.util.Arrays;
+
 public enum Gender {
-    MALE, FEMALE
+    MALE, FEMALE;
+
+    public static Gender from(final String gender) {
+        return Arrays.stream(values())
+                .filter(value -> value.name().equals(gender.toUpperCase()))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException());
+    }
 }
