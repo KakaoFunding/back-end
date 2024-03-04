643321720b175d3765d0d014d1ff1e22e64a575d
YeaChan05 <qkenrdl05@gmail.com>
2024-03-04 21:18:39 +0900

    [refactor]setter 제거 (#12)
    불필요한 setter 제거

--- src/main/java/org/kakaoshare/backend/domain/product/entity/query/SimpleProductDto.java
+++ src/main/java/org/kakaoshare/backend/domain/product/entity/query/SimpleProductDto.java
@@ -3,12 +3,10 @@ package org.kakaoshare.backend.domain.product.entity.query;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Getter
-@Setter
 @NoArgsConstructor
 public class SimpleProductDto {
     private String name;
