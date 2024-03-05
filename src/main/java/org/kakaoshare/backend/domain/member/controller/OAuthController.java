7ad2deda2c5aa1b9be864ae40293d47bf25748de
kmw2378 <rlarla677@gmail.com>
2024-03-05 16:40:48 +0900

    [refactor]OAuthController 의존관계 추가 및 login() 메서드 수정 ${#38} RefreshToken 을 Cookie에 추가, 응답값 수정

--- src/main/java/org/kakaoshare/backend/domain/member/controller/OAuthController.java
+++ src/main/java/org/kakaoshare/backend/domain/member/controller/OAuthController.java
@@ -3,7 +3,10 @@ package org.kakaoshare.backend.domain.member.controller;
 import lombok.RequiredArgsConstructor;
 import org.kakaoshare.backend.domain.member.dto.oauth.authenticate.OAuthLoginRequest;
 import org.kakaoshare.backend.domain.member.dto.oauth.authenticate.OAuthLoginResponse;
+import org.kakaoshare.backend.domain.member.dto.oauth.authenticate.OAuthLoginResult;
 import org.kakaoshare.backend.domain.member.service.oauth.OAuthService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,9 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OAuthController {
     private final OAuthService oAuthService;
+    private final RefreshTokenCookieProvider refreshTokenCookieProvider;
 
     @PostMapping("/login")
-    public ResponseEntity<OAuthLoginResponse> login(@ModelAttribute final OAuthLoginRequest oAuthAuthenticateRequest) {
-        return ResponseEntity.ok(oAuthService.login(oAuthAuthenticateRequest));
+    public ResponseEntity<OAuthLoginResponse> login(@ModelAttribute final OAuthLoginRequest oAuthLoginRequest) {
+        final OAuthLoginResult loginResult = oAuthService.login(oAuthLoginRequest);
+        final String refreshToken = loginResult.refreshToken();
+        final ResponseCookie cookie = refreshTokenCookieProvider.createCookie(refreshToken);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(OAuthLoginResponse.from(loginResult));
     }
 }
