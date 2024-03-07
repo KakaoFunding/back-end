6b8fc36872abfd5af59c8120a22c1377ba06c19d
kmw2378 <rlarla677@gmail.com>
2024-03-08 01:41:45 +0900

    [test]MemberRepositoryCustomImpl 테스트 수정 ${#28} DatabaseCleanUp과 의존성 제거

--- src/test/java/org/kakaoshare/backend/domain/member/repository/query/MemberRepositoryCustomImplTest.java
+++ src/test/java/org/kakaoshare/backend/domain/member/repository/query/MemberRepositoryCustomImplTest.java
@@ -1,33 +1,21 @@
 package org.kakaoshare.backend.domain.member.repository.query;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.kakaoshare.backend.util.DatabaseCleanUp;
+import org.kakaoshare.backend.common.RepositoryTest;
 import org.kakaoshare.backend.domain.member.entity.Member;
 import org.kakaoshare.backend.domain.member.entity.MemberDetails;
 import org.kakaoshare.backend.domain.member.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.kakaoshare.backend.fixture.MemberFixture.KAKAO;
 
-@EnableJpaAuditing
-@SpringBootTest
+@RepositoryTest
 class MemberRepositoryCustomImplTest {
     @Autowired
     MemberRepository memberRepository;
-
-    @Autowired
-    private DatabaseCleanUp databaseCleanUp;
-
-    @BeforeEach
-    public void setUp() {
-        databaseCleanUp.execute();
-    }
 
     @Test
     @DisplayName("providerId로 Member 조회")
