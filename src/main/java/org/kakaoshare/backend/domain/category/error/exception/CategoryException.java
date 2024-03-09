0b69fc0a0f82abd6ceec609e6e721f3005b47443
YeaChan05 <qkenrdl05@gmail.com>
2024-03-09 16:14:27 +0900

    [rename]예외명 수정 (#41)
    `CategoryNotFoundException` -> `CategoryException`으로 변경

--- src/main/java/org/kakaoshare/backend/domain/category/error/exception/CategoryException.java
+++ src/main/java/org/kakaoshare/backend/domain/category/error/exception/CategoryException.java
@@ -3,8 +3,8 @@ package org.kakaoshare.backend.domain.category.error.exception;
 import org.kakaoshare.backend.common.error.ErrorCode;
 import org.kakaoshare.backend.common.error.exception.BusinessException;
 
-public class CategoryNotFoundException extends BusinessException {
-    public CategoryNotFoundException(final ErrorCode errorCode) {
+public class CategoryException extends BusinessException {
+    public CategoryException(final ErrorCode errorCode) {
         super(errorCode);
     }
 }
--- src/main/java/org/kakaoshare/backend/domain/category/service/CategoryService.java
+++ src/main/java/org/kakaoshare/backend/domain/category/service/CategoryService.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.kakaoshare.backend.domain.category.dto.CategoryDto;
 import org.kakaoshare.backend.domain.category.entity.Category;
 import org.kakaoshare.backend.domain.category.error.CategoryErrorCode;
-import org.kakaoshare.backend.domain.category.error.exception.CategoryNotFoundException;
+import org.kakaoshare.backend.domain.category.error.exception.CategoryException;
 import org.kakaoshare.backend.domain.category.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,13 +24,13 @@ public class CategoryService {
     
     public CategoryDto getParentCategory(final Long categoryId) {
         Category category = categoryRepository.findParentCategoryWithChildren(categoryId)
-                .orElseThrow(() -> new CategoryNotFoundException(CategoryErrorCode.CATEGORY_NOT_FOUND));
+                .orElseThrow(() -> new CategoryException(CategoryErrorCode.CATEGORY_NOT_FOUND));
         return CategoryDto.from(category);
     }
     
     public CategoryDto getChildCategory(final Long categoryId, final Long subcategoryId) {
         Category category = categoryRepository.findChildCategoryWithParentCheck(categoryId, subcategoryId)
-                .orElseThrow(() -> new CategoryNotFoundException(CategoryErrorCode.INVALID_SUB_CATEGORY_ID));
+                .orElseThrow(() -> new CategoryException(CategoryErrorCode.INVALID_SUB_CATEGORY_ID));
         return CategoryDto.from(category);
     }
 }
