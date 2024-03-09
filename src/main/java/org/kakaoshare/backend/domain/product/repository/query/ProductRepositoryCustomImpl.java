e7fd7b577b5145e7d8ce0e50c32101326e78bff3
YeaChan05 <qkenrdl05@gmail.com>
2024-03-09 16:28:39 +0900

    [rename]`SimpleProductDto`위치 변경 (#41)
    `SimpleProductDto`의 위치를 `dto`패키지로 이동

--- src/main/java/org/kakaoshare/backend/domain/product/repository/query/ProductRepositoryCustomImpl.java
+++ src/main/java/org/kakaoshare/backend/domain/product/repository/query/ProductRepositoryCustomImpl.java
@@ -10,7 +10,7 @@ import org.kakaoshare.backend.common.util.sort.SortUtil;
 import org.kakaoshare.backend.common.util.sort.SortableRepository;
 import org.kakaoshare.backend.domain.product.dto.DescriptionResponse;
 import org.kakaoshare.backend.domain.product.dto.DetailResponse;
-import org.kakaoshare.backend.domain.product.entity.query.QSimpleProductDto;
+import org.kakaoshare.backend.domain.product.dto.QSimpleProductDto;
 import org.kakaoshare.backend.domain.product.dto.SimpleProductDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
