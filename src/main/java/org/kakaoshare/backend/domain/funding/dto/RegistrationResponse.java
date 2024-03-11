f2434acee28537306d04cda08770b724a0bb34ea
hwan2-99 <thh1233123@gmail.com>
2024-03-11 17:36:55 +0900

    [feat] 펀딩아이템 등록 응답 dto 클래스 구현 (KakaoFunding#47)

---
+++ src/main/java/org/kakaoshare/backend/domain/funding/dto/RegistrationResponse.java
@@ -0,0 +1,10 @@
+package org.kakaoshare.backend.domain.funding.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class RegistrationResponse {
+    private final Long id;
+}
