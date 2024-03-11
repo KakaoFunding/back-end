3b23e8deb178cc3f87d9fd686204796325ce7b0c
hwan2-99 <thh1233123@gmail.com>
2024-03-11 18:42:49 +0900

    [feat] 펀딩아이템 등록 응답 dto 클래스 정적팩토리 메소드 구현 (KakaoFunding#47)

--- src/main/java/org/kakaoshare/backend/domain/funding/dto/RegistrationResponse.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/dto/RegistrationResponse.java
@@ -2,9 +2,16 @@ package org.kakaoshare.backend.domain.funding.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.kakaoshare.backend.domain.funding.entity.Funding;
 
 @Getter
 @Builder
 public class RegistrationResponse {
     private final Long id;
+
+    public static RegistrationResponse from(Funding funding) {
+        return RegistrationResponse.builder()
+                .id(funding.getFundingId())
+                .build();
+    }
 }
