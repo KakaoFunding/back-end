7f581d7b7d5fb5a05abc36b1120934f309b51f41
hwan2-99 <thh1233123@gmail.com>
2024-03-11 16:58:56 +0900

    [feat] Querydsl을 이용하기 위한 funding관련 레포지토리 구현 (KakaoFunding#47)

--- src/main/java/org/kakaoshare/backend/domain/funding/repository/FundingRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/repository/FundingRepository.java
@@ -1,8 +1,9 @@
 package org.kakaoshare.backend.domain.funding.repository;
 
 import org.kakaoshare.backend.domain.funding.entity.Funding;
+import org.kakaoshare.backend.domain.funding.repository.query.FundingRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface FundingRepository extends JpaRepository<Funding, Long> {
+public interface FundingRepository extends JpaRepository<Funding, Long>, FundingRepositoryCustom {
 }
