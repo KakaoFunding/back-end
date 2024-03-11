8400889ac958fd78036e69c2863fe2479f5114aa
hwan2-99 <thh1233123@gmail.com>
2024-03-11 18:42:31 +0900

    [feat] 펀딩아이템 등록 요청 dto 클래스 구현 (KakaoFunding#47)

---
+++ src/main/java/org/kakaoshare/backend/domain/funding/dto/RegisterRequest.java
@@ -0,0 +1,27 @@
+package org.kakaoshare.backend.domain.funding.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+import org.kakaoshare.backend.domain.funding.entity.Funding;
+import org.kakaoshare.backend.domain.member.entity.Member;
+import org.kakaoshare.backend.domain.product.entity.Product;
+
+@Getter
+@Builder
+public class RegisterRequest {
+    private BigDecimal goalAmount;
+    private LocalDate expiredAt;
+
+    public Funding toEntity(Member member, Product product){
+        return Funding.builder()
+                .member(member)
+                .product(product)
+                .goalAmount(this.goalAmount)
+                .expiredAt(this.expiredAt)
+                .accumulateAmount(BigDecimal.ZERO)
+                .status("ACTIVE")
+                .build();
+    }
+}
