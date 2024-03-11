b80802dd767ae2f2da55b47970ddaf61f9ab4a04
hwan2-99 <thh1233123@gmail.com>
2024-03-11 18:43:55 +0900

    [refactor] 펀딩엔티티 클래스 @Builder 어노테이션 추가(KakaoFunding#47)

--- src/main/java/org/kakaoshare/backend/domain/funding/entity/Funding.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/entity/Funding.java
@@ -8,17 +8,21 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.List;
+import lombok.Builder;
 import lombok.Getter;
 import org.kakaoshare.backend.domain.base.entity.BaseTimeEntity;
 import org.kakaoshare.backend.domain.member.entity.Member;
 import org.kakaoshare.backend.domain.product.entity.Product;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
 
 @Entity
 @Getter
+@Builder
 public class Funding extends BaseTimeEntity {
 
     @Id
@@ -40,6 +44,9 @@ public class Funding extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
+
+    @OneToMany(mappedBy = "funding")
+    private List<FundingDetail> fundingDetails;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", nullable = false)
