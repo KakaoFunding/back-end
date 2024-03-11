5fb0103d492a8c18ea20913fd7fd82c81f2eb77b
hwan2-99 <thh1233123@gmail.com>
2024-03-11 18:43:11 +0900

    [feat] 토큰에서 얻은 providerId 값으로 유저 추출 메소드 구현 (KakaoFunding#47)

--- src/main/java/org/kakaoshare/backend/domain/member/repository/MemberRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/member/repository/MemberRepository.java
@@ -1,8 +1,10 @@
 package org.kakaoshare.backend.domain.member.repository;
 
+import java.util.Optional;
 import org.kakaoshare.backend.domain.member.entity.Member;
 import org.kakaoshare.backend.domain.member.repository.query.MemberRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+    Optional<Member> findByProviderId(String providerId);
 }
