0c61f6d512a41999bc280f9afc0cc7e5dbd4e2fa
hwan2-99 <thh1233123@gmail.com>
2024-03-12 13:22:23 +0900

    [feat] 펀딩 아이템 현재 상태 응답 dto 클래스 구현 및 정적 팩토리 메소드 구현(KakaoFunding#47)

---
+++ src/main/java/org/kakaoshare/backend/domain/funding/dto/ProgressResponse.java
@@ -0,0 +1,36 @@
+package org.kakaoshare.backend.domain.funding.dto;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Optional;
+import lombok.Builder;
+import lombok.Getter;
+import org.kakaoshare.backend.domain.funding.entity.Funding;
+
+@Getter
+@Builder
+public class ProgressResponse {
+    private final Long fundingId;
+    private final double progressRate;
+    private final BigDecimal remainAmount;
+    private final BigDecimal goalAmount;
+    private final BigDecimal accumulateAmount;
+
+    public static ProgressResponse from(Funding funding) {
+        BigDecimal goalAmount = funding.getGoalAmount();
+        BigDecimal accumulateAmount = funding.getAccumulateAmount();
+        double progressRate = Optional.of(goalAmount)
+                .filter(ga -> ga.compareTo(BigDecimal.ZERO) != 0)
+                .map(ga -> accumulateAmount.divide(ga, 4, RoundingMode.HALF_UP).doubleValue() * 100)
+                .orElse(0.0);
+        BigDecimal remainAmount = goalAmount.subtract(accumulateAmount);
+
+        return ProgressResponse.builder()
+                .fundingId(funding.getFundingId())
+                .progressRate(progressRate)
+                .remainAmount(remainAmount)
+                .goalAmount(goalAmount)
+                .accumulateAmount(accumulateAmount)
+                .build();
+    }
+}
