20de568820491d1539f03ab044091c8bb69e27b4
hwan2-99 <thh1233123@gmail.com>
2024-03-12 13:21:53 +0900

    [feat] 펀딩 아이템 정보불러오는 메소드 구현(KakaoFunding#47)

--- src/main/java/org/kakaoshare/backend/domain/funding/repository/FundingRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/repository/FundingRepository.java
@@ -1,9 +1,11 @@
 package org.kakaoshare.backend.domain.funding.repository;
 
+import java.util.Optional;
 import org.kakaoshare.backend.domain.funding.entity.Funding;
 import org.kakaoshare.backend.domain.funding.repository.query.FundingRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface FundingRepository extends JpaRepository<Funding, Long>, FundingRepositoryCustom {
+
 }
--- src/main/java/org/kakaoshare/backend/domain/funding/repository/query/FundingRepositoryCustom.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/repository/query/FundingRepositoryCustom.java
@@ -1,5 +1,9 @@
 package org.kakaoshare.backend.domain.funding.repository.query;
 
 
+import java.util.Optional;
+import org.kakaoshare.backend.domain.funding.entity.Funding;
+
 public interface FundingRepositoryCustom {
+    Optional<Funding> findByIdAndMemberId(Long fundingId, Long memberId);
 }
