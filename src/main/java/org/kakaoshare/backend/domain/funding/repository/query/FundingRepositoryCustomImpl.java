b09e82826f94a17d74904799da0adcd981a4f9c6
hwan2-99 <thh1233123@gmail.com>
2024-03-12 13:21:53 +0900

    [feat] 펀딩 아이템 정보불러오는 메소드 구현(KakaoFunding#47)

--- src/main/java/org/kakaoshare/backend/domain/funding/repository/query/FundingRepositoryCustomImpl.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/repository/query/FundingRepositoryCustomImpl.java
@@ -1,9 +1,22 @@
 package org.kakaoshare.backend.domain.funding.repository.query;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.kakaoshare.backend.domain.funding.entity.Funding;
+import org.kakaoshare.backend.domain.funding.entity.QFunding;
 
 @RequiredArgsConstructor
 public class FundingRepositoryCustomImpl implements FundingRepositoryCustom {
     private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Optional<Funding> findByIdAndMemberId(Long fundingId, Long memberId) {
+        Funding funding = queryFactory
+                .selectFrom(QFunding.funding)
+                .where(QFunding.funding.fundingId.eq(fundingId)
+                        .and(QFunding.funding.member.memberId.eq(memberId)))
+                .fetchOne();
+        return Optional.ofNullable(funding);
+    }
 }
