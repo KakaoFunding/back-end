8294f5024e789f5d2827a861ab09d7671155cb10
hwan2-99 <thh1233123@gmail.com>
2024-03-12 17:40:11 +0900

    [feat] 진행중인 펀딩 조회 메소드 구현 (KakaoFunding#47)

--- src/main/java/org/kakaoshare/backend/domain/funding/repository/FundingRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/repository/FundingRepository.java
@@ -1,5 +1,6 @@
 package org.kakaoshare.backend.domain.funding.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.kakaoshare.backend.domain.funding.entity.Funding;
 import org.kakaoshare.backend.domain.funding.repository.query.FundingRepositoryCustom;
