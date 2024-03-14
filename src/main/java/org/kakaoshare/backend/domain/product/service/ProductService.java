bc48f0ce1fd946b06f2f974959edfe9684acb28f
YeaChan05 <qkenrdl05@gmail.com>
2024-03-14 19:29:39 +0900

    [style]불필요한 세미콜론 제거 (#42)
    불필요한 세미콜론 제거

--- src/main/java/org/kakaoshare/backend/domain/product/service/ProductService.java
+++ src/main/java/org/kakaoshare/backend/domain/product/service/ProductService.java
@@ -14,8 +14,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-;
-
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
