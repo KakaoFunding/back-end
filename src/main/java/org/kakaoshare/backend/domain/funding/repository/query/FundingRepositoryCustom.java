459c0365b5041e976e99de83335ed975fcb671c0
hwan2-99 <thh1233123@gmail.com>
2024-03-15 23:57:16 +0900

    [feat] 펀딩 전채 조회기능 구현 및 slice화 하여 조회기능 구현 (KakaoFunding#47)

--- src/main/java/org/kakaoshare/backend/domain/funding/repository/query/FundingRepositoryCustom.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/repository/query/FundingRepositoryCustom.java
@@ -1,9 +1,16 @@
 package org.kakaoshare.backend.domain.funding.repository.query;
 
 
+import java.util.List;
 import java.util.Optional;
 import org.kakaoshare.backend.domain.funding.entity.Funding;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface FundingRepositoryCustom {
     Optional<Funding> findByIdAndMemberId(Long fundingId, Long memberId);
+
+    List<Funding> findAllByMemberId(Long memberId);
+
+    Slice<Funding> findFundingByMemberIdWithSlice(Long memberId, Pageable pageable);
 }
