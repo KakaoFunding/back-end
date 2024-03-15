70160e3d1b2880499c3cb131699554ac2aac7e4d
hwan2-99 <thh1233123@gmail.com>
2024-03-15 23:57:27 +0900

    [feat] 펀딩 전채 조회기능 구현 및 slice화 하여 조회기능 구현체 구현 (KakaoFunding#47)

--- src/main/java/org/kakaoshare/backend/domain/funding/repository/query/FundingRepositoryCustomImpl.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/repository/query/FundingRepositoryCustomImpl.java
@@ -1,10 +1,14 @@
 package org.kakaoshare.backend.domain.funding.repository.query;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.kakaoshare.backend.domain.funding.entity.Funding;
 import org.kakaoshare.backend.domain.funding.entity.QFunding;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 
 @RequiredArgsConstructor
 public class FundingRepositoryCustomImpl implements FundingRepositoryCustom {
@@ -18,5 +22,32 @@ public class FundingRepositoryCustomImpl implements FundingRepositoryCustom {
                         .and(QFunding.funding.member.memberId.eq(memberId)))
                 .fetchOne();
         return Optional.ofNullable(funding);
+    }
+
+    @Override
+    public List<Funding> findAllByMemberId(Long memberId) {
+        return queryFactory
+                .selectFrom(QFunding.funding)
+                .where(QFunding.funding.member.memberId.eq(memberId))
+                .fetch();
+    }
+
+    @Override
+    public Slice<Funding> findFundingByMemberIdWithSlice(Long memberId, Pageable pageable) {
+        List<Funding> content = queryFactory
+                .selectFrom(QFunding.funding)
+                .where(QFunding.funding.member.memberId.eq(memberId))
+                .orderBy(QFunding.funding.fundingId.desc()) // 예시로 ID 내림차순 정렬
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1) // 다음 페이지 존재 여부 확인을 위해 1 추가
+                .fetch();
+
+        boolean hasNext = false;
+        if (content.size() > pageable.getPageSize()) {
+            content.remove(content.size() - 1);
+            hasNext = true;
+        }
+
+        return new SliceImpl<>(content, pageable, hasNext);
     }
 }
