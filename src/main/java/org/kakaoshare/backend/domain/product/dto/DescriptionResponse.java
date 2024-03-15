50b30a1f55982df9e1d4f8e9f588290ae6a2988c
hwan2-99 <thh1233123@gmail.com>
2024-03-15 16:44:13 +0900

    [refactor] 응답 dto 클래스에 썸네일 필드 추가 (KakaoFunding#47)

--- src/main/java/org/kakaoshare/backend/domain/product/dto/DescriptionResponse.java
+++ src/main/java/org/kakaoshare/backend/domain/product/dto/DescriptionResponse.java
@@ -1,26 +1,27 @@
 package org.kakaoshare.backend.domain.product.dto;
 
+import java.math.BigDecimal;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import org.kakaoshare.backend.domain.brand.entity.Brand;
 import org.kakaoshare.backend.domain.option.entity.Option;
 import org.kakaoshare.backend.domain.product.entity.Product;
 import org.kakaoshare.backend.domain.product.entity.ProductDescriptionPhoto;
-
-import java.util.List;
+import org.kakaoshare.backend.domain.product.entity.ProductThumbnail;
 
 @Getter
 @Builder
 public class DescriptionResponse {
     private final Long productId;
     private final String name;
-    private final Long price;
+    private final BigDecimal price;
     private final String type;
     private final String description;
     private final List<ProductDescriptionPhoto> descriptionPhotos;
-    private final Boolean hasPhoto;
     private final String productName;
     private final List<Option> options;
+    private final List<ProductThumbnail> productThumbnails;
     private final Brand brand;
 
     public static DescriptionResponse from(final Product product) {
@@ -30,10 +31,9 @@ public class DescriptionResponse {
                 .price(product.getPrice())
                 .type(product.getType())
                 .description(product.getProductDetail().getDescription())
-                .descriptionPhotos(null)//TODO 2024 03 21 16:33:40 : 추후 수정 필요
-                .hasPhoto(product.getProductDetail().getHasPhoto())
+                .descriptionPhotos(product.getProductDescriptionPhotos())
                 .productName(product.getProductDetail().getProductName())
-                .options(null)//TODO 2024 03 21 16:33:50 : 추후 수정 필요
+                .options(product.getOptions())
                 .brand(product.getBrand())
                 .build();
     }
