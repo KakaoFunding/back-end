86fed292cce0cf833260cdd4fd8147d4d693061f
kmw2378 <rlarla677@gmail.com>
2024-03-16 21:36:36 +0900

    [test]BrandFixture 추가 ${#55} 테스트에서 Brand 엔티티를 만들 때 사용되는 Fixture 열거체 추가

---
+++ src/test/java/org/kakaoshare/backend/fixture/BrandFixture.java
@@ -0,0 +1,24 @@
+package org.kakaoshare.backend.fixture;
+
+import org.kakaoshare.backend.domain.brand.entity.Brand;
+
+public enum BrandFixture {
+    STARBUCKS("스타벅스");
+
+    private final String name;
+
+    BrandFixture(final String name) {
+        this.name = name;
+    }
+
+    public Brand 생성() {
+        return 생성(null);
+    }
+
+    public Brand 생성(final Long brandId) {
+        return Brand.builder()
+                .brandId(brandId)
+                .name(name)
+                .build();
+    }
+}
