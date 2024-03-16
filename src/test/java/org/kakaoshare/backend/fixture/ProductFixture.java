726e5378d39201369d83499ef3385c5b19c340ee
hwan2-99 <thh1233123@gmail.com>
2024-03-16 16:08:38 +0900

    [test] 상품 fixture 브랜드 포함 생성 메소드 구현 (KakaoFunding#47)

--- src/test/java/org/kakaoshare/backend/fixture/ProductFixture.java
+++ src/test/java/org/kakaoshare/backend/fixture/ProductFixture.java
@@ -1,49 +1,38 @@
 package org.kakaoshare.backend.fixture;
 
+import java.math.BigDecimal;
 import org.kakaoshare.backend.domain.brand.entity.Brand;
 import org.kakaoshare.backend.domain.product.entity.Product;
 
 public enum ProductFixture {
-    TEST_PRODUCT("Test Product", 999L, "Test Type"),
-    CAKE("케이크", 10_000L, "Dessert"),
-    COFFEE("커피", 3_000L, "Beverage");
+    TEST_PRODUCT("Test Product", new BigDecimal("999.99"), "Test Type");
 
     private final String name;
-    private final Long price;
+    private final BigDecimal price;
     private final String type;
 
-    ProductFixture(String name, Long price, String type) {
+    ProductFixture(String name, BigDecimal price, String type) {
         this.name = name;
         this.price = price;
         this.type = type;
     }
 
     public Product 생성() {
-        return 생성(null);
-    }
-
-    public Product 생성(final Long productId) {
-        return 생성(productId, null);
-    }
-
-    public Product 생성(final Long productId,
-                      final Brand brand) {
-        return 생성(productId, brand, this.price);
-    }
-
-    public Product 생성(final Long productId,
-                      final Brand brand,
-                      final BigDecimal price) {
         return Product.builder()
-                .productId(productId)
-                .brand(brand)
                 .name(name)
                 .price(price)
                 .type(type)
                 .build();
     }
 
-    public Product 가격_설정_생성(final BigDecimal price) {
-        return 생성(null, null, price);
+    public Product 브랜드포함생성(Brand brand) {
+        return Product.builder()
+                .name(name)
+                .price(price)
+                .type(type)
+                .brand(brand)
+                .build();
     }
+
+
 }
