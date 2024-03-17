7341e21f63c4e35d745f07728a5be30075536080
hwan2-99 <thh1233123@gmail.com>
2024-03-17 22:59:03 +0900

    [refactor] 미사용 어노테이션 삭제 (KakaoFunding#47)

--- src/main/java/org/kakaoshare/backend/domain/funding/controller/FundingController.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/controller/FundingController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/funding")
