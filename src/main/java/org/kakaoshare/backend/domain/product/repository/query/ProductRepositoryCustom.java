0675f1eb36c4179050d5687d1075e3f7c6be8f7e
kmw2378 <rlarla677@gmail.com>
2024-03-19 17:28:20 +0900

    [feat]ProductRepositoryCustom에 검색 관련 추상 메서드 추가 ${#45}

--- src/main/java/org/kakaoshare/backend/domain/product/repository/query/ProductRepositoryCustom.java
+++ src/main/java/org/kakaoshare/backend/domain/product/repository/query/ProductRepositoryCustom.java
@@ -4,12 +4,18 @@ import org.kakaoshare.backend.domain.product.dto.DescriptionResponse;
 import org.kakaoshare.backend.domain.product.dto.DetailResponse;
 import org.kakaoshare.backend.domain.product.dto.Product4DisplayDto;
 import org.kakaoshare.backend.domain.product.dto.ProductDto;
+import org.kakaoshare.backend.domain.search.dto.SimpleBrandProductDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
+import java.util.List;
 
 public interface ProductRepositoryCustom {
     Page<Product4DisplayDto> findAllByCategoryId(Long categoryId, Pageable pageable);
     Page<ProductDto> findAllByBrandId(final Long brandId, final Pageable pageable);
     DescriptionResponse findProductWithDetailsAndPhotos(Long productId);
     DetailResponse findProductDetail(Long productId);
+    Slice<Product4DisplayDto> findBySearchConditions(final String keyword, final Integer minPrice, final Integer maxPrice, final List<String> categories, final Pageable pageable);
+    Slice<SimpleBrandProductDto> findBySearchConditionsGroupByBrand(final String keyword, final Pageable pageable);
 }
