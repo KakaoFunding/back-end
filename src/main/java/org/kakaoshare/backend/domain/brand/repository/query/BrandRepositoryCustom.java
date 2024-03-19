c91ecbe117fccf19fdfc1b785a4d37822d2e2ef8
kmw2378 <rlarla677@gmail.com>
2024-03-19 17:27:18 +0900

    [feat]BrandRepositoryCustom에 findBySearchConditions() 추상 메서드 추가 ${#45} 브랜드 검색 결과를 가져올 때 사용되는 메서드 추가

--- src/main/java/org/kakaoshare/backend/domain/brand/repository/query/BrandRepositoryCustom.java
+++ src/main/java/org/kakaoshare/backend/domain/brand/repository/query/BrandRepositoryCustom.java
@@ -4,6 +4,9 @@ import org.kakaoshare.backend.domain.brand.dto.SimpleBrandDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface BrandRepositoryCustom {
     Page<SimpleBrandDto> findAllSimpleBrandByCategoryId(Long categoryId, Pageable pageable);
+    List<SimpleBrandDto> findBySearchConditions(final String keyword, final Pageable pageable);
 }
