39c068d2a0d41a72063a4ceea046f1bb7a213ae1
kmw2378 <rlarla677@gmail.com>
2024-03-22 21:40:03 +0900

    [feat]TestJpaAdultingConfig 추가 ${#45} 테스트 환경에서 @CreateDate, @LastModifiedDate 을 적용하기 위해 설정 클래스 추가

---
+++ src/test/java/org/kakaoshare/backend/common/TestJpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package org.kakaoshare.backend.common;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@TestConfiguration
+@EnableJpaAuditing
+public class TestJpaAuditingConfig {
+}
