d850c6556ea2f147ce34768bc6ee7273b9d74a30
YeaChan05 <qkenrdl05@gmail.com>
2024-03-24 14:14:56 +0900

    [fix]테이블 관계 변경에 따른 로직 수정 (#66)
    상품, 브랜드, 카테고리의 관계 변경에 따라 쿼리 요청 메서드 수정

--- src/main/java/org/kakaoshare/backend/domain/brand/repository/query/BrandRepositoryCustomImpl.java
+++ src/main/java/org/kakaoshare/backend/domain/brand/repository/query/BrandRepositoryCustomImpl.java
@@ -2,12 +2,12 @@ package org.kakaoshare.backend.domain.brand.repository.query;
 
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.kakaoshare.backend.common.util.sort.SortableRepository;
 import org.kakaoshare.backend.domain.brand.dto.QSimpleBrandDto;
 import org.kakaoshare.backend.domain.brand.dto.SimpleBrandDto;
+import org.kakaoshare.backend.domain.category.entity.QCategory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -26,26 +26,46 @@ import static org.kakaoshare.backend.domain.product.entity.QProduct.product;
 @RequiredArgsConstructor
 public class BrandRepositoryCustomImpl implements BrandRepositoryCustom, SortableRepository {
     private final JPAQueryFactory queryFactory;
-
+    
+    
+    private BooleanExpression categoryTypeOf(final Long categoryId) {
+        BooleanExpression isChildCategory = product.category.categoryId.eq(categoryId);
+        return isChildCategory.or(product.category.parent.categoryId.eq(categoryId));
+    }
+    
     @Override
-    public Page<SimpleBrandDto> findAllSimpleBrandByCategoryId(final Long categoryId,
-                                                               final Pageable pageable) {
-        final List<SimpleBrandDto> fetch = queryFactory
+    public Page<SimpleBrandDto> findAllSimpleBrandByCategoryId(final Long categoryId, final Pageable pageable) {
+        QCategory child = new QCategory("child");
+        BooleanExpression isParentCategory = category.parent.categoryId.isNull().and(category.categoryId.eq(categoryId));
+        
+        List<SimpleBrandDto> fetch = queryFactory
                 .select(new QSimpleBrandDto(
                         brand.brandId,
                         brand.name,
                         brand.iconPhoto))
                 .from(brand)
-                .where(categoryIdEqualTo(categoryId))
+                .join(brand.products, product)
+                .leftJoin(product.category, child)
+                .on(isParentCategory)
+                .where(isCondition(categoryId))
+                .distinct()
                 .orderBy(getOrderSpecifiers(pageable))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
         
+        Long total =queryFactory
+                .select(brand.countDistinct())
+                .from(brand)
+                .join(brand.products, product)
+                .leftJoin(product.category, child)
+                .on(isParentCategory)
+                .where(isCondition(categoryId))
+                .fetchOne();
         
-        return new PageImpl<>(fetch, pageable, fetch.size());
+        return new PageImpl<>(fetch, pageable, total);
     }
-
+    
     @Override
     public List<SimpleBrandDto> findBySearchConditions(final String keyword, final Pageable pageable) {
         return queryFactory.select(getSimpleBrandDto())
@@ -57,14 +77,14 @@ public class BrandRepositoryCustomImpl implements BrandRepositoryCustom, Sortabl
                 .limit(pageable.getPageSize())
                 .fetch();
     }
-
+    
     private QSimpleBrandDto getSimpleBrandDto() {
         return new QSimpleBrandDto(
                 brand.brandId,
                 brand.name,
                 brand.iconPhoto);
     }
-
+    
     private BooleanExpression categoryIdEqualTo(final Long categoryId) {
         BooleanExpression isParentCategory = brand.category
                 .in(JPAExpressions
