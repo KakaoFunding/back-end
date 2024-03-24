3b5981ce363d3a9e54ffac026ca0fa86719d7213
YeaChan05 <qkenrdl05@gmail.com>
2024-03-24 14:17:17 +0900

    [refactor]사용하지 않는 함수 삭제 (#66)
    사용하지 않는 함수 삭제

--- src/main/java/org/kakaoshare/backend/domain/brand/repository/BrandRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/brand/repository/BrandRepository.java
@@ -4,9 +4,6 @@ import org.kakaoshare.backend.domain.brand.entity.Brand;
 import org.kakaoshare.backend.domain.brand.repository.query.BrandRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 
 public interface BrandRepository extends JpaRepository<Brand, Long> , BrandRepositoryCustom {
-    List<Brand> findByCategory_CategoryId(Long categoryId);
 }
