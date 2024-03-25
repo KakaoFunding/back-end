db1d32b44d1672e03dbc0f8861b962d6f96271df
YeaChan05 <qkenrdl05@gmail.com>
2024-03-25 16:09:38 +0900

    [remove] 불필요한 컴포넌트 삭제 (#66)
    불필요한 데이터 초기화 컴포넌트 삭제

--- src/main/java/org/kakaoshare/backend/domain/member/repository/MemberRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/member/repository/MemberRepository.java
@@ -4,6 +4,8 @@ import org.kakaoshare.backend.domain.member.entity.Member;
 import org.kakaoshare.backend.domain.member.repository.query.MemberRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
     Optional<Member> findByProviderId(String providerId);
 }
