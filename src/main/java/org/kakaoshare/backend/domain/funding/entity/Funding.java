2608f7cdac6b749bedfc121b364d203da694a90d
YeaChan05 <qkenrdl05@gmail.com>
2024-03-26 11:40:58 +0900

    [refactor]컬럼 초기값 리팩토링 (#66)
    컬럼 초기값을 생성자가 아닌 컬럼 설정으로 변경

--- src/main/java/org/kakaoshare/backend/domain/funding/entity/Funding.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/entity/Funding.java
@@ -11,6 +11,7 @@ import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 import org.kakaoshare.backend.domain.base.entity.BaseTimeEntity;
 import org.kakaoshare.backend.domain.member.entity.Member;
 import org.kakaoshare.backend.domain.product.entity.Product;
@@ -23,36 +24,37 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Funding extends BaseTimeEntity {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long fundingId;
-
+    
     @Column
     private String status;
-
+    
     @Column(nullable = false)
     private LocalDate expiredAt;
-
+    
     @Column(nullable = false, precision = 12, scale = 2)
     private Long goalAmount;
-
+    
+    @ColumnDefault("0")
     @Column(nullable = false, precision = 12, scale = 2)
-    private Long accumulateAmount;
-
+    private Long accumulateAmount = 0L;
+    
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
-
+    
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
+    
     public Funding(Member member, Product product, Long goalAmount, LocalDate expiredAt) {
         this.member = member;
         this.product = product;
         this.goalAmount = goalAmount;
         this.expiredAt = expiredAt;
-        this.accumulateAmount = 0L; // 초기값 설정
         this.status = "ACTIVE"; // 초기 상태 설정
     }
 }
