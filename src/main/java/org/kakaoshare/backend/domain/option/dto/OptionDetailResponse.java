7b1fc86659c325bc5b6bdcea2ea0f7b3f98479a9
hwan2-99 <thh1233123@gmail.com>
2024-03-28 18:10:11 +0900

    [refactor] 미사용 어노테이션 삭제(KakaoFunding#70)

--- src/main/java/org/kakaoshare/backend/domain/option/dto/OptionDetailResponse.java
+++ src/main/java/org/kakaoshare/backend/domain/option/dto/OptionDetailResponse.java
@@ -1,8 +1,6 @@
 package org.kakaoshare.backend.domain.option.dto;
 
-import lombok.Getter;
 
-@Getter
 public record OptionDetailResponse(Long id, String photo, String name) {
     public OptionDetailResponse {
     }
