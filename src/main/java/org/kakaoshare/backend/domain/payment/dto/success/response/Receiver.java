070cbb6635da1b044a733145e0bf8073f5978d16
kmw2378 <rlarla677@gmail.com>
2024-04-01 00:33:17 +0900

    [refactor]Receiver empty() 메서드 삭제, from() 메서드 추가 ${#69}

--- src/main/java/org/kakaoshare/backend/domain/payment/dto/success/response/Receiver.java
+++ src/main/java/org/kakaoshare/backend/domain/payment/dto/success/response/Receiver.java
@@ -1,7 +1,9 @@
 package org.kakaoshare.backend.domain.payment.dto.success.response;
 
+import org.kakaoshare.backend.domain.member.entity.Member;
+
 public record Receiver(String name, String photoUrl) {
-    public static Receiver empty() {
-        return new Receiver("이름 없음", null);
+    public static Receiver from(final Member member) {
+        return new Receiver(member.getName(), null);    // TODO: 3/28/24 프로필 사진 URL이 없어 null로 대체
     }
 }
