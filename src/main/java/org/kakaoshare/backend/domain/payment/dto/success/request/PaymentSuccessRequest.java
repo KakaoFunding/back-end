4983f6febba1c3d5e6357ee12ae2c3d734b8828e
kmw2378 <rlarla677@gmail.com>
2024-04-01 00:36:15 +0900

    [refactor]PaymentSuccessRequest의 details 필드 삭제 ${#69} 결제 승인 요청 시 주문 내역을 Redis에 저장하므로 별도로 전달받지 않아 details 필드 삭제

--- src/main/java/org/kakaoshare/backend/domain/payment/dto/success/request/PaymentSuccessRequest.java
+++ src/main/java/org/kakaoshare/backend/domain/payment/dto/success/request/PaymentSuccessRequest.java
@@ -1,8 +1,4 @@
 package org.kakaoshare.backend.domain.payment.dto.success.request;
 
-import org.kakaoshare.backend.domain.payment.dto.OrderDetail;
-
-import java.util.List;
-
-public record PaymentSuccessRequest(List<OrderDetail> details, String orderNumber, String pgToken, String tid) {
+public record PaymentSuccessRequest(String orderNumber, String pgToken, String tid) {
 }
