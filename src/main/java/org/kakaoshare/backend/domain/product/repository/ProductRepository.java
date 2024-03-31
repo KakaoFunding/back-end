6b36355e8463081d90a8fc853244c5100e5a1749
kmw2378 <rlarla677@gmail.com>
2024-04-01 00:32:39 +0900

    [feat]ProductRepository에 메서드 추가 ${#69} 특정 상품의 총 가격과 특정 상품의 브랜드 및 상품 명, 가격을 가져오는 메서드 추가

--- src/main/java/org/kakaoshare/backend/domain/product/repository/ProductRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/product/repository/ProductRepository.java
@@ -1,9 +1,21 @@
 package org.kakaoshare.backend.domain.product.repository;
 
+import org.kakaoshare.backend.domain.product.dto.ProductSummaryResponse;
 import org.kakaoshare.backend.domain.product.entity.Product;
 import org.kakaoshare.backend.domain.product.repository.query.ProductRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
+    @Query("SELECT SUM(p.price) FROM Product p " +
+            "WHERE p.productId IN :productIds")
+    Long findTotalPriceByIds(List<Long> productIds);
+
+    @Query("SELECT NEW org.kakaoshare.backend.domain.product.dto.ProductSummaryResponse(p.brandName, p.name, p.price) " +
+            "FROM Product p " +
+            "WHERE p.productId =:productId")
+    ProductSummaryResponse findAllProductSummaryById(final Long productId);
 }
