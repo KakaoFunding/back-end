07040e1b2c4e1559c09e018bc1a8bb4f9dc6b1ba
kmw2378 <rlarla677@gmail.com>
2024-04-01 00:30:55 +0900

    [refactor]PaymentReadyResponse의 orderDetails 필드 삭제 ${#69} 주문 내역을 Redis에 저장하므로 기존 주문 내역을 저장하는 필드 삭제

--- src/main/java/org/kakaoshare/backend/domain/payment/dto/ready/response/PaymentReadyResponse.java
+++ src/main/java/org/kakaoshare/backend/domain/payment/dto/ready/response/PaymentReadyResponse.java
@@ -1,9 +1,4 @@
 package org.kakaoshare.backend.domain.payment.dto.ready.response;
 
-import org.kakaoshare.backend.domain.payment.dto.OrderDetail;
-
-import java.util.List;
-
-
-public record PaymentReadyResponse(String tid, List<OrderDetail> details, String redirectUrl, String orderNumber) {
+public record PaymentReadyResponse(String tid, String redirectUrl, String orderNumber) {
 }
