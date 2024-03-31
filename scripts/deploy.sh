4899043efc442673ea70554f7e080d0429cbc00d
hwan2-99 <thh1233123@gmail.com>
2024-04-01 00:27:18 +0900

    [fix] deploy 쉘 스크릅트 우분투에서 리눅스로 수정(KakaoFunding#71)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/home/ubuntu/kakao-funding
+REPOSITORY=/home/ec2-user/back-end
 cd $REPOSITORY
 
-APP_NAME=kakao-funding
+APP_NAME=back-end
+
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -fl $APP_NAME | grep java | awk '{print $1}')
 
-if [ -z "$CURRENT_PID" ]
-then
+if [ -z "$CURRENT_PID" ]; then
   echo "> 종료할 애플리케이션이 없습니다."
 else
   echo "> kill -15 $CURRENT_PID"
@@ -19,4 +19,4 @@ else
 fi
 
 echo "> Deploy - $JAR_PATH "
-nohup java -jar $JAR_PATH > kakao-funding.log 2>&1 < /dev/null &
+nohup java -jar $JAR_PATH > back-end.log 2>&1 < /dev/null &
