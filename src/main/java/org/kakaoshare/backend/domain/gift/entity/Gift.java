ab2be9a74cc5a36b2a82df59202f64107ebab874
YeaChan05 <qkenrdl05@gmail.com>
2024-04-06 22:42:44 +0900

    [fix]열거체 방지를 위해 DDL 수정 (#78)
    DDL이 열거체 사용시 validate 과정에서 발생하는 오류를 잡기 위해 `columnDefinition`사용

--- src/main/java/org/kakaoshare/backend/domain/gift/entity/Gift.java
+++ src/main/java/org/kakaoshare/backend/domain/gift/entity/Gift.java
@@ -30,7 +30,7 @@ public class Gift extends BaseTimeEntity {
 
     @Builder.Default
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = false,columnDefinition = "varchar(255)")
     private GiftStatus status = NOT_USED;
 
     @Column
@@ -38,7 +38,7 @@ public class Gift extends BaseTimeEntity {
 
     @Column
     private String messagePhoto;
-
+    
     @Column(nullable = false)
     private LocalDateTime expiredAt;
 
