4eaa20089c83d089a00cb39bd5a83476c9ecaa29
hwan2-99 <thh1233123@gmail.com>
2024-04-09 20:49:58 +0900

    [feat] 받은선물 조회 api 구현 (KakaoFunding#83)

--- src/main/java/org/kakaoshare/backend/domain/gift/controller/GiftController.java
+++ src/main/java/org/kakaoshare/backend/domain/gift/controller/GiftController.java
@@ -1,7 +1,13 @@
 package org.kakaoshare.backend.domain.gift.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.kakaoshare.backend.domain.gift.dto.GiftSliceResponse;
 import org.kakaoshare.backend.domain.gift.service.GiftService;
+import org.kakaoshare.backend.jwt.util.LoggedInMember;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class GiftController {
     private final GiftService giftService;
+
+    @GetMapping("giftBox")
+    public ResponseEntity<?> getGiftBox(@LoggedInMember final String providerId,
+                                        @PageableDefault(size = 20) Pageable pageable) {
+        GiftSliceResponse giftSliceResponse = giftService.getMyGiftBox(providerId, pageable);
+        return ResponseEntity.ok(giftSliceResponse);
+
+    }
 }
