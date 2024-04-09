ca0d9a1a8a3299086ba0c96efd31f1a92c9bcbd9
hwan2-99 <thh1233123@gmail.com>
2024-04-09 20:46:14 +0900

    [feat] 받은선물 조회 서비스 구현 (KakaoFunding#83)

--- src/main/java/org/kakaoshare/backend/domain/gift/service/GiftService.java
+++ src/main/java/org/kakaoshare/backend/domain/gift/service/GiftService.java
@@ -1,7 +1,15 @@
 package org.kakaoshare.backend.domain.gift.service;
 
 import lombok.RequiredArgsConstructor;
+import org.kakaoshare.backend.common.dto.PageResponse;
+import org.kakaoshare.backend.domain.gift.dto.GiftResponse;
+import org.kakaoshare.backend.domain.gift.dto.GiftSliceResponse;
+import org.kakaoshare.backend.domain.gift.entity.Gift;
 import org.kakaoshare.backend.domain.gift.repository.GiftRepository;
+import org.kakaoshare.backend.domain.member.entity.Member;
+import org.kakaoshare.backend.domain.member.repository.MemberRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,4 +18,21 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class GiftService {
     private final GiftRepository giftRepository;
+    private final MemberRepository memberRepository;
+
+    public GiftSliceResponse getMyGiftBox(String providerId, Pageable pageable) {
+        Member member = findMemberByProviderId(providerId);
+        Page<GiftResponse> giftResponses = giftRepository.findGiftsByMemberId(member.getMemberId(), pageable);
+
+        PageResponse<GiftResponse> pageResponse = (PageResponse<GiftResponse>) PageResponse.from(giftResponses);
+
+        return GiftSliceResponse.builder()
+                .pageResponse(pageResponse)
+                .build();
+    }
+
+    private Member findMemberByProviderId(String providerId) {
+        return memberRepository.findByProviderId(providerId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid providerId"));
+    }
 }
