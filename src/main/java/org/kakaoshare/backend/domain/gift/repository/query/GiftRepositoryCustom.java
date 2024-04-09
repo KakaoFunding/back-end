0ed78e3ba18298cc086a3d951837d9421d3194ed
hwan2-99 <thh1233123@gmail.com>
2024-04-09 20:14:26 +0900

    [feat] 선물내역 조회 쿼리 구현 (KakaoFunding#83)

--- src/main/java/org/kakaoshare/backend/domain/gift/repository/query/GiftRepositoryCustom.java
+++ src/main/java/org/kakaoshare/backend/domain/gift/repository/query/GiftRepositoryCustom.java
@@ -1,4 +1,9 @@
 package org.kakaoshare.backend.domain.gift.repository.query;
 
+import org.kakaoshare.backend.domain.gift.dto.GiftResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface GiftRepositoryCustom {
+    Page<GiftResponse> findGifts(Pageable pageable);
 }
--- src/main/java/org/kakaoshare/backend/domain/gift/repository/query/GiftRepositoryCustomImpl.java
+++ src/main/java/org/kakaoshare/backend/domain/gift/repository/query/GiftRepositoryCustomImpl.java
@@ -1,11 +1,41 @@
 package org.kakaoshare.backend.domain.gift.repository.query;
 
+import static org.kakaoshare.backend.domain.gift.entity.QGift.gift;
+
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.kakaoshare.backend.domain.gift.dto.GiftResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class GiftRepositoryCustomImpl implements GiftRepositoryCustom{
     private final JPAQueryFactory queryFactory;
+    @Override
+    public Page<GiftResponse> findGifts(Pageable pageable) {
+        List<GiftResponse> content = queryFactory
+                .select(Projections.constructor(GiftResponse.class,
+                        gift.giftId,
+                        gift.expiredAt,
+                        gift.receipt.recipient.name.as("recipientName"),
+                        gift.receipt.product.name.as("productName"),
+                        gift.receipt.product.productThumbnails.any().thumbnailUrl.as("productThumbnail"),
+                        gift.receipt.product.brandName))
+                .from(gift)
+                .leftJoin(gift.receipt).fetchJoin()
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        long total = queryFactory
+                .selectFrom(gift)
+                .fetchCount();
+
+        return new PageImpl<>(content, pageable, total);
+    }
 }
