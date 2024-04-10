972836033d44cad8537825748aee5a065c86879b
kmw2378 <rlarla677@gmail.com>
2024-04-10 15:04:03 +0900

    [refactor]OptionRepository와 OptionRepositoryCustom 상속관계 제거 ${#81}

--- src/main/java/org/kakaoshare/backend/domain/option/repository/OptionRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/option/repository/OptionRepository.java
@@ -1,14 +1,13 @@
 package org.kakaoshare.backend.domain.option.repository;
 
 import org.kakaoshare.backend.domain.option.entity.Option;
-import org.kakaoshare.backend.domain.option.repository.query.OptionRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 
-public interface OptionRepository extends JpaRepository<Option, Long>, OptionRepositoryCustom {
+public interface OptionRepository extends JpaRepository<Option, Long> {
     @Query("SELECT od.option " +
             "FROM OptionDetail od " +
             "LEFT JOIN od.option " +
