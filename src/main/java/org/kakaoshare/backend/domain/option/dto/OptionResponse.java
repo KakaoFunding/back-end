c3933412005cf9ad9c170550ce1876e7aa05ab0b
hwan2-99 <thh1233123@gmail.com>
2024-04-11 01:34:05 +0900

    [refactor] 옵션 응답 dto 클래스 모든 필드가 필요한 생성자 추가(KakaoFunding#70)

--- src/main/java/org/kakaoshare/backend/domain/option/dto/OptionResponse.java
+++ src/main/java/org/kakaoshare/backend/domain/option/dto/OptionResponse.java
@@ -1,6 +1,7 @@
 package org.kakaoshare.backend.domain.option.dto;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
 public class OptionResponse {
     private final Long optionsId;
     private final String name;
