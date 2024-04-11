5c7007d5939701e4c55e8c00cb938b8e30940639
hwan2-99 <thh1233123@gmail.com>
2024-04-11 14:05:14 +0900

    [refactor] 선물목록 조회 서비스 분기 추가(KakaoFunding#83)

--- src/main/java/org/kakaoshare/backend/domain/gift/service/GiftService.java
+++ src/main/java/org/kakaoshare/backend/domain/gift/service/GiftService.java
@@ -5,6 +5,7 @@ import org.kakaoshare.backend.common.dto.PageResponse;
 import org.kakaoshare.backend.domain.gift.dto.GiftResponse;
 import org.kakaoshare.backend.domain.gift.dto.GiftSliceResponse;
 import org.kakaoshare.backend.domain.gift.entity.Gift;
+import org.kakaoshare.backend.domain.gift.entity.GiftStatus;
 import org.kakaoshare.backend.domain.gift.repository.GiftRepository;
 import org.kakaoshare.backend.domain.member.entity.Member;
 import org.kakaoshare.backend.domain.member.repository.MemberRepository;
@@ -20,9 +21,15 @@ public class GiftService {
     private final GiftRepository giftRepository;
     private final MemberRepository memberRepository;
 
-    public GiftSliceResponse getMyGiftBox(String providerId, Pageable pageable) {
+    public GiftSliceResponse getMyGiftBox(String providerId, Pageable pageable, GiftStatus status) {
         Member member = findMemberByProviderId(providerId);
-        Page<GiftResponse> giftResponses = giftRepository.findGiftsByMemberId(member.getMemberId(), pageable);
+        Page<GiftResponse> giftResponses;
+
+        if (status == null || status == GiftStatus.NOT_USED) {
+            giftResponses = giftRepository.findGiftsByMemberIdAndStatus(member.getMemberId(), GiftStatus.NOT_USED, pageable);
+        } else {
+            giftResponses = giftRepository.findGiftsByMemberIdAndOtherStatuses(member.getMemberId(), pageable);
+        }
 
         PageResponse<GiftResponse> pageResponse = (PageResponse<GiftResponse>) PageResponse.from(giftResponses);
 
