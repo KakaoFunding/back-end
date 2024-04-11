1c1ce7c2b827b4a8ca40f0266e2a7726acdd409b
hwan2-99 <thh1233123@gmail.com>
2024-04-12 00:12:24 +0900

    [feat] 선물 관리 서비스, 컨트롤러 구현(KakaoFunding#88)

---
+++ src/main/java/org/kakaoshare/backend/domain/gift/controller/GiftController.java
@@ -0,0 +1,13 @@
+package org.kakaoshare.backend.domain.gift.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
+public class GiftController {
+}
---
+++ src/main/java/org/kakaoshare/backend/domain/gift/service/GIftService.java
@@ -0,0 +1,11 @@
+package org.kakaoshare.backend.domain.gift.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class GIftService {
+}
