8fda966f753f20fae80115e41e76ae8f08245bd0
hwan2-99 <thh1233123@gmail.com>
2024-04-12 00:49:24 +0900

    [feat] 선물 상세조회 서비스 구현(KakaoFunding#88)

--- src/main/java/org/kakaoshare/backend/domain/gift/repository/query/GiftRepositoryCustom.java
+++ src/main/java/org/kakaoshare/backend/domain/gift/repository/query/GiftRepositoryCustom.java
@@ -1,7 +1,6 @@
 package org.kakaoshare.backend.domain.gift.repository.query;
 
 import org.kakaoshare.backend.domain.gift.dto.GiftDetailResponse;
-import org.kakaoshare.backend.domain.product.dto.DetailResponse;
 
 public interface GiftRepositoryCustom {
     GiftDetailResponse findGiftDetail(Long giftId);
---
+++ src/main/java/org/kakaoshare/backend/domain/gift/service/GiftService.java
@@ -0,0 +1,23 @@
+package org.kakaoshare.backend.domain.gift.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.kakaoshare.backend.domain.gift.dto.GiftDetailResponse;
+import org.kakaoshare.backend.domain.gift.repository.GiftRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class GiftService {
+    private final GiftRepository giftRepository;
+
+    public GiftDetailResponse getGiftDetail(Long giftId) {
+        GiftDetailResponse giftDetailResponse = giftRepository.findGiftDetail(giftId);
+        if (giftDetailResponse == null) {
+            throw new EntityNotFoundException("Gift not found with id: " + giftId);
+        }
+        return giftDetailResponse;
+    }
+}
