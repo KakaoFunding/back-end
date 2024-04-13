e9ee96c8535a8124893e030a01e71bce84fd1e55
YeaChan05 <qkenrdl05@gmail.com>
2024-04-13 15:45:20 +0900

    [feat] 위시 삭제 로직 구현 및 복구 로직 구현 (#89)
    위시 삭제 로직 구현 및 위시 등록 및 삭제 실패시 복구 로직 구현

--- src/main/java/org/kakaoshare/backend/domain/wish/service/WishService.java
+++ src/main/java/org/kakaoshare/backend/domain/wish/service/WishService.java
@@ -1,28 +1,31 @@
 package org.kakaoshare.backend.domain.wish.service;
 
-import jakarta.persistence.PersistenceException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.kakaoshare.backend.domain.member.entity.Member;
 import org.kakaoshare.backend.domain.member.exception.MemberErrorCode;
 import org.kakaoshare.backend.domain.member.exception.MemberException;
 import org.kakaoshare.backend.domain.member.repository.MemberRepository;
+import org.kakaoshare.backend.domain.product.dto.WishCancelEvent;
+import org.kakaoshare.backend.domain.product.dto.WishEvent;
 import org.kakaoshare.backend.domain.wish.dto.WishReservationEvent;
 import org.kakaoshare.backend.domain.wish.entity.Wish;
 import org.kakaoshare.backend.domain.wish.error.WishErrorCode;
 import org.kakaoshare.backend.domain.wish.error.exception.WishException;
 import org.kakaoshare.backend.domain.wish.repository.WishRepository;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.TransactionSystemException;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+import java.util.List;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class WishService {
@@ -32,40 +35,87 @@ public class WishService {
     @Async
     @Transactional(propagation = Propagation.REQUIRES_NEW)//TODO 2024 04 11 18:57:19 : 성능 저하 가능성 있음
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMPLETION)
-    @Retryable(retryFor = {
-            DataAccessException.class,
-            DataIntegrityViolationException.class,
-            TransactionSystemException.class,
-            PersistenceException.class
-    }, maxAttempts = 2, backoff = @Backoff(delay = 5000))
+    @Retryable(
+            retryFor = WishException.class,
+            maxAttempts = 2,
+            backoff = @Backoff(delay = 5000),
+            recover = "recoverReservation",
+            notRecoverable = MemberException.class
+    )
     public void handleWishReservation(WishReservationEvent event) {
         Member member = getMember(event.getProviderId());
         
-        if (isAlreadyRegistered(event, member)) {
+        if (isRegistered(event, member)) {
             throw new WishException(WishErrorCode.DUPLICATED_WISH);
+        }
+        
+        Wish wish = createWish(event, member)
+                .checkIsPublic(event.getType());
+        
+        try {
+            wishRepository.save(wish);
+        } catch (RuntimeException e) {
+            throw new WishException(WishErrorCode.SAVING_FAILED);
+        }
+    }
+    
+    @Async
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMPLETION)
+    @Retryable(
+            retryFor = WishException.class,
+            maxAttempts = 2,
+            backoff = @Backoff(delay = 5000),
+            recover = "recoverCancel",
+            notRecoverable = MemberException.class
+    )
+    public void handleWishCancel(WishCancelEvent event) {
+        Member member = getMember(event.getProviderId());
+        
+        if (!isRegistered(event, member)) {
+            throw new WishException(WishErrorCode.NOT_FOUND);
         }
         
         Wish wish = createWish(event, member);
         
-        wishRepository.save(wish);
+        try {
+            wishRepository.delete(wish);
+        } catch (RuntimeException e) {
+            throw new WishException(WishErrorCode.REMOVING_FAILED);
+        }
     }
     
-    private boolean isAlreadyRegistered(final WishReservationEvent event, final Member member) {
-        return member.getWishes().stream()
+    @Recover
+    @Transactional
+    public void recoverReservation(RuntimeException e, WishReservationEvent event) {
+        log.error("wish reservation failed: {}", e.getMessage());
+        event.getProduct().decreaseWishCount();
+    }
+    
+    @Recover
+    @Transactional
+    public void recoverCancel(RuntimeException e, WishCancelEvent event) {
+        log.error("wish cancel failed: {}", e.getMessage());
+        event.getProduct().increaseWishCount();
+    }
+    
+    private boolean isRegistered(final WishEvent event, final Member member) {
+        List<Wish> wishes = member.getWishes();
+        return wishes != null && wishes.stream()
                 .anyMatch(wish -> wish.getProduct()
                         .getProductId()
                         .equals(event.getProduct().getProductId()));
     }
     
-    private Wish createWish(final WishReservationEvent event, final Member member) {
+    private Wish createWish(final WishEvent event, final Member member) {
         return Wish.builder()
                 .member(member)
                 .product(event.getProduct())
-                .isPublic(event.getType().isPublic())
                 .build();
     }
     
-    private Member getMember(final String providerId) {
+    @Transactional(readOnly = true)
+    public Member getMember(final String providerId) {
         return memberRepository.findByProviderId(providerId)
                 .orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND));
     }
