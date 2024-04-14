4d74c0cb3ad314edb794fefe6910dc440d668db9
kmw2378 <rlarla677@gmail.com>
2024-04-14 23:39:11 +0900

    [feat]ProductRepository.findTotalPriceByIds() 메서드 추가 ${#86}

--- src/main/java/org/kakaoshare/backend/domain/product/repository/ProductRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/product/repository/ProductRepository.java
@@ -6,8 +6,14 @@ import org.kakaoshare.backend.domain.product.repository.query.ProductRepositoryC
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
+    @Query("SELECT SUM(p.price) FROM Product p " +
+            "WHERE p.productId IN :productIds")
+    Long findTotalPriceByIds(List<Long> productIds);
+
     @Query("SELECT NEW org.kakaoshare.backend.domain.product.dto.ProductSummaryResponse(p.brandName, p.name, p.price) " +
             "FROM Product p " +
             "WHERE p.productId =:productId")
