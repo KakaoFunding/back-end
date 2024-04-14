2f2be8c521461745a73bb8485e70e11c57f3f657
kmw2378 <rlarla677@gmail.com>
2024-04-15 00:46:12 +0900

    [refactor]PaymentPreviewRequest 에 quantity 필드 추가 ${#86} 가격 계산 시 수량이 필요하여 quantity 필드 추가

--- src/main/java/org/kakaoshare/backend/domain/payment/dto/preview/PaymentPreviewRequest.java
+++ src/main/java/org/kakaoshare/backend/domain/payment/dto/preview/PaymentPreviewRequest.java
@@ -1,4 +1,4 @@
 package org.kakaoshare.backend.domain.payment.dto.preview;
 
-public record PaymentPreviewRequest(Long productId) {
+public record PaymentPreviewRequest(Long productId, Integer quantity) {
 }
