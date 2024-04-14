e9c5b4bcbfd28caf46a8d8a81eec2eec7402be9d
kmw2378 <rlarla677@gmail.com>
2024-04-15 00:28:20 +0900

    [feat]OptionDetailRepository.findNamesByIds() 추가 ${#86} optionDetailId에 해당하는 이름을 가져오는 메서드 추가

--- src/main/java/org/kakaoshare/backend/domain/option/repository/OptionDetailRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/option/repository/OptionDetailRepository.java
@@ -2,7 +2,11 @@ package org.kakaoshare.backend.domain.option.repository;
 
 import org.kakaoshare.backend.domain.option.entity.OptionDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 
 public interface OptionDetailRepository extends JpaRepository<OptionDetail, Long> {
+    @Query("SELECT od.name FROM OptionDetail od WHERE od.optionDetailId IN :ids")
+    List<String> findNamesByIds(final List<Long> ids);
 }
