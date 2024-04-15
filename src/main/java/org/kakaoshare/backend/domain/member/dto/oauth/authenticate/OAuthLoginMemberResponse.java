75b2139de1d5ff80a7bd3f4f96f193a106235a8d
kmw2378 <rlarla677@gmail.com>
2024-04-15 15:39:44 +0900

    [refactor]OAuthLoginMemberResponse 클래스를 레코드로 변환 ${#93}

--- src/main/java/org/kakaoshare/backend/domain/member/dto/oauth/authenticate/OAuthLoginMemberResponse.java
+++ src/main/java/org/kakaoshare/backend/domain/member/dto/oauth/authenticate/OAuthLoginMemberResponse.java
@@ -1,27 +1,9 @@
 package org.kakaoshare.backend.domain.member.dto.oauth.authenticate;
 
-import lombok.Builder;
-import lombok.Getter;
 import org.kakaoshare.backend.domain.member.dto.oauth.profile.OAuthProfile;
 
-@Getter
-public class OAuthLoginMemberResponse {
-    private String profileUrl;
-    private String name;
-
-    private OAuthLoginMemberResponse() {
-
-    }
-
-    @Builder
-    private OAuthLoginMemberResponse(final String profileUrl, final String name) {
-        this.profileUrl = profileUrl;
-        this.name = name;
-    }
-
+public record OAuthLoginMemberResponse(String profileUrl, String name) {
     public static OAuthLoginMemberResponse from(final OAuthProfile oAuthProfile) {
-        return OAuthLoginMemberResponse.builder()
-                .name(oAuthProfile.getName())
-                .build();
+        return new OAuthLoginMemberResponse(null, oAuthProfile.getName());
     }
 }
