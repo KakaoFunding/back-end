9da72e51ab80ab964fdde44acde338f567172e66
hwan2-99 <thh1233123@gmail.com>
2024-04-16 19:26:05 +0900

    [feat] 상품 전체 랭킹 조회컨트롤러 구현 (KakaoFunding#97)

--- src/main/java/org/kakaoshare/backend/domain/rank/controller/RankController.java
+++ src/main/java/org/kakaoshare/backend/domain/rank/controller/RankController.java
@@ -1,7 +1,12 @@
 package org.kakaoshare.backend.domain.rank.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.kakaoshare.backend.domain.rank.dto.RankResponse;
+import org.kakaoshare.backend.domain.rank.service.RankService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/ranking")
 public class RankController {
+    private final RankService rankService;
+    @GetMapping
+    public ResponseEntity<?> getTopRankedProducts() {
+        List<RankResponse> rankResponses = rankService.getTopRankedProducts();
+        return ResponseEntity.ok(rankResponses);
+    }
 }
