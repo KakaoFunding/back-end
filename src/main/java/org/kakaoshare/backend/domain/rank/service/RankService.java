ca89321258080ea0b2ebcdff62b2fe24da27de9f
hwan2-99 <thh1233123@gmail.com>
2024-04-16 17:55:37 +0900

    [feat] 상품 랭크에 대한 서비스 클래스 구현 (KakaoFunding#97)

---
+++ src/main/java/org/kakaoshare/backend/domain/rank/service/RankService.java
@@ -0,0 +1,11 @@
+package org.kakaoshare.backend.domain.rank.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class RankService {
+}
