3e6f561780446b4b1684b688e4dab5a86832b16d
hwan2-99 <thh1233123@gmail.com>
2024-04-16 17:55:25 +0900

    [feat] 상품 랭크에 대한 api 관리 컨트롤러 구현 (KakaoFunding#97)

---
+++ src/main/java/org/kakaoshare/backend/domain/rank/controller/RankController.java
@@ -0,0 +1,13 @@
+package org.kakaoshare.backend.domain.rank.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/ranking")
+public class RankController {
+}
