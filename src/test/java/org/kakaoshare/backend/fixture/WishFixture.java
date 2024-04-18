905e77261ba96753d92c54ce4aecfe84c9fbd959
YeaChan05 <qkenrdl05@gmail.com>
2024-04-18 21:33:40 +0900

    [test] 위시 공개범위 수정 테스트 (#101)
    위시 공개범위 수정 테스트

--- src/test/java/org/kakaoshare/backend/fixture/WishFixture.java
+++ src/test/java/org/kakaoshare/backend/fixture/WishFixture.java
@@ -4,22 +4,24 @@ import org.kakaoshare.backend.domain.member.entity.Member;
 import org.kakaoshare.backend.domain.product.entity.Product;
 import org.kakaoshare.backend.domain.wish.entity.Wish;
 public enum WishFixture {
-    TEST_WISH1(ProductFixture.TEST_PRODUCT.브랜드_설정_생성(BrandFixture.EDIYA.생성()), MemberFixture.KAKAO.생성(), true),
-    TEST_WISH2(ProductFixture.CAKE.브랜드_설정_생성(BrandFixture.STARBUCKS.생성()), MemberFixture.KAKAO.생성(), false),
-    TEST_WISH3(ProductFixture.COFFEE.브랜드_설정_생성(BrandFixture.EDIYA.생성()), MemberFixture.KAKAO.생성(), false),
-    TEST_WISH4(ProductFixture.TEST_PRODUCT.브랜드_설정_생성(BrandFixture.STARBUCKS.생성()), MemberFixture.KIM.생성(), true),
-    TEST_WISH5(ProductFixture.CAKE.브랜드_설정_생성(BrandFixture.EDIYA.생성()), MemberFixture.KIM.생성(), true),
-    TEST_WISH6(ProductFixture.COFFEE.브랜드_설정_생성(BrandFixture.STARBUCKS.생성()), MemberFixture.KIM.생성(), false);
+    TEST_WISH1(ProductFixture.TEST_PRODUCT.브랜드_설정_생성(BrandFixture.EDIYA.생성()), MemberFixture.KAKAO.생성(), true, 1L),
+    TEST_WISH2(ProductFixture.CAKE.브랜드_설정_생성(BrandFixture.STARBUCKS.생성()), MemberFixture.KAKAO.생성(), false, 2L),
+    TEST_WISH3(ProductFixture.COFFEE.브랜드_설정_생성(BrandFixture.EDIYA.생성()), MemberFixture.KAKAO.생성(), false, 3L),
+    TEST_WISH4(ProductFixture.TEST_PRODUCT.브랜드_설정_생성(BrandFixture.STARBUCKS.생성()), MemberFixture.KIM.생성(), true, 4L),
+    TEST_WISH5(ProductFixture.CAKE.브랜드_설정_생성(BrandFixture.EDIYA.생성()), MemberFixture.KIM.생성(), true, 5L),
+    TEST_WISH6(ProductFixture.COFFEE.브랜드_설정_생성(BrandFixture.STARBUCKS.생성()), MemberFixture.KIM.생성(), false, 6L);
     
     
     private final Product product;
     private final Member member;
     private final boolean isPublic;
+    private final Long wishId;
     
-    WishFixture(Product product, Member member, boolean isPublic) {
+    WishFixture(Product product, Member member, boolean isPublic, Long wishId) {
         this.product = product;
         this.member = member;
         this.isPublic = isPublic;
+        this.wishId = wishId;
     }
     
     public Member getMember() {
@@ -31,6 +33,7 @@ public enum WishFixture {
     }
     public  Wish 생성() {
         return Wish.builder()
+                .wishId(wishId)
                 .product(product)
                 .member(member)
                 .isPublic(isPublic)
@@ -39,6 +42,7 @@ public enum WishFixture {
     
     public  Wish 생성(Product product, Member member) {
         return Wish.builder()
+                .wishId(wishId)
                 .product(product)
                 .member(member)
                 .isPublic(isPublic)
