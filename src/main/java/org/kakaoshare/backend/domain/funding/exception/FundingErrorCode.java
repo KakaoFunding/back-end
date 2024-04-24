940724f6fe51e4429c67c24d75033cecdc39d2e5
kmw2378 <rlarla677@gmail.com>
2024-04-24 11:03:30 +0900

    [feat]FundingErrorCode 에 열거체 상수 추가 ${#96}

--- src/main/java/org/kakaoshare/backend/domain/funding/exception/FundingErrorCode.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/exception/FundingErrorCode.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum FundingErrorCode {
     INVALID_ACCUMULATE_AMOUNT("기여 금액은 잔여 금액보다 클 수 없습니다."),
+    INVALID_ATTRIBUTE_AMOUNT("기여 금액이 잘못되었습니다."),
     NOT_FOUND("펀딩 내역을 찾을 수 없습니다.");
 
     private final String message;
