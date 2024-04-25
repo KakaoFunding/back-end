112717d3293c3ece363911731ef2a5e5e6bebc8d
hwan2-99 <thh1233123@gmail.com>
2024-04-26 00:27:02 +0900

    [test] 랭킹조회 서비스 페이징 추가한 테스트로 수정 (KakaoFunding#97)

--- src/test/java/org/kakaoshare/backend/domain/rank/service/RankServiceTest.java
+++ src/test/java/org/kakaoshare/backend/domain/rank/service/RankServiceTest.java
@@ -16,6 +16,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @ExtendWith(MockitoExtension.class)
 public class RankServiceTest {
@@ -25,23 +28,28 @@ public class RankServiceTest {
     @InjectMocks
     private RankService rankService;
 
+    private Pageable pageable;
 
     @BeforeEach
     void setUp() {
+        pageable = PageRequest.of(0, 10, Sort.by("totalSales").descending());
+
         RankResponse rank1 = new RankResponse(1L, "Product A", 15000.0, "thumbnail1.jpg");
         RankResponse rank2 = new RankResponse(2L, "Product B", 7500.0, "thumbnail2.jpg");
         List<RankResponse> rankResponses = Arrays.asList(rank1, rank2);
 
-        when(orderRepository.findTopRankedProductsByOrders(any(LocalDateTime.class))).thenReturn(rankResponses);
+        when(orderRepository.findTopRankedProductsByOrders(any(LocalDateTime.class), eq(pageable)))
+                .thenReturn(rankResponses);
     }
 
     @Test
     @DisplayName("랭킹 조회 성공 테스트")
     public void testGetTopRankedProducts() {
-        List<RankResponse> results = rankService.getTopRankedProducts();
+        List<RankResponse> results = rankService.getTopRankedProducts(pageable);
         assertNotNull(results);
         assertEquals(2, results.size());
         assertEquals("Product A", results.get(0).getProductName());
-        verify(orderRepository).findTopRankedProductsByOrders(any(LocalDateTime.class));
+        assertEquals("Product B", results.get(1).getProductName());
+        verify(orderRepository).findTopRankedProductsByOrders(any(LocalDateTime.class), eq(pageable));
     }
 }
