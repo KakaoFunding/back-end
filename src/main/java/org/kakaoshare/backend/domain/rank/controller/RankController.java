a1b1f60de763c9bab22c05e855a8662fcd5936d1
hwan2-99 <thh1233123@gmail.com>
2024-04-26 00:40:31 +0900

    [refactor] 랭킹조회 컨트롤러 메소드내의 랭킹응답 객체 Page형태로 변경 (KakaoFunding#97)

--- src/main/java/org/kakaoshare/backend/domain/rank/controller/RankController.java
+++ src/main/java/org/kakaoshare/backend/domain/rank/controller/RankController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.kakaoshare.backend.domain.rank.dto.RankResponse;
 import org.kakaoshare.backend.domain.rank.service.RankService;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,7 @@ public class RankController {
     private final RankService rankService;
     @GetMapping
     public ResponseEntity<?> getTopRankedProducts(@PageableDefault(size = 20) Pageable pageable) {
-        List<RankResponse> rankResponses = rankService.getTopRankedProducts(pageable);
+        Page<RankResponse> rankResponses = rankService.getTopRankedProducts(pageable);
         return ResponseEntity.ok(rankResponses);
     }
 }
