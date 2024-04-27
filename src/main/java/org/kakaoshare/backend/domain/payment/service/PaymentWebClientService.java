302543885dc34abf0ca6c06f1686050ae8e8c954
kmw2378 <rlarla677@gmail.com>
2024-04-28 00:16:38 +0900

    [feat]PaymentWebClientService에 결제 취소 관련 메서드 및 필드 추가 ${#108} 결제 취소 요청 URL, 취소 요청을 보내는 메서드 추가

--- src/main/java/org/kakaoshare/backend/domain/payment/service/PaymentWebClientService.java
+++ src/main/java/org/kakaoshare/backend/domain/payment/service/PaymentWebClientService.java
@@ -2,6 +2,9 @@ package org.kakaoshare.backend.domain.payment.service;
 
 import org.kakaoshare.backend.domain.payment.dto.approve.request.KakaoPayApproveRequest;
 import org.kakaoshare.backend.domain.payment.dto.approve.response.KakaoPayApproveResponse;
+import org.kakaoshare.backend.domain.payment.dto.cancel.request.PaymentCancelDto;
+import org.kakaoshare.backend.domain.payment.dto.kakaopay.cancel.request.KakaoPayCancelRequest;
+import org.kakaoshare.backend.domain.payment.dto.kakaopay.cancel.response.KakaoPayCancelResponse;
 import org.kakaoshare.backend.domain.payment.dto.ready.request.KakaoPayReadyRequest;
 import org.kakaoshare.backend.domain.payment.dto.ready.request.PaymentReadyProductDto;
 import org.kakaoshare.backend.domain.payment.dto.ready.response.KakaoPayReadyResponse;
@@ -19,17 +22,20 @@ public class PaymentWebClientService {
     private static final String SECRET_KEY_PREFIX = "DEV_SECRET_KEY ";
     private final String readyUrl;
     private final String approveUrl;
+    private final String cancelUrl;
     private final String secretKey;
     private final WebClient webClient;
     private final KakaoPayRequestProvider requestProvider;
 
     public PaymentWebClientService(@Value("${pay.request-url.ready}") final String readyUrl,
                                    @Value("${pay.request-url.approve}") final String approveUrl,
+                                   @Value("${pay.request-url.cancel}") final String cancelUrl,
                                    @Value("${pay.secret-key}") final String secretKey,
                                    final WebClient webClient,
                                    final KakaoPayRequestProvider requestProvider) {
         this.readyUrl = readyUrl;
         this.approveUrl = approveUrl;
+        this.cancelUrl = cancelUrl;
         this.secretKey = secretKey;
         this.webClient = webClient;
         this.requestProvider = requestProvider;
@@ -58,6 +64,17 @@ public class PaymentWebClientService {
                 .bodyValue(kakaoPayApproveRequest)
                 .retrieve()
                 .bodyToMono(KakaoPayApproveResponse.class)
+                .block();
+    }
+
+    public KakaoPayCancelResponse cancel(final PaymentCancelDto paymentCancelDto) {
+        final KakaoPayCancelRequest kakaoPayCancelRequest = requestProvider.createCancelRequest(paymentCancelDto);
+        return webClient.post()
+                .uri(cancelUrl)
+                .header(HttpHeaders.AUTHORIZATION, SECRET_KEY_PREFIX + secretKey)
+                .bodyValue(kakaoPayCancelRequest)
+                .retrieve()
+                .bodyToMono(KakaoPayCancelResponse.class)
                 .block();
     }
 }
