5f85d87e97153c25874b245c73dbe8d7895d9e8c
kmw2378 <rlarla677@gmail.com>
2024-04-28 00:05:50 +0900

    [feat]FundingDetailRepository.findAllByFundingId() 메서드 추가 ${#108}

--- src/main/java/org/kakaoshare/backend/domain/funding/repository/FundingDetailRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/repository/FundingDetailRepository.java
@@ -4,10 +4,16 @@ import org.kakaoshare.backend.domain.funding.entity.Funding;
 import org.kakaoshare.backend.domain.funding.entity.FundingDetail;
 import org.kakaoshare.backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface FundingDetailRepository extends JpaRepository<FundingDetail, Long> {
     Optional<FundingDetail> findByFundingAndMember(final Funding funding, final Member member);
+
+    @Query("SELECT fd FROM FundingDetail fd " +
+            "WHERE fd.funding.fundingId =:fundingId")
+    List<FundingDetail> findAllByFundingId(final Long fundingId);
 }
