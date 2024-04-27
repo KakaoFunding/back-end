d3745af1e3d09c1601c471ff49c64ee75b69bfed
kmw2378 <rlarla677@gmail.com>
2024-04-28 00:06:38 +0900

    [feat]GiftRepository.findByReceiptId() 메서드 추가 ${#108}

--- src/main/java/org/kakaoshare/backend/domain/gift/repository/GiftRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/gift/repository/GiftRepository.java
@@ -3,7 +3,13 @@ package org.kakaoshare.backend.domain.gift.repository;
 import org.kakaoshare.backend.domain.gift.entity.Gift;
 import org.kakaoshare.backend.domain.gift.repository.query.GiftRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 
 public interface GiftRepository extends JpaRepository<Gift, Long>, GiftRepositoryCustom {
+    @Query("SELECT g FROM Gift g " +
+            "WHERE g.receipt.receiptId =:receiptId")
+    Optional<Gift> findByReceiptId(final Long receiptId);
 }
