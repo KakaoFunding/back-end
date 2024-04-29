df60edc6bd54a93ad835ee0b4c62bd2c87554fde
YeaChan05 <qkenrdl05@gmail.com>
2024-04-29 17:13:58 +0900

    [fix] 배포 후 실행 스크립트 수정 (#121)
    배포된 프로젝트를 실행할때 docker-compose.yml를 실행하도록 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,39 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-REPOSITORY=/home/ubuntu/kakao-funding
-cd $REPOSITORY
+#ROOT_PATH="/home/ec2-user/back-end"
+ROOT_PATH="/Users/sin-yechan/IdeaProjects/back-end"
+DOCKERFILE_PATH="$ROOT_PATH/Dockerfile"
+DOCKER_COMPOSE_PATH="$ROOT_PATH/docker-compose.yml"
 
-APP_NAME=kakao-funding
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-
-CURRENT_PID=$(pgrep -fl $APP_NAME | grep java | awk '{print $1}')
-
-if [ -z "$CURRENT_PID" ]
-then
-  echo "> 종료할 애플리케이션이 없습니다."
-else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+if [ -z "$IMAGE_NAME" ]; then
+    IMAGE_NAME="application"
+    export IMAGE_NAME
 fi
 
-echo "> Deploy - $JAR_PATH "
-nohup java -jar $JAR_PATH > kakao-funding.log 2>&1 < /dev/null &
+if [ -z "$IMAGE_TAG" ]; then
+    IMAGE_TAG="latest"
+    export IMAGE_TAG
+fi
+
+START_LOG="$ROOT_PATH/start.log"
+BUILD_LOG="$ROOT_PATH/build.log"
+
+NOW=$(date +%c)
+
+echo "[$NOW] Docker 이미지 빌드 시작" >> $START_LOG
+docker build -t $IMAGE_NAME:$IMAGE_TAG -f $DOCKERFILE_PATH $ROOT_PATH >> $BUILD_LOG 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "[$NOW] Docker 이미지 빌드 성공: $IMAGE_NAME:$IMAGE_TAG" >> $START_LOG
+
+    echo "[$NOW] Docker Compose를 사용하여 애플리케이션 시작" >> $START_LOG
+    docker-compose -f $DOCKER_COMPOSE_PATH up -d
+
+    if [ $? -eq 0 ]; then
+        echo "[$NOW] Docker Compose로 애플리케이션 시작 성공" >> $START_LOG
+    else
+        echo "[$NOW] Docker Compose로 애플리케이션 시작 실패" >> $START_LOG
+    fi
+else
+    echo "[$NOW] Docker 이미지 빌드 실패: $IMAGE_NAME:$IMAGE_TAG" >> $START_LOG
+fi
