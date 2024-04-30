ac103edd6f511b2aee368f9a5091e28bc04f6e06
kmw2378 <rlarla677@gmail.com>
2024-04-30 15:08:57 +0900

    [remove]flyway migration 스크립트 V4 삭제 ${#108}

--- src/main/resources/db/migration/V4.0__add_funding_detail_status.sql
+++
@@ -1,2 +0,0 @@
--- funding_detail 에 status 컬럼 추가
-ALTER TABLE funding_detail ADD COLUMN `status` enum ('PROGRESS', 'COMPLETE', 'CANCEL_REFUND') DEFAULT 'PROGRESS' NOT NULL;
--- src/main/resources/db/migration/V4.2__modify_funding_status.sql
+++
@@ -1,2 +0,0 @@
--- funding 기존 status 컬럼을 열거체화
-ALTER TABLE funding MODIFY `status` enum('PROGRESS', 'COMPLETE', 'CANCEL') DEFAULT 'PROGRESS' NOT NULL;
