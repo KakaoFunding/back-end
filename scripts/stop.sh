090b40a9b60983135532a03da9bb6e68723646b6
YeaChan05 <qkenrdl05@gmail.com>
2024-05-02 14:53:13 +0900

    [ci]종료 스크립트 수정(#${closes})
    종료 스크립트 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-for PORT in 8081 8082 8083; do
-    CONTAINER_ID=$(docker ps --filter "publish=$PORT" --format "{{.ID}}")
-    if [ ! -z "$CONTAINER_ID" ]; then
-        echo "Stopping container on port $PORT..."
-        docker stop $CONTAINER_ID
-    else
-        echo "No container is running on port $PORT."
-    fi
-done
+cd /home/ec2-user/cicd/scripts
+docker-compose down
