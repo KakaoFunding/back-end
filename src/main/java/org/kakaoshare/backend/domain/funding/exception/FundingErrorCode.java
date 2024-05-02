1292dde3c2e20c4b7bfba1f57fdd716ee5898dad
hwan2-99 <thh1233123@gmail.com>
2024-05-03 01:23:39 +0900

    [feat] 올바르지 않은 펀딩 상태의 접근에 대한 예외 메시지 추가 (KakaoFunding#133)

--- src/main/java/org/kakaoshare/backend/domain/funding/exception/FundingErrorCode.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/exception/FundingErrorCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum FundingErrorCode {
     INVALID_ACCUMULATE_AMOUNT("기여 금액은 잔여 금액보다 클 수 없습니다."),
     INVALID_ATTRIBUTE_AMOUNT("기여 금액이 잘못되었습니다."),
-    NOT_FOUND("펀딩 내역을 찾을 수 없습니다.");
+    NOT_FOUND("펀딩 내역을 찾을 수 없습니다."),
+    INVALID_STATUS("올바른 펀딩의 상태 값이 아닙니다.");
 
     private final String message;
 
