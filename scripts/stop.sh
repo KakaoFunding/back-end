d8b050d1b81e92d63cbcee2516475893fdb518bd
YeaChan05 <qkenrdl05@gmail.com>
2024-05-03 18:40:50 +0900

    [ci] 정지 스크립트 수정(#${closes})
    정지 스크립트 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# 모든 앱 컨테이너 종료 및 삭제
+cd /home/ec2-user/cicd || exit
+
 for PORT in 8081 8082 8083; do
     CONTAINER_ID=$(docker ps --filter "publish=$PORT" --format "{{.ID}}")
     if [ ! -z "$CONTAINER_ID" ]; then
-        echo "Stopping and removing container on port $PORT..."
+        echo "Stopping container on port $PORT..."
         docker stop $CONTAINER_ID
         docker rm $CONTAINER_ID
     else
@@ -12,7 +13,6 @@ for PORT in 8081 8082 8083; do
     fi
 done
 
-# Nginx 컨테이너 종료 및 삭제
 NGINX_CONTAINER_ID=$(docker ps --filter "name=nginx" --format "{{.ID}}")
 if [ ! -z "$NGINX_CONTAINER_ID" ]; then
     echo "Stopping and removing Nginx container..."
