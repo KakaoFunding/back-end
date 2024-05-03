230c0c0b03eef1bd357333d7e4d7371e4635b02a
YeaChan05 <qkenrdl05@gmail.com>
2024-05-03 18:25:45 +0900

    [ci] 정지 스크립트 수정(#${closes})
    정지 스크립트 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,11 +2,16 @@
 
 #!/bin/bash
 
-# 절대 경로를 사용하여 docker-compose 명령 실행
-DOCKER_COMPOSE_FILE="/home/ec2-user/cicd/docker-compose.yml"
+# shellcheck disable=SC2164
+cd /home/ec2-user/cicd
 
+DOCKER_COMPOSE_FILE="docker-compose.yml"
+
+# 파일 존재 확인 및 Docker Compose 실행
 if [ -f "$DOCKER_COMPOSE_FILE" ]; then
+    echo "docker-compose 파일을 찾았습니다. 컨테이너를 종료합니다."
     docker-compose -f "$DOCKER_COMPOSE_FILE" down
 else
-    echo "docker-compose.yml 파일을 찾을 수 없습니다."
+    echo "docker-compose.yml 파일을 찾을 수 없습니다. 현재 위치: $(pwd)"
+    exit 1
 fi
