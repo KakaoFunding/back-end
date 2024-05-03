c4196c92cf92a1b52c5d10001d288f8ae8cf3968
YeaChan05 <qkenrdl05@gmail.com>
2024-05-03 18:48:14 +0900

    [ci] 정지 스크립트 수정(#${closes})
    정지 스크립트 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-cd /home/ec2-user/cicd || exit
-
 for PORT in 8081 8082 8083; do
     CONTAINER_ID=$(docker ps --filter "publish=$PORT" --format "{{.ID}}")
     if [ ! -z "$CONTAINER_ID" ]; then
         echo "Stopping container on port $PORT..."
         docker stop $CONTAINER_ID
-        docker rm $CONTAINER_ID
     else
         echo "No container is running on port $PORT."
     fi
 done
-
-NGINX_CONTAINER_ID=$(docker ps --filter "name=nginx" --format "{{.ID}}")
-if [ ! -z "$NGINX_CONTAINER_ID" ]; then
-    echo "Stopping and removing Nginx container..."
-    docker stop $NGINX_CONTAINER_ID
-    docker rm $NGINX_CONTAINER_ID
-else
-    echo "No Nginx container is running."
-fi
