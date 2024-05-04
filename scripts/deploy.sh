99e9411f8d0bca83f2e7c2ddb452b04e5cb921b8
YeaChan05 <qkenrdl05@gmail.com>
2024-05-04 22:51:56 +0900

    [ci] gradle build 추가(#${closes})
    gradle build 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ROOT_PATH="/home/ec2-user/cicd"
-DOCKERFILE_PATH="$ROOT_PATH/Dockerfile"
 DOCKER_COMPOSE_PATH="$ROOT_PATH/docker-compose.yml"
 
 IMAGE_NAME="application"
@@ -22,12 +21,7 @@ chmod +x ./gradlew
 if [ $? -eq 0 ]; then
     echo "[$(date +%c)] Gradle 빌드 성공" >> $START_LOG
 
-    echo "[$(date +%c)] Docker 이미지 빌드 시작" >> $START_LOG
-    docker build -t $IMAGE_NAME:$IMAGE_TAG -f $DOCKERFILE_PATH $ROOT_PATH >> $BUILD_LOG 2>&1
-
     if [ $? -eq 0 ]; then
-        echo "[$(date +%c)] Docker 이미지 빌드 성공: $IMAGE_NAME:$IMAGE_TAG" >> $START_LOG
-
         echo "[$(date +%c)] Docker Compose로 애플리케이션 시작" >> $START_LOG
         export SPRING_PROFILES_ACTIVE=prod
         docker-compose -f $DOCKER_COMPOSE_PATH up -d >> $BUILD_LOG 2>&1
