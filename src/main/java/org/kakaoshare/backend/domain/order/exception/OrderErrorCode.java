0fdd00f388f24086f6b1e252bb68f542b4dfab1f
kmw2378 <rlarla677@gmail.com>
2024-05-07 22:37:53 +0900

    [refactor]Order 관련 예외 및 에러 코드 수정 (#${closes})

--- src/main/java/org/kakaoshare/backend/domain/order/exception/OrderErrorCode.java
+++ src/main/java/org/kakaoshare/backend/domain/order/exception/OrderErrorCode.java
@@ -1,14 +1,18 @@
 package org.kakaoshare.backend.domain.order.exception;
 
 import lombok.Getter;
+import org.kakaoshare.backend.common.error.ErrorCode;
+import org.springframework.http.HttpStatus;
 
 @Getter
-public enum OrderErrorCode {
-    NOT_FOUND("주문 내역을 찾을 수 없습니다.");
+public enum OrderErrorCode implements ErrorCode {
+    NOT_FOUND(HttpStatus.NOT_FOUND, "주문 내역을 찾을 수 없습니다.");
 
+    private final HttpStatus httpStatus;
     private final String message;
 
-    OrderErrorCode(final String message) {
+    OrderErrorCode(final HttpStatus httpStatus, final String message) {
+        this.httpStatus = httpStatus;
         this.message = message;
     }
 }
