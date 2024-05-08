71e5c5fcc04a537e7d08123f46285c8856cd5c67
kmw2378 <rlarla677@gmail.com>
2024-05-08 15:57:03 +0900

    [refactor]로그인 시 소셜 id 필드명을 providerId로 수정 (#${closes})

--- src/main/java/org/kakaoshare/backend/domain/member/dto/oauth/authenticate/OAuthLoginMemberResponse.java
+++ src/main/java/org/kakaoshare/backend/domain/member/dto/oauth/authenticate/OAuthLoginMemberResponse.java
@@ -2,7 +2,7 @@ package org.kakaoshare.backend.domain.member.dto.oauth.authenticate;
 
 import org.kakaoshare.backend.domain.member.dto.oauth.profile.OAuthProfile;
 
-public record OAuthLoginMemberResponse(String id, String profileUrl, String name) {
+public record OAuthLoginMemberResponse(String providerId, String profileUrl, String name) {
     public static OAuthLoginMemberResponse from(final OAuthProfile oAuthProfile) {
         return new OAuthLoginMemberResponse(
                 oAuthProfile.getProviderId(),
