d754b23e1004a2efdf9f149c8892b1d6434b7cfe
kmw2378 <rlarla677@gmail.com>
2024-05-08 16:01:08 +0900

    [refactor]FundingErrorCode HttpStatus static import 해제 (#${closes})

--- src/main/java/org/kakaoshare/backend/domain/funding/exception/FundingErrorCode.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/exception/FundingErrorCode.java
@@ -4,12 +4,10 @@ import lombok.Getter;
 import org.kakaoshare.backend.common.error.ErrorCode;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-
 @Getter
 public enum FundingErrorCode implements ErrorCode {
-    INVALID_ACCUMULATE_AMOUNT(BAD_REQUEST, "기여 금액은 잔여 금액보다 클 수 없습니다."),
-    INVALID_ATTRIBUTE_AMOUNT(BAD_REQUEST, "기여 금액이 잘못되었습니다."),
+    INVALID_ACCUMULATE_AMOUNT(HttpStatus.BAD_REQUEST, "기여 금액은 잔여 금액보다 클 수 없습니다."),
+    INVALID_ATTRIBUTE_AMOUNT(HttpStatus.BAD_REQUEST, "기여 금액이 잘못되었습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "펀딩 내역을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
