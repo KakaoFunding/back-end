105075c29be4f0fdf5878dadf68d9b84c9fdf6d7
kmw2378 <rlarla677@gmail.com>
2024-05-09 23:05:48 +0900

    [feat]OrderErrorCode에 상수 필드 추가 (#${closes}) 주문 내역 조회시 검증에 필요한 에러 코드 추가

--- src/main/java/org/kakaoshare/backend/domain/order/exception/OrderErrorCode.java
+++ src/main/java/org/kakaoshare/backend/domain/order/exception/OrderErrorCode.java
@@ -4,7 +4,10 @@ import lombok.Getter;
 
 @Getter
 public enum OrderErrorCode {
-    NOT_FOUND("주문 내역을 찾을 수 없습니다.");
+    NOT_FOUND("주문 내역을 찾을 수 없습니다."),
+    DATE_NOT_NULL("조회 시작, 종료일은 필수입니다."),
+    INVALID_DATE("조회 시작일은 종료일보다 전이여야 합니다."),
+    INVALID_DATE_RANGE("조회기간은 최대 1년까지 설정 가능합니다.");
 
     private final String message;
 
