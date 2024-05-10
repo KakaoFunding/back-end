b1edab3e8d75709110840b0f034d227ffd3e8c86
YeaChan05 <qkenrdl05@gmail.com>
2024-05-10 14:24:53 +0900

    [ci] compose 실패 로그(#${closes})
    compose 실패 로그

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -27,15 +27,14 @@ chmod +x ./gradlew
 if [ $? -eq 0 ]; then
     echo "[$(date +%c)] Gradle 빌드 성공" >> $START_LOG
 
-    echo "[$(date +%c)] Docker Compose로 애플리케이션 시작 시도" >> $START_LOG
+    echo "[$(date +%c)] Docker 이미지 빌드 및 실행 시도" >> $START_LOG
     docker-compose -f $DOCKER_COMPOSE_PATH up -d >> $BUILD_LOG 2>&1
-
     if [ $? -eq 0 ]; then
         echo "[$(date +%c)] Docker Compose로 애플리케이션 시작 성공" >> $START_LOG
     else
         echo "[$(date +%c)] Docker Compose로 애플리케이션 시작 실패" >> $START_LOG
-        echo "[$(date +%c)] 오류 내용:" >> $START_LOG
-        docker-compose -f $DOCKER_COMPOSE_PATH logs >> $START_LOG
+        echo "[$(date +%c)] Docker Compose 오류 로그:" >> $START_LOG
+        docker-compose logs >> $START_LOG 2>&1
     fi
 else
     echo "[$(date +%c)] Gradle 빌드 실패" >> $START_LOG
