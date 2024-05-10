eec0d89d0c4675101c01fe417514614be7e918f6
kmw2378 <rlarla677@gmail.com>
2024-05-10 17:27:53 +0900

    [remove]필요없어진 FundingSummaryResponse 삭제 (#${close})

--- src/main/java/org/kakaoshare/backend/domain/funding/dto/FundingSummaryResponse.java
+++
@@ -1,6 +0,0 @@
-package org.kakaoshare.backend.domain.funding.dto;
-
-import org.kakaoshare.backend.domain.product.dto.ProductSummaryResponse;
-
-public record FundingSummaryResponse(ProductSummaryResponse product, Long attributeAmount) {
-}
