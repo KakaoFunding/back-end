39de781f21a59bc2417815cd2a425aadef5e44ed
kmw2378 <rlarla677@gmail.com>
2024-05-12 18:00:36 +0900

    [test]기여한 펀딩내역 조회 테스트 코드 추가 (#${close})

--- src/test/java/org/kakaoshare/backend/domain/funding/service/FundingServiceTest.java
+++ src/test/java/org/kakaoshare/backend/domain/funding/service/FundingServiceTest.java
@@ -1,58 +1,135 @@
-//package org.kakaoshare.backend.domain.funding.service;
-//
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//import static org.mockito.ArgumentMatchers.any;
-//
-//import java.math.BigDecimal;
-//import java.time.LocalDate;
-//import java.util.Arrays;
-//import java.util.List;
-//import java.util.Optional;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.kakaoshare.backend.domain.brand.entity.Brand;
-//import org.kakaoshare.backend.domain.funding.dto.FundingSliceResponse;
-//import org.kakaoshare.backend.domain.funding.dto.ProgressResponse;
-//import org.kakaoshare.backend.domain.funding.dto.RegisterRequest;
-//import org.kakaoshare.backend.domain.funding.dto.RegisterResponse;
-//import org.kakaoshare.backend.domain.funding.entity.Funding;
-//import org.kakaoshare.backend.domain.funding.repository.FundingRepository;
-//import org.kakaoshare.backend.domain.member.entity.Member;
-//import org.kakaoshare.backend.domain.member.repository.MemberRepository;
-//import org.kakaoshare.backend.domain.product.entity.Product;
-//import org.kakaoshare.backend.domain.product.repository.ProductRepository;
-//import org.kakaoshare.backend.fixture.BrandFixture;
-//import org.kakaoshare.backend.fixture.FundingFixture;
-//import org.kakaoshare.backend.fixture.MemberFixture;
-//import org.kakaoshare.backend.fixture.ProductFixture;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.data.domain.PageRequest;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.data.domain.Slice;
-//import org.springframework.data.domain.SliceImpl;
-//
-//import static org.mockito.Mockito.*;
-//import static org.mockito.BDDMockito.given;
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//@ExtendWith(MockitoExtension.class)
-//public class FundingServiceTest {
-//
-//    @InjectMocks
-//    private FundingService fundingService;
-//
-//    @Mock
-//    private ProductRepository productRepository;
-//
-//    @Mock
-//    private MemberRepository memberRepository;
-//
-//    @Mock
-//    private FundingRepository fundingRepository;
-//
+package org.kakaoshare.backend.domain.funding.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.kakaoshare.backend.common.dto.PageResponse;
+import org.kakaoshare.backend.common.vo.Date;
+import org.kakaoshare.backend.domain.funding.dto.inquiry.ContributedFundingHistoryDto;
+import org.kakaoshare.backend.domain.funding.dto.inquiry.ContributedFundingHistoryResponse;
+import org.kakaoshare.backend.domain.funding.repository.FundingDetailRepository;
+import org.kakaoshare.backend.domain.funding.repository.FundingRepository;
+import org.kakaoshare.backend.domain.member.entity.Member;
+import org.kakaoshare.backend.domain.member.repository.MemberRepository;
+import org.kakaoshare.backend.domain.product.dto.ProductDto;
+import org.kakaoshare.backend.domain.product.entity.Product;
+import org.kakaoshare.backend.domain.product.repository.ProductRepository;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.kakaoshare.backend.domain.funding.entity.FundingDetailStatus.COMPLETE;
+import static org.kakaoshare.backend.domain.funding.entity.FundingDetailStatus.PROGRESS;
+import static org.kakaoshare.backend.fixture.MemberFixture.KAKAO;
+import static org.kakaoshare.backend.fixture.MemberFixture.KIM;
+import static org.kakaoshare.backend.fixture.ProductFixture.CAKE;
+import static org.kakaoshare.backend.fixture.ProductFixture.COFFEE;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+public class FundingServiceTest {
+
+    @InjectMocks
+    private FundingService fundingService;
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private FundingRepository fundingRepository;
+
+    @Mock
+    private FundingDetailRepository fundingDetailRepository;
+
+    Member contributor;
+    Member creator;
+
+    @BeforeEach
+    public void setUp() {
+        contributor = KAKAO.생성();
+        creator = KIM.생성();
+    }
+
+    @ParameterizedTest
+    @DisplayName("내가 기여한 펀딩 내역 조회 (상태 필터링 O)")
+    @ValueSource(strings = {"PROGRESS", "COMPLETE", "CANCEL_REFUND"})
+    public void lookUp(final String status) throws Exception {
+        final String providerId = contributor.getProviderId();
+        final Date date = new Date(
+                LocalDate.now().minusMonths(5L),
+                LocalDate.now()
+        );
+        final Pageable pageable = PageRequest.of(0, 5, Sort.by("createdAt"));
+
+        final Product cake = CAKE.생성();
+        final Product coffee = COFFEE.생성();
+
+        final List<ContributedFundingHistoryResponse> content = List.of(
+                new ContributedFundingHistoryResponse(
+                        getProductDto(cake),
+                        getFundingHistoryDto(1L, 1L, LocalDateTime.now().minusDays(20L), creator.getName(), status)
+                ),
+                new ContributedFundingHistoryResponse(
+                        getProductDto(coffee),
+                        getFundingHistoryDto(2L, 2L, LocalDateTime.now().minusDays(20L), creator.getName(), status)
+                )
+        );
+        final Page<?> page = new PageImpl<>(content, pageable, content.size());
+        doReturn(page).when(fundingDetailRepository).findHistoryByProviderIdAndDateAndStatus(providerId, date, status, pageable);
+
+        final PageResponse<?> actual = fundingService.lookUp(providerId, date, status, pageable);
+        final PageResponse<?> expect = PageResponse.from(page);
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expect);
+    }
+
+    @Test
+    @DisplayName("내가 기여한 펀딩 내역 조회 (상태 필터링 X)")
+    public void lookUpWithoutStatus() throws Exception {
+        final String providerId = contributor.getProviderId();
+        final Date date = new Date(
+                LocalDate.now().minusMonths(5L),
+                LocalDate.now()
+        );
+        final String status = null;
+        final Pageable pageable = PageRequest.of(0, 5, Sort.by("createdAt"));
+
+        final Product cake = CAKE.생성();
+        final Product coffee = COFFEE.생성();
+
+        final List<ContributedFundingHistoryResponse> content = List.of(
+                new ContributedFundingHistoryResponse(
+                        getProductDto(cake),
+                        getFundingHistoryDto(1L, 1L, LocalDateTime.now().minusDays(20L), creator.getName(), COMPLETE.name())
+                ),
+                new ContributedFundingHistoryResponse(
+                        getProductDto(coffee),
+                        getFundingHistoryDto(2L, 2L, LocalDateTime.now().minusDays(20L), creator.getName(), PROGRESS.name()
+                        )
+                ));
+        final Page<?> page = new PageImpl<>(content, pageable, content.size());
+        doReturn(page).when(fundingDetailRepository).findHistoryByProviderIdAndDate(providerId, date, pageable);
+
+        final PageResponse<?> actual = fundingService.lookUp(providerId, date, status, pageable);
+        final PageResponse<?> expect = PageResponse.from(page);
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expect);
+    }
+
 //    @Test
 //    @DisplayName("성공적으로 펀딩 아이템 등록")
 //    void registerFundingItem_Success() {
@@ -117,4 +194,21 @@
 //        assertThat(response).isNotNull();
 //        assertThat(response.getFundingItems()).hasSize(fundingList.size());
 //    }
-//}
+
+    public ProductDto getProductDto(final Product product) {
+        return new ProductDto(
+                product.getProductId(),
+                product.getName(),
+                product.getPhoto(),
+                product.getPrice(),
+                product.getBrandName()
+        );
+    }
+
+    public ContributedFundingHistoryDto getFundingHistoryDto(final Long fundingId,
+                                                             final Long fundingDetailId,
+                                                             final LocalDateTime attributedAt,
+                                                             final String creatorName, final String status) {
+        return new ContributedFundingHistoryDto(fundingId, fundingDetailId, attributedAt, creatorName, status);
+    }
+}
