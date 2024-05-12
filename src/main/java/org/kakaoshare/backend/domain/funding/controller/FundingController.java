b9e75c379df9bc3143d015b9197aad1c21ad744a
kmw2378 <rlarla677@gmail.com>
2024-05-12 18:27:43 +0900

    [refactor]기존 기여한 펀딩내역 조회 로직을 FundingDetail 로 이동 (#${close})

--- src/main/java/org/kakaoshare/backend/domain/funding/controller/FundingController.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/controller/FundingController.java
@@ -1,23 +1,17 @@
 package org.kakaoshare.backend.domain.funding.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.kakaoshare.backend.common.util.EnumValue;
-import org.kakaoshare.backend.common.vo.Date;
 import org.kakaoshare.backend.domain.funding.dto.FundingSliceResponse;
 import org.kakaoshare.backend.domain.funding.dto.ProgressResponse;
 import org.kakaoshare.backend.domain.funding.dto.RegisterRequest;
 import org.kakaoshare.backend.domain.funding.dto.RegisterResponse;
 import org.kakaoshare.backend.domain.funding.dto.preview.request.FundingPreviewRequest;
 import org.kakaoshare.backend.domain.funding.dto.preview.response.FundingPreviewResponse;
-import org.kakaoshare.backend.domain.funding.entity.FundingDetailStatus;
 import org.kakaoshare.backend.domain.funding.service.FundingService;
 import org.kakaoshare.backend.jwt.util.LoggedInMember;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,27 +19,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.data.domain.Sort.Direction;
-
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1")
 public class FundingController {
-    private static final int DEFAULT_HISTORY_SIZE = 10;
-    private static final String DEFAULT_HISTORY_SORT_VALUE = "createdAt";
-
     private final FundingService fundingService;
-
-    @GetMapping("/funding")
-    public ResponseEntity<?> lookUp(@LoggedInMember final String providerId,
-                                    @ModelAttribute final Date date,
-                                    @EnumValue(enumClass = FundingDetailStatus.class, ignoreCase = true, nullable = true) final String status,
-                                    @PageableDefault(size = DEFAULT_HISTORY_SIZE, sort = DEFAULT_HISTORY_SORT_VALUE, direction = Direction.DESC) final Pageable pageable
-    ) {
-        return ResponseEntity.ok(
-                fundingService.lookUp(providerId, date, status, pageable)
-        );
-    }
 
     @PostMapping("/funding/{productId}")
     public ResponseEntity<?> registerFunding(@PathVariable Long productId, @LoggedInMember String providerId,
