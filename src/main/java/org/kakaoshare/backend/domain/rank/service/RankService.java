d0c4c8bfc080e5ee921f0cf321852ff49e2ebbff
hwan2-99 <thh1233123@gmail.com>
2024-05-14 01:42:13 +0900

    [feat] 필터링을 통한 랭킹 조회 서비스구현(KakaoFunding#181)

--- src/main/java/org/kakaoshare/backend/domain/rank/service/RankService.java
+++ src/main/java/org/kakaoshare/backend/domain/rank/service/RankService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.kakaoshare.backend.domain.order.repository.OrderRepository;
 import org.kakaoshare.backend.domain.rank.dto.RankResponse;
+import org.kakaoshare.backend.domain.rank.entity.RankType;
+import org.kakaoshare.backend.domain.rank.entity.TargetType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,8 +18,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class RankService {
     private final OrderRepository orderRepository;
     private static final int MONTH_VALUE = 6;
+    private static final int LIMIT_PRODUCT_COUNT = 20;
+
     public Page<RankResponse> getTopRankedProducts(Pageable pageable) {
-        return orderRepository.findTopRankedProductsByOrders(LocalDateTime.now().minusMonths(MONTH_VALUE),pageable);
+        return orderRepository.findTopRankedProductsByOrders(LocalDateTime.now().minusMonths(MONTH_VALUE), pageable);
     }
 
+    public List<RankResponse> findProductsByFilters(RankType rankType, TargetType targetType, int minPrice,
+                                                    int maxPrice) {
+        if (rankType.equals(RankType.MANY_WISH)) {
+            return orderRepository.findProductsByWish(targetType, minPrice, maxPrice, LIMIT_PRODUCT_COUNT);
+        }
+        if (rankType.equals(RankType.MANY_RECEIVE)) {
+            return orderRepository.findProductsByReceived(targetType, minPrice, maxPrice, LIMIT_PRODUCT_COUNT);
+        }
+        return null;
+    }
 }
