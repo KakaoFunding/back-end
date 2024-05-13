ce53954f3ab3043c7cd4178ac493b44bee832023
hwan2-99 <thh1233123@gmail.com>
2024-05-13 19:34:35 +0900

    [feat] 성별에 따른 위시 많은순 랭킹조회 쿼리 작성(KakaoFunding#181)

--- src/main/java/org/kakaoshare/backend/domain/order/repository/query/OrderRepositoryCustom.java
+++ src/main/java/org/kakaoshare/backend/domain/order/repository/query/OrderRepositoryCustom.java
@@ -1,10 +1,13 @@
 package org.kakaoshare.backend.domain.order.repository.query;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import org.kakaoshare.backend.domain.rank.dto.RankResponse;
+import org.kakaoshare.backend.domain.rank.entity.TargetType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface OrderRepositoryCustom {
     Page<RankResponse> findTopRankedProductsByOrders(LocalDateTime sixMonthsAgo, Pageable pageable);
+    List<RankResponse> findProductsByFilters(TargetType targetType, int minPrice, int maxPrice, int limit);
 }
--- src/main/java/org/kakaoshare/backend/domain/order/repository/query/OrderRepositoryCustomImpl.java
+++ src/main/java/org/kakaoshare/backend/domain/order/repository/query/OrderRepositoryCustomImpl.java
@@ -1,18 +1,26 @@
 package org.kakaoshare.backend.domain.order.repository.query;
 
+import static org.kakaoshare.backend.domain.member.entity.QMember.member;
 import static org.kakaoshare.backend.domain.order.entity.QOrder.order;
 import static org.kakaoshare.backend.domain.product.entity.QProduct.product;
 import static org.kakaoshare.backend.domain.receipt.entity.QReceipt.receipt;
+import static org.kakaoshare.backend.domain.wish.entity.QWish.wish;
 
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.kakaoshare.backend.common.util.RepositoryUtils;
+import org.kakaoshare.backend.domain.member.entity.Gender;
 import org.kakaoshare.backend.domain.rank.dto.RankResponse;
+import org.kakaoshare.backend.domain.rank.entity.TargetType;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
@@ -49,5 +57,36 @@ public class OrderRepositoryCustomImpl implements OrderRepositoryCustom{
                 .groupBy(product.productId);
 
         return RepositoryUtils.toPage(pageable, contentQuery, countQuery);
+    }
+    public List<RankResponse> findProductsByFilters(TargetType targetType, int minPrice, int maxPrice, int limit) {
+
+        BooleanExpression genderCondition = createGenderCondition(targetType);
+        BooleanExpression priceCondition = product.price.between(minPrice, maxPrice);
+
+
+        return queryFactory
+                .select(Projections.constructor(RankResponse.class,
+                        product.productId,
+                        product.name,
+                        wish.count().as("totalWishes"),
+                        product.photo
+                ))
+                .from(wish)
+                .join(wish.product, product)
+                .join(wish.member, member)
+                .where(genderCondition.and(priceCondition))
+                .groupBy(product.productId)
+                .orderBy(wish.count().desc())
+                .limit(limit)
+                .fetch();
+    }
+
+    private BooleanExpression createGenderCondition(TargetType targetType) {
+        if (targetType == TargetType.MALE) {
+            return member.gender.eq(Gender.MALE);
+        } else if (targetType == TargetType.FEMALE) {
+            return member.gender.eq(Gender.FEMALE);
+        }
+        return null;
     }
 }
