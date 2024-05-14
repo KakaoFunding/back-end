17d89e3ff573d20f8e16d02154458a7024e45e11
kmw2378 <rlarla677@gmail.com>
2024-05-14 16:08:11 +0900

    [feat]FundingGiftRepository 엔티티 추가 (#${closes})

---
+++ src/main/java/org/kakaoshare/backend/domain/funding/repository/FundingGiftRepository.java
@@ -0,0 +1,7 @@
+package org.kakaoshare.backend.domain.funding.repository;
+
+import org.kakaoshare.backend.domain.funding.entity.FundingGift;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FundingGiftRepository extends JpaRepository<FundingGift, Long> {
+}
