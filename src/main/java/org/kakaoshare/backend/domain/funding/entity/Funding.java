ee3b80c79838ca2260f887f56e910f7f83be620e
kmw2378 <rlarla677@gmail.com>
2024-05-14 16:20:31 +0900

    [feat]Funding 목표 금액 달성 시 잔여 금액 결제 여부 판단 로직 추가 (#${closes}) 목표 금액 달성 시 남은 금액이 존재하는지에 따라 상태를 변경하는 로직 추가

--- src/main/java/org/kakaoshare/backend/domain/funding/entity/Funding.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/entity/Funding.java
@@ -22,6 +22,7 @@ import org.kakaoshare.backend.domain.product.entity.Product;
 
 import java.time.LocalDate;
 
+import static org.kakaoshare.backend.domain.funding.entity.FundingStatus.BEFORE_PAYING_REMAINING;
 import static org.kakaoshare.backend.domain.funding.entity.FundingStatus.CANCEL;
 import static org.kakaoshare.backend.domain.funding.entity.FundingStatus.COMPLETE;
 import static org.kakaoshare.backend.domain.funding.entity.FundingStatus.PROGRESS;
@@ -105,20 +106,42 @@ public class Funding extends BaseTimeEntity {
         this.accumulateAmount = 0L;
     }
 
-    public void finish() {
+    public void reflectStatus(final Long amount,
+                              final Long contributorId) {
         if (!satisfiedAccumulateAmount()) {
             return;
         }
 
-        this.status = COMPLETE;
+        this.status = BEFORE_PAYING_REMAINING;
+        if (isPayRemainingByCreator(amount, contributorId) || hasNoRemainingPay()) {
+            this.status = COMPLETE;
+        }
     }
 
     public boolean canceled() {
         return status.canceled();
     }
 
+    public boolean completed() {
+        return status.completed();
+    }
+
     public boolean satisfiedAccumulateAmount() {
         return goalAmount.equals(accumulateAmount);
+    }
+
+    private boolean hasNoRemainingPay() {
+        return product.getPrice().equals(goalAmount);
+    }
+
+    private boolean isPayRemainingByCreator(final Long amount,
+                                            final Long contributorId) {
+        final Long creatorId = member.getMemberId();
+        if (!creatorId.equals(contributorId)) {
+            return false;
+        }
+
+        return product.getPrice() == amount + goalAmount;
     }
 
     @Override
