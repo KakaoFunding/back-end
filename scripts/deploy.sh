fb30413a9364546b2ae47796eb79d40268737365
YeaChan05 <qkenrdl05@gmail.com>
2024-05-14 14:50:19 +0900

    [ci] 배포 프로세스 수정 수정(#${closes})
    배포 방식을 docker hub을 이용한 방식으로 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,12 +4,12 @@ ROOT_PATH="/home/ec2-user/cicd"
 DOCKER_COMPOSE_PATH="$ROOT_PATH/docker-compose.yml"
 
 if [ -z "$IMAGE_NAME" ]; then
-    IMAGE_NAME="application"
+    IMAGE_NAME="yeachan05/application"  # 여기에 Docker Hub 경로 작성
     export IMAGE_NAME
 fi
 
 if [ -z "$IMAGE_TAG" ]; then
-    IMAGE_TAG="latest"
+    IMAGE_TAG=$(git rev-parse --short HEAD)
     export IMAGE_TAG
 fi
 
@@ -18,26 +18,12 @@ BUILD_LOG="$ROOT_PATH/build.log"
 
 touch $START_LOG $BUILD_LOG
 
-echo "[$(date +%c)] Gradle 빌드 시작" >> $START_LOG
-cd $ROOT_PATH || exit
-
-chmod +x ./gradlew
-./gradlew clean >> $BUILD_LOG 2>&1
-./gradlew build -x test >> $BUILD_LOG 2>&1
+docker-compose -f $DOCKER_COMPOSE_PATH up -d
 
 if [ $? -eq 0 ]; then
-    echo "[$(date +%c)] Gradle 빌드 성공" >> $START_LOG
-
-    echo "[$(date +%c)] Docker 이미지 빌드 및 실행 시도" >> $START_LOG
-    docker-compose -f $DOCKER_COMPOSE_PATH up -d >> $BUILD_LOG 2>&1
-    compose_status=$?
-    if [ $compose_status -eq 0 ]; then
-        echo "[$(date +%c)] Docker Compose로 애플리케이션 시작 성공" >> $START_LOG
-    else
-        echo "[$(date +%c)] Docker Compose로 애플리케이션 시작 실패" >> $START_LOG
-        echo "[$(date +%c)] Docker Compose 오류 로그:" >> $START_LOG
-        docker-compose -f $DOCKER_COMPOSE_PATH logs >> $START_LOG 2>&1
-    fi
+    echo "[$(date +%c)] Docker Compose로 애플리케이션 시작 성공" >> $START_LOG
 else
-    echo "[$(date +%c)] Gradle 빌드 실패" >> $START_LOG
+    echo "[$(date +%c)] Docker Compose로 애플리케이션 시작 실패" >> $START_LOG
+    echo "[$(date +%c)] Docker Compose 오류 로그:" >> $START_LOG
+    docker-compose -f $DOCKER_COMPOSE_PATH logs >> $START_LOG 2>&1
 fi
