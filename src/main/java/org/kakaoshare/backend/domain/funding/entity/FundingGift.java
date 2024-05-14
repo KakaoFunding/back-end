21e2a1f38954dea9c235aba4cd107a4a23c6791e
kmw2378 <rlarla677@gmail.com>
2024-05-14 16:08:02 +0900

    [feat]FundingGift 엔티티 추가 (#${closes}) 펀딩 완료시 선물 정보를 담는 엔티티 클래스 추가

---
+++ src/main/java/org/kakaoshare/backend/domain/funding/entity/FundingGift.java
@@ -0,0 +1,64 @@
+package org.kakaoshare.backend.domain.funding.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Builder;
+import lombok.Getter;
+import org.kakaoshare.backend.domain.gift.entity.GiftStatus;
+
+import java.time.LocalDateTime;
+
+import static org.kakaoshare.backend.domain.gift.entity.GiftStatus.NOT_USED;
+
+@Entity
+@Getter
+public class FundingGift {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long fundingReceiptId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "funding_id", nullable = false)
+    private Funding funding;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private GiftStatus status = NOT_USED;
+
+    @Column(nullable = false)
+    private LocalDateTime expiredAt;
+
+    protected FundingGift() {
+
+    }
+
+    @Builder
+    public FundingGift(final Funding funding,
+                       final LocalDateTime expiredAt) {
+        validateFunding(funding);
+        validateExpiredAt(expiredAt);
+        this.funding = funding;
+        this.expiredAt = expiredAt;
+    }
+
+    private void validateFunding(final Funding funding) {
+        if (!funding.finished()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateExpiredAt(final LocalDateTime expiredAt) {
+        if (!expiredAt.isAfter(LocalDateTime.now())) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
