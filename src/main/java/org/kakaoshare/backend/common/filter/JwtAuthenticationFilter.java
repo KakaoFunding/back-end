5f6c43ab720bae480a6040af2a8b8a4f4c3cba5b
YeaChan05 <qkenrdl05@gmail.com>
2024-05-15 23:07:31 +0900

    [fix] 필터 예외처리 수정 (#188)
    예외 처리 코드 추가

--- src/main/java/org/kakaoshare/backend/common/filter/JwtAuthenticationFilter.java
+++ src/main/java/org/kakaoshare/backend/common/filter/JwtAuthenticationFilter.java
@@ -1,11 +1,14 @@
 package org.kakaoshare.backend.common.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.querydsl.core.util.StringUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.kakaoshare.backend.common.error.response.ErrorResponse;
+import org.kakaoshare.backend.jwt.exception.JwtException;
 import org.kakaoshare.backend.jwt.util.JwtProvider;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -30,27 +33,40 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(final HttpServletRequest request,
                                     final HttpServletResponse response,
                                     final FilterChain filterChain) throws ServletException, IOException {
-        final String accessToken = getAccessToken(request);
-        if (accessToken != null && jwtProvider.validateToken(accessToken)) {
-            SecurityContextHolder.getContext()
-                    .setAuthentication(getAuthentication(accessToken));
+        try {
+            final String accessToken = getAccessToken(request);
+            if (accessToken != null && jwtProvider.validateToken(accessToken)) {
+                SecurityContextHolder.getContext()
+                        .setAuthentication(getAuthentication(accessToken));
+            }
+            
+            filterChain.doFilter(request, response);
+        } catch (JwtException e) {
+            handleJwtException(response, e);
         }
-
-        filterChain.doFilter(request, response);
     }
-
+    
     private String getAccessToken(final HttpServletRequest request) {
         final String token = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (StringUtils.isNullOrEmpty(token)) {
             return null;
         }
-
+        
         return token.substring(TOKEN_PREFIX.length());
     }
-
+    
     private Authentication getAuthentication(final String accessToken) {
         final String username = jwtProvider.getUsername(accessToken);
         final UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        return new UsernamePasswordAuthenticationToken(userDetails.getUsername(),null, userDetails.getAuthorities());
+        return new UsernamePasswordAuthenticationToken(userDetails.getUsername(), null, userDetails.getAuthorities());
+    }
+    
+    private void handleJwtException(HttpServletResponse response, JwtException e) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.setStatus(e.getErrorCode().getHttpStatus().value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        ErrorResponse errorResponse = ErrorResponse.from(e.getErrorCode());
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
     }
 }
