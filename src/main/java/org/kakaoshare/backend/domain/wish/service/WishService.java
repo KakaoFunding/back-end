39770391e78c1c43bd4e4d2dcfb8c3efbbf41567
YeaChan05 <qkenrdl05@gmail.com>
2024-05-15 17:34:18 +0900

    [feat]친구 위시리스트 조회 로직 구현 (#186)
    카카오 인증을 통한 사용자 검증 및 친구 위시 조회 로직 구현

--- src/main/java/org/kakaoshare/backend/domain/wish/service/WishService.java
+++ src/main/java/org/kakaoshare/backend/domain/wish/service/WishService.java
@@ -2,13 +2,17 @@ package org.kakaoshare.backend.domain.wish.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.kakaoshare.backend.domain.member.dto.oauth.profile.detail.KakaoFriendListDto;
 import org.kakaoshare.backend.domain.member.entity.Member;
 import org.kakaoshare.backend.domain.member.exception.MemberErrorCode;
 import org.kakaoshare.backend.domain.member.exception.MemberException;
 import org.kakaoshare.backend.domain.member.repository.MemberRepository;
+import org.kakaoshare.backend.domain.member.service.oauth.OAuthWebClientService;
 import org.kakaoshare.backend.domain.product.dto.WishCancelEvent;
 import org.kakaoshare.backend.domain.product.dto.WishEvent;
-import org.kakaoshare.backend.domain.wish.dto.WishDetail;
+import org.kakaoshare.backend.domain.wish.dto.FriendWishDetail;
+import org.kakaoshare.backend.domain.wish.dto.FriendsWishRequest;
+import org.kakaoshare.backend.domain.wish.dto.MyWishDetail;
 import org.kakaoshare.backend.domain.wish.dto.WishReservationEvent;
 import org.kakaoshare.backend.domain.wish.entity.Wish;
 import org.kakaoshare.backend.domain.wish.error.WishErrorCode;
@@ -28,11 +32,12 @@ import java.util.List;
 
 @Slf4j
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class WishService {
     private final WishRepository wishRepository;
     private final MemberRepository memberRepository;
+    private final OAuthWebClientService oAuthWebClientService;
     
     @Async
     @Transactional(propagation = Propagation.REQUIRES_NEW)//TODO 2024 04 11 18:57:19 : 성능 저하 가능성 있음
@@ -92,14 +97,31 @@ public class WishService {
         event.getProduct().increaseWishCount();
     }
     
-    @Transactional(readOnly = true)
+
     public Member getMember(final String providerId) {
         return memberRepository.findMemberByProviderId(providerId)
                 .orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND));
     }
-    
-    public List<WishDetail> getMembersWishList(final String providerId) {
+
+    public List<MyWishDetail> getMembersWishList(final String providerId) {
         return wishRepository.findWishDetailsByProviderId(providerId);
+    }
+    
+
+    public List<FriendWishDetail> getFriendsWishList(final String providerId, final FriendsWishRequest friendsWishRequest) {
+        checkIsFriend(friendsWishRequest);
+        return wishRepository.findWishDetailsByFriendProviderId(providerId, friendsWishRequest.friendsProviderId());
+    }
+    
+    private void checkIsFriend(final FriendsWishRequest friendsWishRequest) {
+        List<KakaoFriendListDto> friends = oAuthWebClientService.getFriendsList(friendsWishRequest.kakaoAccessToken());
+        boolean isFriend = friends.stream()
+                .anyMatch(kakaoFriendListDto -> kakaoFriendListDto
+                        .getId()
+                        .equals(friendsWishRequest.friendsProviderId()));
+        if(!isFriend){
+            throw new MemberException(MemberErrorCode.NO_SUCH_RELATIONSHIP);
+        }
     }
     
     private Wish createWish(final WishEvent event, final Member member) {
@@ -111,7 +133,7 @@ public class WishService {
     
     public void changeWishType(final String providerId, final Long wishId) {
         boolean exists = getMembersWishList(providerId).stream()
-                .anyMatch(wishDetail -> wishDetail.wishId().equals(wishId));
+                .anyMatch(wishDetail -> wishDetail.getWishId().equals(wishId));
         if (!exists) {
             throw new WishException(WishErrorCode.NOT_FOUND);
         }
