94aa57e9e540cbced9d9bbed5d825375fb2bf194
YeaChan05 <qkenrdl05@gmail.com>
2024-05-15 17:35:20 +0900

    [feat] 위시 조회 DTO 수정 (#186)
    위시 조회 요청 DTO 생성 및 조회 DTO 추상화

--- src/main/java/org/kakaoshare/backend/domain/wish/dto/WishDetail.java
+++
@@ -1,3 +0,0 @@
-package org.kakaoshare.backend.domain.wish.dto;
-public record WishDetail(Long wishId,Long productId, String productName, Long productPrice, String productPhoto, boolean isPublic) {
-}
