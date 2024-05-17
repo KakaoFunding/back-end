9e428e299ef44cf3be80f3b3fe7cce00b3c725e7
hwan2-99 <thh1233123@gmail.com>
2024-05-17 22:12:36 +0900

    [feat] 장바구니에 상품 등록 서비스 구현(KakaoFunding#161)

--- src/main/java/org/kakaoshare/backend/domain/cart/service/CartService.java
+++ src/main/java/org/kakaoshare/backend/domain/cart/service/CartService.java
@@ -1,7 +1,14 @@
 package org.kakaoshare.backend.domain.cart.service;
 
 import lombok.RequiredArgsConstructor;
+import org.kakaoshare.backend.domain.cart.dto.CartRegisterResponse;
+import org.kakaoshare.backend.domain.cart.entity.Cart;
 import org.kakaoshare.backend.domain.cart.repository.CartRepository;
+import org.kakaoshare.backend.domain.member.entity.Member;
+import org.kakaoshare.backend.domain.member.repository.MemberRepository;
+import org.kakaoshare.backend.domain.product.entity.Product;
+import org.kakaoshare.backend.domain.product.repository.ProductRepository;
+import org.kakaoshare.backend.domain.product.service.ProductService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,4 +17,38 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class CartService {
     private final CartRepository cartRepository;
+    private final MemberRepository memberRepository;
+    private final ProductRepository productRepository;
+
+
+    public CartRegisterResponse registerItem(Long productId, String providerId) {
+        Member member = findMemberByProviderId(providerId);
+        Product product = findProductByProductId(productId);
+
+        Cart existingCart = cartRepository.findByMemberIdAndProductId(member.getMemberId(), product.getProductId()).orElse(null);
+        if (existingCart != null) {
+            existingCart.updateItemCount(existingCart.getItemCount() + 1);
+            cartRepository.save(existingCart);
+        } else {
+            Cart newCart = Cart.builder()
+                    .member(member)
+                    .product(product)
+                    .itemCount(1)
+                    .build();
+            cartRepository.save(newCart);
+        }
+
+        return CartRegisterResponse.builder()
+                .message("상품이 장바구니에 추가되었습니다.")
+                .build();
+    }
+    private Member findMemberByProviderId(String providerId) {
+        return memberRepository.findMemberByProviderId(providerId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid providerId"));
+    }
+
+    private Product findProductByProductId(Long productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid product ID"));
+    }
 }
