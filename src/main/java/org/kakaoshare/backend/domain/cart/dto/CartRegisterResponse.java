249f96dcc1932adc447305756c75ade76812b5ff
hwan2-99 <thh1233123@gmail.com>
2024-05-17 19:50:13 +0900

    [feat] 장바구니 등록 응답 dto 클래스 작성 및 정적 팩토리 메소드 구현(KakaoFunding#161)

---
+++ src/main/java/org/kakaoshare/backend/domain/cart/dto/CartRegisterResponse.java
@@ -0,0 +1,17 @@
+package org.kakaoshare.backend.domain.cart.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.kakaoshare.backend.domain.cart.entity.Cart;
+
+@Getter
+@Builder
+public class CartRegisterResponse {
+    private Long cartId;
+
+    public static CartRegisterResponse from(Cart cart){
+        return CartRegisterResponse.builder()
+                .cartId(cart.getCartId())
+                .build();
+    }
+}
