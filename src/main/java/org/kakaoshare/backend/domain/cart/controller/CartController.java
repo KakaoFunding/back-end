5ae938f1fe91083d3e952d463742b58d52b0c97a
hwan2-99 <thh1233123@gmail.com>
2024-05-17 13:02:41 +0900

    [feat] 장바구니 컨트롤러 작성(KakaoFunding#181)

---
+++ src/main/java/org/kakaoshare/backend/domain/cart/controller/CartController.java
@@ -0,0 +1,11 @@
+package org.kakaoshare.backend.domain.cart.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/cart")
+@RequiredArgsConstructor
+public class CartController {
+}
