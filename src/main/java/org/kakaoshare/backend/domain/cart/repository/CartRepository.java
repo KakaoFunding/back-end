8bc90fcb20c5d6f88d988c9ceee62796391239f3
hwan2-99 <thh1233123@gmail.com>
2024-05-17 23:54:57 +0900

    [feat] memberId를 통한 장바구니 조회 쿼리 작성(KakaoFunding#161)

--- src/main/java/org/kakaoshare/backend/domain/cart/repository/CartRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/cart/repository/CartRepository.java
@@ -1,5 +1,6 @@
 package org.kakaoshare.backend.domain.cart.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.kakaoshare.backend.domain.cart.entity.Cart;
 import org.kakaoshare.backend.domain.cart.repository.query.CartRepositoryCustom;
@@ -9,4 +10,5 @@ import org.springframework.data.jpa.repository.Query;
 public interface CartRepository extends JpaRepository<Cart,Long>, CartRepositoryCustom {
     @Query("SELECT c FROM Cart c WHERE c.member.memberId = :memberId AND c.product.productId = :productId")
     Optional<Cart> findByMemberIdAndProductId(Long memberId, Long productId);
+    List<Cart> findByMemberId(Long memberId);
 }
