62cd42b7a58ac4fdfd29c3ea7ebfbff2033fa192
hwan2-99 <thh1233123@gmail.com>
2024-05-17 22:12:44 +0900

    [feat] 장바구니에 상품 등록 컨트롤러 구현(KakaoFunding#161)

--- src/main/java/org/kakaoshare/backend/domain/cart/controller/CartController.java
+++ src/main/java/org/kakaoshare/backend/domain/cart/controller/CartController.java
@@ -1,6 +1,15 @@
 package org.kakaoshare.backend.domain.cart.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.kakaoshare.backend.domain.cart.dto.CartRegisterResponse;
+import org.kakaoshare.backend.domain.cart.service.CartService;
+import org.kakaoshare.backend.domain.funding.dto.RegisterRequest;
+import org.kakaoshare.backend.domain.funding.dto.RegisterResponse;
+import org.kakaoshare.backend.jwt.util.LoggedInMember;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/cart")
 @RequiredArgsConstructor
 public class CartController {
+    private final CartService cartService;
+    @PostMapping("/{productId}")
+    public ResponseEntity<?> registerItem(@PathVariable Long productId, @LoggedInMember String providerId) {
+        CartRegisterResponse response = cartService.registerItem(productId,providerId);
+        return ResponseEntity.ok(response);
+    }
 }
