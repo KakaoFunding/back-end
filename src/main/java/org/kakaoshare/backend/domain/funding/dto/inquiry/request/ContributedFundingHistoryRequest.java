3ce81068371283561aa2f3661e64c4d3ad5edeef
kmw2378 <rlarla677@gmail.com>
2024-05-17 23:09:32 +0900

    [refactor]ContributedFundingHistoryRequest 에 @NoArgsConstructor 추가 (#${closes}) @ModelAttribute 바인딩 시 기본 생성자를 사용하도록 어노테이션 추가

--- src/main/java/org/kakaoshare/backend/domain/funding/dto/inquiry/request/ContributedFundingHistoryRequest.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/dto/inquiry/request/ContributedFundingHistoryRequest.java
@@ -2,6 +2,7 @@ package org.kakaoshare.backend.domain.funding.dto.inquiry.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.kakaoshare.backend.common.util.EnumValue;
 import org.kakaoshare.backend.domain.funding.entity.FundingDetailStatus;
 import org.kakaoshare.backend.domain.funding.vo.FundingHistoryDate;
@@ -11,6 +12,7 @@ import java.time.LocalDate;
 
 @AllArgsConstructor
 @Data
+@NoArgsConstructor
 public final class ContributedFundingHistoryRequest {
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate startDate;
