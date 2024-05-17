2ba06f8875e84e5c8e34b58cdbea739dd73ff088
hwan2-99 <thh1233123@gmail.com>
2024-05-17 22:11:11 +0900

    [feat] 회원정보와 상품정보를 가지고 장바구니 조회 쿼리 작성(KakaoFunding#161)

--- src/main/java/org/kakaoshare/backend/domain/cart/repository/CartRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/cart/repository/CartRepository.java
@@ -1,8 +1,12 @@
 package org.kakaoshare.backend.domain.cart.repository;
 
+import java.util.Optional;
 import org.kakaoshare.backend.domain.cart.entity.Cart;
 import org.kakaoshare.backend.domain.cart.repository.query.CartRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CartRepository extends JpaRepository<Cart,Long>, CartRepositoryCustom {
+    @Query("SELECT c FROM Cart c WHERE c.member.memberId = :memberId AND c.product.productId = :productId")
+    Optional<Cart> findByMemberIdAndProductId(Long memberId, Long productId);
 }
