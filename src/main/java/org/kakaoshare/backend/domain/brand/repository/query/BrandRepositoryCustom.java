0ed77a8e4cb451a8c6021b8608398313f305801c
kmw2378 <rlarla677@gmail.com>
2024-05-20 15:11:19 +0900

    [refactor]BrandRepositoryCustom.findAllSimpleBrandByCategoryId() 반환 타입 수정  (#${closes}) Page<?>를 반환하지 않고 List<?>를 반환하도록 수정

--- src/main/java/org/kakaoshare/backend/domain/brand/repository/query/BrandRepositoryCustom.java
+++ src/main/java/org/kakaoshare/backend/domain/brand/repository/query/BrandRepositoryCustom.java
@@ -1,12 +1,11 @@
 package org.kakaoshare.backend.domain.brand.repository.query;
 
 import org.kakaoshare.backend.domain.brand.dto.SimpleBrandDto;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface BrandRepositoryCustom {
-    Page<SimpleBrandDto> findAllSimpleBrandByCategoryId(Long categoryId, Pageable pageable);
+    List<SimpleBrandDto> findAllSimpleBrandByCategoryId(Long categoryId);
     List<SimpleBrandDto> findBySearchConditions(final String keyword, final Pageable pageable);
 }
