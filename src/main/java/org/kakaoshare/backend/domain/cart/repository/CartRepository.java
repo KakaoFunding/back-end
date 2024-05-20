73fcd349c986c707d3624488880bb4db43073623
hwan2-99 <thh1233123@gmail.com>
2024-05-20 09:34:43 +0900

    [refactor] 장바구니 조회 쿼리 파라미터에 어노테이션 검증 추가(KakaoFunding#161)

--- src/main/java/org/kakaoshare/backend/domain/cart/repository/CartRepository.java
+++ src/main/java/org/kakaoshare/backend/domain/cart/repository/CartRepository.java
@@ -6,10 +6,11 @@ import org.kakaoshare.backend.domain.cart.entity.Cart;
 import org.kakaoshare.backend.domain.cart.repository.query.CartRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CartRepository extends JpaRepository<Cart,Long>, CartRepositoryCustom {
     @Query("SELECT c FROM Cart c WHERE c.member.memberId = :memberId AND c.product.productId = :productId")
-    Optional<Cart> findByMemberIdAndProductId(Long memberId, Long productId);
+    Optional<Cart> findByMemberIdAndProductId(@Param("memberId") Long memberId, @Param("productId") Long productId);
     List<Cart> findByMemberId(Long memberId);
     void deleteByMemberId(Long memberId);
 }
