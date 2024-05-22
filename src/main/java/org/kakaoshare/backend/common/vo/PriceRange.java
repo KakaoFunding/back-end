be07e0bd3f6d86b0a5fd788b0b5723fd8a7ca3d4
hwan2-99 <thh1233123@gmail.com>
2024-05-22 14:09:25 +0900

    [refactor] 가격범위 클래스 검증메소드 구현체에서 구현하도록 삭제(KakaoFunding#181)

--- src/main/java/org/kakaoshare/backend/common/vo/PriceRange.java
+++ src/main/java/org/kakaoshare/backend/common/vo/PriceRange.java
@@ -9,18 +9,7 @@ public abstract class PriceRange {
     private int minPrice;
     private int maxPrice;
     public PriceRange(final int minPrice, final int maxPrice) {
-        validatePrice(minPrice, maxPrice);
         this.minPrice = minPrice;
         this.maxPrice = maxPrice;
-    }
-
-    private void validatePrice(final int minPrice, final int maxPrice) {
-        if (minPrice > maxPrice) {
-            throw new IllegalArgumentException();
-        }
-
-        if (minPrice < 0) {
-            throw new IllegalArgumentException();
-        }
     }
 }
