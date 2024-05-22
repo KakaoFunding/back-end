d9a6ed9dc79b0e312c5eeb15069a6bbc07a076a4
hwan2-99 <thh1233123@gmail.com>
2024-05-23 02:37:11 +0900

    [refactor] 진행중인 펀딩 아이템 진행률 응답 dto 클래스에 펀딩아이템과 관련된 필드 추가(KakaoFunding#216)

--- src/main/java/org/kakaoshare/backend/domain/funding/dto/ProgressResponse.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/dto/ProgressResponse.java
@@ -2,9 +2,11 @@ package org.kakaoshare.backend.domain.funding.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.kakaoshare.backend.domain.brand.entity.Brand;
 import org.kakaoshare.backend.domain.funding.entity.Funding;
 
 import java.util.Optional;
+import org.kakaoshare.backend.domain.product.entity.Product;
 
 @Getter
 @Builder
@@ -20,17 +22,26 @@ public class ProgressResponse {
     private final Long remainAmount;
     private final Long goalAmount;
     private final Long accumulateAmount;
+    private final Long productId;
+    private final Long brandId;
+    private final String brandPhoto;
+    private final String productPhoto;
+    private final String brandName;
+    private final String productName;
+
+
 
     public static ProgressResponse from(Funding funding) {
         Long goalAmount = funding.getGoalAmount();
         Long accumulateAmount = funding.getAccumulateAmount();
         double progressRate = Optional.of(goalAmount)
                 .filter(goalAmountValue -> goalAmountValue.compareTo(ZERO) != 0)
-                .map(goalAmountValue ->
-                         divide(accumulateAmount,goalAmountValue, SCALE)
-                                * PERCENT_MULTIPLIER)
+                .map(goalAmountValue -> divide(accumulateAmount, goalAmountValue, SCALE) * PERCENT_MULTIPLIER)
                 .orElse(DEFAULT_PROGRESS_RATE);
-        Long remainAmount = goalAmount-accumulateAmount;
+        Long remainAmount = goalAmount - accumulateAmount;
+
+        Product product = funding.getProduct();
+        Brand brand = product.getBrand();
 
         return ProgressResponse.builder()
                 .fundingId(funding.getFundingId())
@@ -38,6 +49,12 @@ public class ProgressResponse {
                 .remainAmount(remainAmount)
                 .goalAmount(goalAmount)
                 .accumulateAmount(accumulateAmount)
+                .productId(product.getProductId())
+                .brandId(brand.getBrandId())
+                .brandPhoto(brand.getIconPhoto())
+                .productPhoto(product.getPhoto())
+                .brandName(brand.getName())
+                .productName(product.getName())
                 .build();
     }
     
