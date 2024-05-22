f04ad991849ce0939ac3a3acedee6d799ee358eb
hwan2-99 <thh1233123@gmail.com>
2024-05-22 14:14:40 +0900

    [refactor] 랭킹 조회에 사용되는 가격범위 클래스 검증메소드 구현체에서 사용하도록 수정(KakaoFunding#181)

--- src/main/java/org/kakaoshare/backend/domain/rank/dto/RankPriceRange.java
+++ src/main/java/org/kakaoshare/backend/domain/rank/dto/RankPriceRange.java
@@ -2,11 +2,19 @@ package org.kakaoshare.backend.domain.rank.dto;
 
 import lombok.Getter;
 import org.kakaoshare.backend.common.vo.PriceRange;
+import org.kakaoshare.backend.domain.rank.error.RankErrorCode;
+import org.kakaoshare.backend.domain.rank.error.exception.RankException;
+
 @Getter
 public class RankPriceRange extends PriceRange {
     public RankPriceRange(int minPrice, int maxPrice) {
         super(minPrice, maxPrice);
+        validatePriceRange(minPrice, maxPrice);
     }
 
-
+    private void validatePriceRange(int minPrice, int maxPrice) {
+        if (minPrice < 0 || minPrice > maxPrice) {
+            throw new RankException(RankErrorCode.INCORRECT_PRICE_RANGE);
+        }
+    }
 }
