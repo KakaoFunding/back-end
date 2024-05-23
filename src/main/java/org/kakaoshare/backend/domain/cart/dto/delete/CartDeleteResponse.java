4f311838bafa83c4d9db2dff4fa39a730feb33f1
hwan2-99 <thh1233123@gmail.com>
2024-05-23 09:01:22 +0900

    [refactor] 추상화된 응답 dto 클래스 구현 (KakaoFunding#181)

--- src/main/java/org/kakaoshare/backend/domain/cart/dto/delete/CartDeleteResponse.java
+++ src/main/java/org/kakaoshare/backend/domain/cart/dto/delete/CartDeleteResponse.java
@@ -2,16 +2,13 @@ package org.kakaoshare.backend.domain.cart.dto.delete;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.kakaoshare.backend.domain.cart.dto.CartIdResponse;
 import org.kakaoshare.backend.domain.cart.entity.Cart;
 
 @Getter
 @Builder
-public class CartDeleteResponse {
-    private Long cartId;
-
-    public static CartDeleteResponse from(Cart cart){
-        return CartDeleteResponse.builder()
-                .cartId(cart.getCartId())
-                .build();
+public class CartDeleteResponse extends CartIdResponse {
+    public CartDeleteResponse(Long cartId) {
+        super(cartId);
     }
 }
--- src/main/java/org/kakaoshare/backend/domain/cart/dto/register/CartRegisterResponse.java
+++ src/main/java/org/kakaoshare/backend/domain/cart/dto/register/CartRegisterResponse.java
@@ -2,16 +2,13 @@ package org.kakaoshare.backend.domain.cart.dto.register;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.kakaoshare.backend.domain.cart.dto.CartIdResponse;
 import org.kakaoshare.backend.domain.cart.entity.Cart;
 
 @Getter
 @Builder
-public class CartRegisterResponse {
-    private Long cartId;
-
-    public static CartRegisterResponse from(Cart cart){
-        return CartRegisterResponse.builder()
-                .cartId(cart.getCartId())
-                .build();
+public class CartRegisterResponse extends CartIdResponse {
+    public CartRegisterResponse(Long cartId) {
+        super(cartId);
     }
 }
