dd809a641d977116bd71b4d7a0b489942cef8b6a
hwan2-99 <thh1233123@gmail.com>
2024-05-23 09:51:01 +0900

    [feat] 카카오 친구목록 불러오는 메소드 작성 (KakaoFunding#216)

--- src/main/java/org/kakaoshare/backend/domain/member/service/oauth/OAuthWebClientService.java
+++ src/main/java/org/kakaoshare/backend/domain/member/service/oauth/OAuthWebClientService.java
@@ -1,6 +1,9 @@
 package org.kakaoshare.backend.domain.member.service.oauth;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.kakaoshare.backend.domain.member.dto.oauth.profile.detail.KakaoFriendListDto;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.stereotype.Service;
@@ -11,6 +14,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 @Service
 public class OAuthWebClientService {
+    private static final String FRIENDS_LIST_SERVICE_URL = "https://kapi.kakao.com/v1/api/talk/friends";
     private final WebClient webClient;
 
     public Map<String, Object> getSocialProfile(final ClientRegistration registration,
@@ -20,6 +24,25 @@ public class OAuthWebClientService {
                 .headers(header -> header.setBearerAuth(socialToken))
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
+                .block();
+    }
+
+    public List<KakaoFriendListDto> getFriendsList(String accessToken) {
+        return webClient.get()
+                .uri(FRIENDS_LIST_SERVICE_URL)
+                .headers(headers -> headers.setBearerAuth(accessToken))
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
+                .map(response -> (List<Map<String, Object>>) response.get("elements"))
+                .map(elements -> elements.stream()
+                        .map(element -> KakaoFriendListDto.builder()
+                                .id(element.get("id").toString())
+                                .uuid(element.get("uuid").toString())
+                                .favorite((Boolean) element.get("favorite"))
+                                .profileNickname(element.get("profile_nickname").toString())
+                                .profileThumbnailImage(element.get("profile_thumbnail_image").toString())
+                                .build())
+                        .collect(Collectors.toList()))
                 .block();
     }
 
