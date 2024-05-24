0f42f2d6b66df035259681fde5d66bbf18e9b8b4
hwan2-99 <thh1233123@gmail.com>
2024-05-25 02:30:13 +0900

    [refactor] 수정된 로직을 이용하여 테스트 코드 수정(KakaoFunding#25)

--- src/test/java/org/kakaoshare/backend/domain/product/service/ProductServiceTest.java
+++ src/test/java/org/kakaoshare/backend/domain/product/service/ProductServiceTest.java
@@ -1,13 +1,18 @@
 package org.kakaoshare.backend.domain.product.service;
 
 import jakarta.persistence.EntityNotFoundException;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.kakaoshare.backend.domain.member.entity.Member;
+import org.kakaoshare.backend.domain.member.repository.MemberRepository;
 import org.kakaoshare.backend.domain.product.dto.DescriptionResponse;
 import org.kakaoshare.backend.domain.product.dto.DetailResponse;
 import org.kakaoshare.backend.domain.product.entity.Product;
+import org.kakaoshare.backend.domain.product.exception.ProductException;
 import org.kakaoshare.backend.domain.product.repository.ProductRepository;
+import org.kakaoshare.backend.fixture.MemberFixture;
 import org.kakaoshare.backend.fixture.ProductFixture;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -21,7 +26,8 @@ import static org.mockito.Mockito.*;
 public class ProductServiceTest {
     @Mock
     private ProductRepository productRepository;
-
+    @Mock
+    private MemberRepository memberRepository;
     @InjectMocks
     private ProductService productService;
 
@@ -30,18 +36,20 @@ public class ProductServiceTest {
     @Test
     @DisplayName("상품 상세정보 조회 성공")
     void getProductDetail_Success() {
+        Member member = MemberFixture.KAKAO.생성();
         final Product product = ProductFixture.TEST_PRODUCT.생성();
         final Long productId = product.getProductId();
 
         DetailResponse expectedDetailResponse = DetailResponse.builder()
                 .deliverDescription("배송 설명")
                 .build();
-
+        when(memberRepository.findMemberByProviderId(member.getProviderId()))
+                .thenReturn(Optional.of(member));
         doReturn(expectedDetailResponse)
                 .when(productRepository)
-                .findProductDetail(productId);
+                .findProductDetail(productId, member);
 
-        final DetailResponse actual = productService.getProductDetail(productId);
+        final DetailResponse actual = productService.getProductDetail(productId, member.getProviderId());
 
         assertEquals(expectedDetailResponse.getDeliverDescription(), actual.getDeliverDescription());
     }
@@ -49,30 +57,32 @@ public class ProductServiceTest {
     @Test
     @DisplayName("상품 상세설명 조회 성공")
     void getProductDescription_Success() {
+        Member member = MemberFixture.KAKAO.생성();
         final Long productId = 1L;
         DescriptionResponse expectedDescriptionResponse = DescriptionResponse.builder()
                 .description("설명")
                 .build();
 
+        when(memberRepository.findMemberByProviderId(member.getProviderId()))
+                .thenReturn(Optional.of(member));
         doReturn(expectedDescriptionResponse)
                 .when(productRepository)
-                .findProductWithDetailsAndPhotos(productId);
+                .findProductWithDetailsAndPhotos(productId,member);
 
-        DescriptionResponse actualDescriptionResponse = productService.getProductDescription(productId);
+        DescriptionResponse actualDescriptionResponse = productService.getProductDescription(productId,member.getProviderId());
 
         assertEquals(expectedDescriptionResponse, actualDescriptionResponse);
-        verify(productRepository).findProductWithDetailsAndPhotos(productId);
+        verify(productRepository).findProductWithDetailsAndPhotos(productId,member);
     }
 
     @Test
     @DisplayName("존재하지 않는 상품 ID로 조회 시 예외 발생")
     void getProductDetail_WhenProductNotFound_ThenThrowException() {
         final Long nonExistingProductId = 999L;
+        when(productRepository.findProductWithDetailsAndPhotos(nonExistingProductId, null))
+                .thenReturn(null);
 
-        when(productRepository.findProductWithDetailsAndPhotos(nonExistingProductId)).thenReturn(null);
-
-        assertThatThrownBy(() -> productService.getProductDescription(nonExistingProductId))
-                .isInstanceOf(EntityNotFoundException.class)
-                .hasMessageContaining("Product not found with id: " + nonExistingProductId);
+        assertThatThrownBy(() -> productService.getProductDescription(nonExistingProductId, null))
+                .isInstanceOf(ProductException.class);
     }
 }
