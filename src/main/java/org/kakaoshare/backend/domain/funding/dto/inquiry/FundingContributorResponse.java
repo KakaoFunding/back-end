ec9447029bac1d64f6e63890a76f0a6f0832ac73
hwan2-99 <thh1233123@gmail.com>
2024-05-24 12:18:07 +0900

    [feat] 펀딩 기여한 사람 조회 응답 dto 클래스 정적팩토리 메소드 구현(KakaoFunding#216)

--- src/main/java/org/kakaoshare/backend/domain/funding/dto/inquiry/FundingContributorResponse.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/dto/inquiry/FundingContributorResponse.java
@@ -2,6 +2,7 @@ package org.kakaoshare.backend.domain.funding.dto.inquiry;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.kakaoshare.backend.domain.friend.dto.response.Friend;
 
 @Getter
 @Builder
@@ -9,4 +10,18 @@ public class FundingContributorResponse {
     private String profilePhoto;
     private String profileName;
     private Long contributedAmount;
+    private Double contributedPercentage;
+
+    public static FundingContributorResponse of(
+            String profilePhoto,
+            String profileName,
+            Long contributedAmount,
+            Double contributionPercentage) {
+        return FundingContributorResponse.builder()
+                .profilePhoto(profilePhoto)
+                .profileName(profileName)
+                .contributedAmount(contributedAmount)
+                .contributedPercentage(contributionPercentage)
+                .build();
+    }
 }
