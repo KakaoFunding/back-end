271500e5dfff7daf5f6ade3b80ad3080e16699bf
kmw2378 <rlarla677@gmail.com>
2024-05-25 23:16:05 +0900

    [refactor]카카오페이 결제 준비 과정에서 요청값 로깅 추가 (#${closes})

--- src/main/java/org/kakaoshare/backend/domain/payment/service/KakaoPayRequestProvider.java
+++ src/main/java/org/kakaoshare/backend/domain/payment/service/KakaoPayRequestProvider.java
@@ -1,5 +1,6 @@
 package org.kakaoshare.backend.domain.payment.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.kakaoshare.backend.domain.payment.dto.approve.request.KakaoPayApproveRequest;
 import org.kakaoshare.backend.domain.payment.dto.cancel.request.PaymentCancelDto;
 import org.kakaoshare.backend.domain.payment.dto.kakaopay.cancel.request.KakaoPayCancelRequest;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
+@Slf4j
 public class KakaoPayRequestProvider {
     private static final String PRODUCT_NAME_SEPARATOR = " 외 ";
     private static final String PRODUCT_NAME_SUFFIX = "건";
@@ -26,6 +28,7 @@ public class KakaoPayRequestProvider {
                                    @Value("${pay.redirect-url.approval}") final String approvalUrl,
                                    @Value("${pay.redirect-url.cancel}") final String cancelUrl,
                                    @Value("${pay.redirect-url.fail}") final String failUrl) {
+        log.info("approvalUrl = {}", approvalUrl);
         this.cid = cid;
         this.cidSecret = cidSecret;
         this.approvalUrl = approvalUrl;
