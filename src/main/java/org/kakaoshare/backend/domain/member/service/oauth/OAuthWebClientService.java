28943e2596e9c6fcac9af0767a4f2efd81d74fa4
kmw2378 <rlarla677@gmail.com>
2024-05-26 22:36:27 +0900

    [feat]OAuthWebClientService 인터페이스화 및 KakaoOAuthService 구현체 추가 (#236) 추후 다른 소셜 로그인을 대비하여 OCP 원칙을 준수하는 설계 구현

--- src/main/java/org/kakaoshare/backend/domain/member/service/oauth/OAuthWebClientService.java
+++ src/main/java/org/kakaoshare/backend/domain/member/service/oauth/OAuthWebClientService.java
@@ -1,31 +1,12 @@
 package org.kakaoshare.backend.domain.member.service.oauth;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.core.ParameterizedTypeReference;
+import org.kakaoshare.backend.domain.member.dto.oauth.logout.OAuthSocialLogoutRequest;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Map;
 
-@RequiredArgsConstructor
-@Service
-public class OAuthWebClientService {
-    private final WebClient webClient;
+public interface OAuthWebClientService {
+    Map<String, Object> getSocialProfile(final ClientRegistration registration, final String socialToken);
 
-    public Map<String, Object> getSocialProfile(final ClientRegistration registration,
-                                                final String socialToken) {
-        return webClient.get()
-                .uri(getProfileRequestUri(registration))
-                .headers(header -> header.setBearerAuth(socialToken))
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
-                .block();
-    }
-
-    private String getProfileRequestUri(final ClientRegistration registration) {
-        return registration.getProviderDetails()
-                .getUserInfoEndpoint()
-                .getUri();
-    }
+    void expireToken(final ClientRegistration registration, final OAuthSocialLogoutRequest oAuthSocialLogoutRequest);
 }
