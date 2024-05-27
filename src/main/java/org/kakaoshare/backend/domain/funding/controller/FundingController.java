a62e2f6b8cda909c4b489c9f4b48a60a4e133c57
kmw2378 <rlarla677@gmail.com>
2024-05-27 20:16:25 +0900

    [fix]충돌 해결 (#227)

--- src/main/java/org/kakaoshare/backend/domain/funding/controller/FundingController.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/controller/FundingController.java
@@ -1,15 +1,22 @@
 package org.kakaoshare.backend.domain.funding.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.kakaoshare.backend.common.dto.PageResponse;
+import org.kakaoshare.backend.domain.funding.dto.FriendFundingItemRequest;
+import org.kakaoshare.backend.domain.funding.dto.FundingResponse;
 import org.kakaoshare.backend.domain.funding.dto.FundingSliceResponse;
 import org.kakaoshare.backend.domain.funding.dto.ProgressResponse;
 import org.kakaoshare.backend.domain.funding.dto.RegisterRequest;
 import org.kakaoshare.backend.domain.funding.dto.RegisterResponse;
+import org.kakaoshare.backend.domain.funding.dto.inquiry.FundingContributorResponse;
 import org.kakaoshare.backend.domain.funding.dto.preview.request.FundingPreviewRequest;
 import org.kakaoshare.backend.domain.funding.dto.preview.response.FundingPreviewResponse;
 import org.kakaoshare.backend.domain.funding.entity.FundingStatus;
+import org.kakaoshare.backend.domain.funding.service.FundingDetailService;
 import org.kakaoshare.backend.domain.funding.service.FundingService;
 import org.kakaoshare.backend.jwt.util.LoggedInMember;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +24,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class FundingController {
     private final FundingService fundingService;
+    private final FundingDetailService fundingDetailService;
     private static final int FUNDING_DEFAULT_SIZE = 20;
 
     @PostMapping("/funding/{productId}")
@@ -35,10 +44,17 @@ public class FundingController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/members/funding/{fundingId}")
+    @GetMapping("/funding/{fundingId}")
     public ResponseEntity<?> getFundingProgress(@PathVariable Long fundingId, @LoggedInMember String providerId) {
         ProgressResponse response = fundingService.getFundingProgress(fundingId, providerId);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/funding/friends")
+    public ResponseEntity<?> getFriendsActiveFundingItems(@LoggedInMember String providerId,
+                                                          @RequestBody FriendFundingItemRequest fundingRequest) {
+        List<FundingResponse> activeFundings = fundingService.getFriendsActiveFundingItems(providerId, fundingRequest);
+        return ResponseEntity.ok(activeFundings);
     }
 
     @GetMapping("/members/funding/products")
@@ -53,5 +69,16 @@ public class FundingController {
     public ResponseEntity<?> preview(@RequestBody final FundingPreviewRequest fundingPreviewRequest) {
         final FundingPreviewResponse fundingPreviewResponse = fundingService.preview(fundingPreviewRequest);
         return ResponseEntity.ok(fundingPreviewResponse);
+    }
+
+    @GetMapping("/{fundingId}/contributors")
+    public ResponseEntity<?> getTopContributors(@PathVariable Long fundingId,
+                                                @PageableDefault(size = 5) Pageable pageable,
+                                                @RequestHeader("Authorization") String accessToken) {
+
+        accessToken = accessToken.substring("Bearer ".length());
+        PageResponse<?> contributors = fundingDetailService.getTopContributors(fundingId, pageable,
+                accessToken);
+        return ResponseEntity.ok(contributors);
     }
 }
--- src/test/java/org/kakaoshare/backend/domain/funding/service/FundingServiceTest.java
+++ src/test/java/org/kakaoshare/backend/domain/funding/service/FundingServiceTest.java
@@ -1,90 +1,87 @@
-//package org.kakaoshare.backend.domain.funding.service;
-//
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.kakaoshare.backend.domain.funding.repository.FundingRepository;
-//import org.kakaoshare.backend.domain.member.repository.MemberRepository;
-//import org.kakaoshare.backend.domain.product.repository.ProductRepository;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//@ExtendWith(MockitoExtension.class)
-//public class FundingServiceTest {
-//
-//    @InjectMocks
-//    private FundingService fundingService;
-//
-//    @Mock
-//    private ProductRepository productRepository;
-//
-//    @Mock
-//    private MemberRepository memberRepository;
-//
-//    @Mock
-//    private FundingRepository fundingRepository;
-//
-//    @Test
-//    @DisplayName("성공적으로 펀딩 아이템 등록")
-//    void registerFundingItem_Success() {
-//        Long productId = 1L;
-//        String providerId = "provider123";
-//        RegisterRequest request = RegisterRequest.builder()
-//                .goalAmount(new BigDecimal("1000"))
-//                .expiredAt(LocalDate.now().plusDays(30))
-//                .build();
-//
-//        Product product = ProductFixture.TEST_PRODUCT.생성();
-//        Member member = MemberFixture.KAKAO.생성();
-//        Funding funding = FundingFixture.SAMPLE_FUNDING.생성(member, product);
-//
-//        given(productRepository.findById(productId)).willReturn(Optional.of(product));
-//        given(memberRepository.findMemberByProviderId(providerId)).willReturn(Optional.of(member));
-//        given(fundingRepository.findByIdAndMemberId(any(), any())).willReturn(Optional.empty());
-//        given(fundingRepository.save(any(Funding.class))).willReturn(funding);
-//
-//        RegisterResponse response = fundingService.registerFundingItem(productId, providerId, request);
-//
-//        assertThat(response).isNotNull();
-//        assertThat(response.getId()).isEqualTo(funding.getFundingId());
-//    }
-//
-//    @Test
-//    @DisplayName("펀딩 진행 상황 조회 성공")
-//    void getFundingProgress_Success() {
-//        Long fundingId = 1L;
-//        Member member = MemberFixture.KAKAO.생성();
-//        Product product = ProductFixture.TEST_PRODUCT.생성();
-//        Funding funding = FundingFixture.SAMPLE_FUNDING.생성(member, product);
-//
-//        given(memberRepository.findMemberByProviderId(member.getProviderId())).willReturn(Optional.of(member));
-//        given(fundingRepository.findByIdAndMemberId(fundingId, member.getMemberId())).willReturn(Optional.of(funding));
-//
-//        ProgressResponse response = fundingService.getFundingProgress(fundingId, member.getProviderId());
-//
-//        assertThat(response).isNotNull();
-//
-//        verify(memberRepository).findMemberByProviderId(member.getProviderId());
-//        verify(fundingRepository).findByIdAndMemberId(fundingId, member.getMemberId());
-//    }
-//
-//    @Test
-//    @DisplayName("내가 등록한 펀딩아이템 조회 성공")
-//    void getMyAllFundingItems_Success() {
-//        Pageable pageable = PageRequest.of(0, 10);
-//        Member member = MemberFixture.KAKAO.생성();
-//        Brand brand = BrandFixture.BRAND_C.생성();
-//        Product product = ProductFixture.TEST_PRODUCT.생성(brand);
-//        List<Funding> fundingList = Arrays.asList(FundingFixture.SAMPLE_FUNDING.생성(member, product),
-//                FundingFixture.SAMPLE_FUNDING2.생성(member, product));
-//        Slice<Funding> fundingSlice = new SliceImpl<>(fundingList, pageable, false);
-//
-//        when(memberRepository.findMemberByProviderId(member.getProviderId())).thenReturn(Optional.of(member));
-//        when(fundingRepository.findAllByMemberId(member.getMemberId())).thenReturn(fundingList);
-//        when(fundingRepository.findFundingByMemberIdWithSlice(member.getMemberId(), pageable)).thenReturn(fundingSlice);
-//
-//        FundingSliceResponse response = fundingService.getMyAllFundingProducts(member.getProviderId(), pageable);
-//
-//        assertThat(response).isNotNull();
-//        assertThat(response.getFundingItems()).hasSize(fundingList.size());
-//    }
-//}
+package org.kakaoshare.backend.domain.funding.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.kakaoshare.backend.domain.brand.entity.Brand;
+import org.kakaoshare.backend.domain.funding.dto.ProgressResponse;
+import org.kakaoshare.backend.domain.funding.dto.RegisterRequest;
+import org.kakaoshare.backend.domain.funding.dto.RegisterResponse;
+import org.kakaoshare.backend.domain.funding.entity.Funding;
+import org.kakaoshare.backend.domain.funding.repository.FundingRepository;
+import org.kakaoshare.backend.domain.member.entity.Member;
+import org.kakaoshare.backend.domain.member.repository.MemberRepository;
+import org.kakaoshare.backend.domain.product.entity.Product;
+import org.kakaoshare.backend.domain.product.repository.ProductRepository;
+import org.kakaoshare.backend.fixture.BrandFixture;
+import org.kakaoshare.backend.fixture.FundingFixture;
+import org.kakaoshare.backend.fixture.MemberFixture;
+import org.kakaoshare.backend.fixture.ProductFixture;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+public class FundingServiceTest {
+
+    @InjectMocks
+    private FundingService fundingService;
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private FundingRepository fundingRepository;
+
+    @Test
+    @DisplayName("성공적으로 펀딩 아이템 등록")
+    void registerFundingItem_Success() {
+        Long productId = 1L;
+        String providerId = "provider123";
+        RegisterRequest request = new RegisterRequest(1000L, LocalDate.now().plusDays(30));
+
+        Product product = ProductFixture.TEST_PRODUCT.생성();
+        Member member = MemberFixture.KAKAO.생성();
+        Funding funding = FundingFixture.SAMPLE_FUNDING.생성(member, product);
+
+        given(productRepository.findById(productId)).willReturn(Optional.of(product));
+        given(memberRepository.findMemberByProviderId(providerId)).willReturn(Optional.of(member));
+        given(fundingRepository.save(any(Funding.class))).willReturn(funding);
+
+        RegisterResponse response = fundingService.registerFundingItem(productId, providerId, request);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getId()).isEqualTo(funding.getFundingId());
+    }
+
+    @Test
+    @DisplayName("펀딩 진행 상황 조회 성공")
+    void getFundingProgress_Success() {
+        Long fundingId = 1L;
+        Brand brand = BrandFixture.EDIYA.생성(1L);
+        Member member = MemberFixture.KAKAO.생성();
+        Product product = ProductFixture.TEST_PRODUCT.생성(1L,brand);
+        Funding funding = FundingFixture.SAMPLE_FUNDING.생성(member, product);
+
+        given(memberRepository.findMemberByProviderId(member.getProviderId())).willReturn(Optional.of(member));
+        given(fundingRepository.findByIdAndMemberId(fundingId, member.getMemberId())).willReturn(Optional.of(funding));
+
+        ProgressResponse response = fundingService.getFundingProgress(fundingId, member.getProviderId());
+
+        assertThat(response).isNotNull();
+
+        verify(memberRepository).findMemberByProviderId(member.getProviderId());
+        verify(fundingRepository).findByIdAndMemberId(fundingId, member.getMemberId());
+    }
+
+}
