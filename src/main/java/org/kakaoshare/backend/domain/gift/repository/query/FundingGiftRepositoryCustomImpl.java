f4f278fae9a0c2231c2ea03704bba87a91430c13
kmw2378 <rlarla677@gmail.com>
2024-05-28 23:23:47 +0900

    [feat]FundingGiftRepositoryCustomImpl 추가 (#249)

---
+++ src/main/java/org/kakaoshare/backend/domain/gift/repository/query/FundingGiftRepositoryCustomImpl.java
@@ -0,0 +1,77 @@
+package org.kakaoshare.backend.domain.gift.repository.query;
+
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.kakaoshare.backend.domain.gift.dto.funding.inquiry.response.FundingGiftHistoryResponse;
+import org.kakaoshare.backend.domain.gift.dto.funding.inquiry.response.QFundingGiftHistoryResponse;
+import org.kakaoshare.backend.domain.product.dto.QProductDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import static org.kakaoshare.backend.common.util.RepositoryUtils.createOrderSpecifiers;
+import static org.kakaoshare.backend.common.util.RepositoryUtils.eqExpression;
+import static org.kakaoshare.backend.common.util.RepositoryUtils.toPage;
+import static org.kakaoshare.backend.domain.funding.entity.QFunding.funding;
+import static org.kakaoshare.backend.domain.gift.entity.QFundingGift.fundingGift;
+import static org.kakaoshare.backend.domain.member.entity.QMember.member;
+import static org.kakaoshare.backend.domain.product.entity.QProduct.product;
+
+@RequiredArgsConstructor
+public class FundingGiftRepositoryCustomImpl implements FundingGiftRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Page<FundingGiftHistoryResponse> findHistoryByCondition(final String providerId, final String status, final Pageable pageable) {
+        final JPAQuery<FundingGiftHistoryResponse> contentQuery = createHistoryContentQuery(providerId, status, pageable);
+        final JPAQuery<Long> countQuery = createHistoryCountQuery(providerId, status, pageable);
+        return toPage(pageable, contentQuery, countQuery);
+    }
+
+    @Override
+    public Page<FundingGiftHistoryResponse> findHistoryByConditionWithoutStatus(final String providerId, final Pageable pageable) {
+        // TODO 5/28 eqExpression()에서 2번째 파라미터가 null인 경우 true고 간주하여 status에 null을 대입
+        final JPAQuery<FundingGiftHistoryResponse> contentQuery = createHistoryContentQuery(providerId, null, pageable);
+        final JPAQuery<Long> countQuery = createHistoryCountQuery(providerId, null, pageable);
+        return toPage(pageable, contentQuery, countQuery);
+    }
+
+    private JPAQuery<Long> createHistoryCountQuery(final String providerId, final String status, final Pageable pageable) {
+        return createHistoryBaseQuery(providerId, status, pageable)
+                .select(fundingGift.count());
+    }
+
+    private JPAQuery<FundingGiftHistoryResponse> createHistoryContentQuery(final String providerId, final String status, final Pageable pageable) {
+        return createHistoryBaseQuery(providerId, status, pageable)
+                .select(getFundingGiftHistoryResponse())
+                .orderBy(createOrderSpecifiers(fundingGift, pageable));
+    }
+
+    private JPAQuery<?> createHistoryBaseQuery(final String providerId, final String status, final Pageable pageable) {
+        return queryFactory.from(fundingGift)
+                .innerJoin(fundingGift.funding, funding)
+                .innerJoin(funding.member, member)
+                .where(
+                        eqExpression(member.providerId, providerId),
+                        eqExpression(fundingGift.status.stringValue(), status)
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize());
+    }
+
+    private QFundingGiftHistoryResponse getFundingGiftHistoryResponse() {
+        // TODO 5/28 펀딩 등록시 수량이 고려되지 않아 수량을 1로 고정
+        return new QFundingGiftHistoryResponse(getProductDto(), Expressions.ONE, fundingGift.createdAt, fundingGift.status.stringValue());
+    }
+
+    private QProductDto getProductDto() {
+        return new QProductDto(
+                product.productId,
+                product.name,
+                product.photo,
+                product.price,
+                product.brandName
+        );
+    }
+}
