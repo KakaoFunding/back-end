21401996f997b1fd4014cdaa8bc384a1ff56f25d
kmw2378 <rlarla677@gmail.com>
2024-05-28 23:26:31 +0900

    [feat]initial_1_member.sql 추가 (#249) Repository 테스트 시 테스트 DB에 회원 레코드를 insert 하는 SQL문 추가

---
+++ src/main/resources/testdata/initial_1_member.sql
@@ -0,0 +1,11 @@
+INSERT INTO member (member_id, gender, name, phone_number, provider_id, role, profile_image_url)
+VALUES (1, 'MALE', '김민우', '010-1111-1111', 'providerId1', 'USER',
+        'http://t1.kakaocdn.net/account_images/default_profile.jpeg.twg.thumb.R640x640');
+
+INSERT INTO member (member_id, gender, name, phone_number, provider_id, role, profile_image_url)
+VALUES (2, 'MALE', '신예찬', '010-2222-2222', 'providerId2', 'USER',
+        'http://t1.kakaocdn.net/account_images/default_profile.jpeg.twg.thumb.R640x640');
+
+INSERT INTO member (member_id, gender, name, phone_number, provider_id, role, profile_image_url)
+VALUES (3, 'MALE', '김태환', '010-3333-3333', 'providerId3', 'USER',
+        'http://t1.kakaocdn.net/account_images/default_profile.jpeg.twg.thumb.R640x640');
