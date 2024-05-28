02327908c329c0e5ffa0595b7868a07b96da1d5f
kmw2378 <rlarla677@gmail.com>
2024-05-28 14:18:11 +0900

    [refactor]flyway ddl V7 수정

--- src/main/resources/db/migration/V7__modify_funding_idx.sql
+++ src/main/resources/db/migration/V7__modify_funding_idx.sql
@@ -1,8 +1,8 @@
 ALTER TABLE funding
-    DROP FOREIGN KEY `FK308j7d5ln7xaq590xo8bxwul0`;
+    DROP FOREIGN KEY `fk_funding_member`;
 
 ALTER TABLE funding
-    DROP FOREIGN KEY `FK5cxch4qfn9ynsvod79uyulcvj`;
+    DROP FOREIGN KEY `fk_funding_product`;
 
 ALTER TABLE funding
     DROP INDEX `idx_funding_member_id`;
@@ -11,11 +11,11 @@ ALTER TABLE funding
     DROP INDEX `idx_funding_product_id`;
 
 ALTER TABLE funding
-    ADD CONSTRAINT `FK308j7d5ln7xaq590xo8bxwul0`
+    ADD CONSTRAINT `fk_funding_member`
         FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`);
 
 ALTER TABLE funding
-    ADD CONSTRAINT `FK5cxch4qfn9ynsvod79uyulcvj`
+    ADD CONSTRAINT `fk_funding_product`
         FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`);
 
 ALTER TABLE funding
