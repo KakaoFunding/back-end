df2a3e662694f52d047dbbbbefc009f2c39a5f0b
kmw2378 <rlarla677@gmail.com>
2024-05-29 19:56:41 +0900

    [test]FundingGiftRepository 테스트 코드 수정 (#249)

--- src/test/java/org/kakaoshare/backend/domain/gift/repository/FundingGiftRepositoryTest.java
+++ src/test/java/org/kakaoshare/backend/domain/gift/repository/FundingGiftRepositoryTest.java
@@ -3,17 +3,26 @@ package org.kakaoshare.backend.domain.gift.repository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.kakaoshare.backend.common.RepositoryTest;
 import org.kakaoshare.backend.domain.gift.dto.funding.inquiry.response.FundingGiftHistoryResponse;
+import org.kakaoshare.backend.domain.gift.entity.GiftStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.kakaoshare.backend.domain.gift.entity.GiftStatus.CANCEL_REFUND;
+import static org.kakaoshare.backend.domain.gift.entity.GiftStatus.NOT_USED;
+import static org.kakaoshare.backend.domain.gift.entity.GiftStatus.USED;
+import static org.kakaoshare.backend.domain.gift.entity.GiftStatus.USING;
 
 @RepositoryTest
 class FundingGiftRepositoryTest {
@@ -22,6 +31,13 @@ class FundingGiftRepositoryTest {
 
     private Pageable pageable;
 
+    static Stream<Arguments> statusData() {
+        return Stream.of(
+                Arguments.of("providerId1", List.of(USING, NOT_USED)),
+                Arguments.of("providerId1", List.of(USED, CANCEL_REFUND))
+        );
+    }
+
     @BeforeEach
     public void setUp() {
         pageable = PageRequest.of(0, 20, Sort.by("createdAt"));
@@ -29,19 +45,12 @@ class FundingGiftRepositoryTest {
 
     @ParameterizedTest
     @DisplayName("나의 펀딩 선물함 조회 (상태 필터링 O)")
-    @CsvSource(value = {
-            "providerId1,USED",
-            "providerId1,USING",
-            "providerId2,USED",
-            "providerId2,USING",
-            "providerId3,USED",
-            "providerId3,USING"
-    })
-    public void findHistoryByStatus(final String providerId, final String status) throws Exception {
-        final Page<FundingGiftHistoryResponse> page = fundingGiftRepository.findHistoryByCondition(providerId, status, pageable);
+    @MethodSource("statusData")
+    public void findHistoryByStatus(final String providerId, final List<GiftStatus> statuses) throws Exception {
+        final Page<FundingGiftHistoryResponse> page = fundingGiftRepository.findHistoryByCondition(providerId, statuses, pageable);
         final boolean isAllSameStatus = page.getContent()
                 .stream()
-                .allMatch(fundingGiftHistoryResponse -> fundingGiftHistoryResponse.status().equals(status));
+                .allMatch(fundingGiftHistoryResponse -> statuses.contains(GiftStatus.valueOf(fundingGiftHistoryResponse.status())));
         assertThat(isAllSameStatus).isTrue();
     }
 
