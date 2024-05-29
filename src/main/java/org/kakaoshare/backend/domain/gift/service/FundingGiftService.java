f49e908c592bb9bedd04f0da06ff5bf1c7951144
kmw2378 <rlarla677@gmail.com>
2024-05-29 19:56:26 +0900

    [refactor]FundingGiftService.lookUp() 시그니처 및 구현부 수정 (#249) GiftStatusConstraint 를 List<GiftStatus> 로 매핑하여 로직을 수행하도록 수정

--- src/main/java/org/kakaoshare/backend/domain/gift/service/FundingGiftService.java
+++ src/main/java/org/kakaoshare/backend/domain/gift/service/FundingGiftService.java
@@ -3,12 +3,16 @@ package org.kakaoshare.backend.domain.gift.service;
 import com.querydsl.core.util.StringUtils;
 import lombok.RequiredArgsConstructor;
 import org.kakaoshare.backend.common.dto.PageResponse;
+import org.kakaoshare.backend.domain.gift.controller.GiftStatusConstraint;
 import org.kakaoshare.backend.domain.gift.dto.funding.inquiry.request.FundingGiftHistoryRequest;
+import org.kakaoshare.backend.domain.gift.entity.GiftStatus;
 import org.kakaoshare.backend.domain.gift.repository.FundingGiftRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -19,18 +23,19 @@ public class FundingGiftService {
     public PageResponse<?> lookUp(final String providerId,
                                   final FundingGiftHistoryRequest fundingGiftHistoryRequest,
                                   final Pageable pageable) {
-        final String status = fundingGiftHistoryRequest.getStatus();
-        final Page<?> page = getFundingGiftHistoryResponse(providerId, status, pageable);
+        final String statusParam = fundingGiftHistoryRequest.getStatus();
+        final Page<?> page = getFundingGiftHistoryResponse(providerId, statusParam, pageable);
         return PageResponse.from(page);
     }
 
     private Page<?> getFundingGiftHistoryResponse(final String providerId,
-                                                  final String status,
+                                                  final String statusParam,
                                                   final Pageable pageable) {
-        if (StringUtils.isNullOrEmpty(status)) {
+        if (StringUtils.isNullOrEmpty(statusParam)) {
             return fundingGiftRepository.findHistoryByConditionWithoutStatus(providerId, pageable);
         }
 
-        return fundingGiftRepository.findHistoryByCondition(providerId, status, pageable);
+        final List<GiftStatus> giftStatuses = GiftStatusConstraint.findByParam(statusParam);
+        return fundingGiftRepository.findHistoryByCondition(providerId, giftStatuses, pageable);
     }
 }
