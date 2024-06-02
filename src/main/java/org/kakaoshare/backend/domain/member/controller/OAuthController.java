0dd3ebc95536d401848a37c3da855f154fce1252
kmw2378 <rlarla677@gmail.com>
2024-06-02 21:04:46 +0900

    [refactor]로그인 요청 파라미터 대신 Body 사용 (#247)

--- src/main/java/org/kakaoshare/backend/domain/member/controller/OAuthController.java
+++ src/main/java/org/kakaoshare/backend/domain/member/controller/OAuthController.java
@@ -1,5 +1,6 @@
 package org.kakaoshare.backend.domain.member.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.kakaoshare.backend.domain.member.dto.oauth.authenticate.OAuthLoginRequest;
 import org.kakaoshare.backend.domain.member.dto.oauth.authenticate.OAuthLoginResponse;
@@ -19,7 +20,7 @@ public class OAuthController {
     private final RefreshTokenCookieProvider refreshTokenCookieProvider;
 
     @PostMapping("/login")
-    public ResponseEntity<OAuthLoginResponse> login(@ModelAttribute final OAuthLoginRequest oAuthLoginRequest) {
+    public ResponseEntity<OAuthLoginResponse> login(@RequestBody @Valid final OAuthLoginRequest oAuthLoginRequest) {
         return ResponseEntity.ok()
                 .body(oAuthService.login(oAuthLoginRequest));
     }
