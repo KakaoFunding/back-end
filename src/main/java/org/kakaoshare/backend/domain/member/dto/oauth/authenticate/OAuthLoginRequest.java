141119c5e6c1467fd4ac8997c3a29810b17df9aa
kmw2378 <rlarla677@gmail.com>
2024-06-02 21:04:18 +0900

    [feat]OAuthLoginRequest 검증 어노테이션 추가 (#247)

--- src/main/java/org/kakaoshare/backend/domain/member/dto/oauth/authenticate/OAuthLoginRequest.java
+++ src/main/java/org/kakaoshare/backend/domain/member/dto/oauth/authenticate/OAuthLoginRequest.java
@@ -1,4 +1,8 @@
 package org.kakaoshare.backend.domain.member.dto.oauth.authenticate;
 
-public record OAuthLoginRequest(String provider, String socialAccessToken) {
-}
+import jakarta.validation.constraints.NotNull;
+
+public record OAuthLoginRequest(
+        @NotNull(message = "소셜 이름은 필수입니다.") String provider,
+        @NotNull(message = "소셜 리프레시 토큰은 필수입니다.") String socialAccessToken
+) {}
