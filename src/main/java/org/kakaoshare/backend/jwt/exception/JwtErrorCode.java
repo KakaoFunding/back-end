81c5b6b6536f2271d2abea562879f2d1e3037ade
kmw2378 <rlarla677@gmail.com>
2024-06-04 18:53:17 +0900

    [chore]공백 제거

--- src/main/java/org/kakaoshare/backend/jwt/exception/JwtErrorCode.java
+++ src/main/java/org/kakaoshare/backend/jwt/exception/JwtErrorCode.java
@@ -6,10 +6,10 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum JwtErrorCode implements ErrorCode {
-    NOT_FOUND(CODE_PREFIX + "001", HttpStatus.UNAUTHORIZED,"토큰을 찾을 수 없습니다."),
-    INVALID(CODE_PREFIX + "002", HttpStatus.UNAUTHORIZED,"유효하지 않은 토큰입니다."),
-    EXPIRED(CODE_PREFIX + "003", HttpStatus.UNAUTHORIZED,"만료된 토큰입니다."),
-    UNSUPPORTED(CODE_PREFIX + "004", HttpStatus.UNAUTHORIZED,"JWT를 지원하지 않습니다.");
+    NOT_FOUND(CODE_PREFIX + "001", HttpStatus.UNAUTHORIZED, "토큰을 찾을 수 없습니다."),
+    INVALID(CODE_PREFIX + "002", HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+    EXPIRED(CODE_PREFIX + "003", HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    UNSUPPORTED(CODE_PREFIX + "004", HttpStatus.UNAUTHORIZED, "JWT를 지원하지 않습니다.");
 
     private final String code;
     private final HttpStatus httpStatus;
