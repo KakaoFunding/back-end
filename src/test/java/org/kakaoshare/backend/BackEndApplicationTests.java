3afc4da7541c8bba7ee0c13d7b091e249cbe8de9
YeaChan05 <qkenrdl05@gmail.com>
2024-06-06 00:41:01 +0900

    revert

--- src/test/java/org/kakaoshare/backend/BackEndApplicationTests.java
+++ src/test/java/org/kakaoshare/backend/BackEndApplicationTests.java
@@ -2,8 +2,13 @@ package org.kakaoshare.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.flyway.enabled=false",
+        "spring.flyway.baseline-on-migrate=false"
+})
 class BackEndApplicationTests {
 
 	@Test
