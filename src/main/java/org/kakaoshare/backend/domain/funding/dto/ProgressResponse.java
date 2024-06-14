94c116426594dce62c0a47512809bd389685eb43
hwan2-99 <thh1233123@gmail.com>
2024-06-14 16:09:28 +0900

    [feat] 펀딩진행 상황 응답 dto 클래스 빈 상태일경우의 정적팩토리 메소드 구현(KakaoFunding#276)

--- src/main/java/org/kakaoshare/backend/domain/funding/dto/ProgressResponse.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/dto/ProgressResponse.java
@@ -2,6 +2,7 @@ package org.kakaoshare.backend.domain.funding.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.kakaoshare.backend.domain.brand.entity.Brand;
 import org.kakaoshare.backend.domain.funding.entity.Funding;
 
@@ -18,7 +19,7 @@ public class ProgressResponse {
     private static final double PERCENT_MULTIPLIER = 100.0;
 
     private final Long fundingId;
-    private final double progressRate;
+    private final Double progressRate;
     private final Long remainAmount;
     private final Long goalAmount;
     private final Long accumulateAmount;
@@ -46,6 +47,22 @@ public class ProgressResponse {
                 .productPhoto(product.getPhoto())
                 .brandName(brand.getName())
                 .productName(product.getName())
+                .build();
+    }
+
+    public static ProgressResponse empty() {
+        return ProgressResponse.builder()
+                .fundingId(null)
+                .progressRate(null)
+                .remainAmount(null)
+                .goalAmount(null)
+                .accumulateAmount(null)
+                .productId(null)
+                .brandId(null)
+                .brandPhoto(null)
+                .productPhoto(null)
+                .brandName(null)
+                .productName(null)
                 .build();
     }
 }
