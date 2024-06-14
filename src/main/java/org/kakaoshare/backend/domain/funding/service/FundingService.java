f6f6e4ddd79e22e06d63c7c2a67d4765b7a7c4d1
hwan2-99 <thh1233123@gmail.com>
2024-06-14 15:18:11 +0900

    [refactor] 등록된 펀딩아이템이 있을 경우 예외처리 커스템된 클래스로 변경(KakaoFunding#276)

--- src/main/java/org/kakaoshare/backend/domain/funding/service/FundingService.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/service/FundingService.java
@@ -59,7 +59,7 @@ public class FundingService {
         List<FundingResponse> existingFundings = fundingRepository.findFundingListByMemberId(member.getMemberId());
         for (FundingResponse funding : existingFundings) {
             if (PROGRESS_STATUS.equals(funding.getStatus())) {
-                throw new IllegalStateException("There is already an active funding for this product and user.");
+                throw new FundingException(FundingErrorCode.ALREADY_REGISTERED);
             }
         }
 
