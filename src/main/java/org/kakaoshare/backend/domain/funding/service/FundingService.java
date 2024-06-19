3f3e83504a3f4fe183d4769350c1470d131ff0a5
hwan2-99 <thh1233123@gmail.com>
2024-06-19 16:40:18 +0900

    [refactor] 친구의 펀딩아이템 조회 서비스 로직 수정(KakaoFunding#286)

--- src/main/java/org/kakaoshare/backend/domain/funding/service/FundingService.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/service/FundingService.java
@@ -91,8 +91,12 @@ public class FundingService {
     public ProgressResponse getFriendFundingProgress(String providerId, FriendFundingInquiryRequest inquiryRequest) {
         Member self = findMemberByProviderId(providerId);
         Member friend = findMemberByProviderId(inquiryRequest.getFriendProviderId()); //todo 친구 검증 메소드 추가해야함
-        Funding funding = findByIdAndMemberId(inquiryRequest.getFundingId(), friend.getMemberId());
-
+        Funding funding = fundingRepository.findByMemberIdAndStatus(friend.getMemberId(), FundingStatus.PROGRESS)
+                .orElse(null);
+        
+        if (funding == null) {
+            return new ProgressResponse();
+        }
         return getFundingProgress(funding.getFundingId(), friend.getMemberId());
     }
 
