ff949711906d5878ce0bc4f5c49d490cc89ad31f
hwan2-99 <thh1233123@gmail.com>
2024-06-20 23:41:35 +0900

    [feat] 펀딩 목표금액 에러코드 추가(KakaoFunding#292)

--- src/main/java/org/kakaoshare/backend/domain/funding/exception/FundingErrorCode.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/exception/FundingErrorCode.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum FundingErrorCode implements ErrorCode {
     INVALID_ATTRIBUTE_AMOUNT(HttpStatus.BAD_REQUEST, "기여 금액이 잘못되었습니다."),
+    INVALID_GOAL_AMOUNT(HttpStatus.BAD_REQUEST, "목표 금액이 잘못되었습니다."),
+
     NOT_FOUND(HttpStatus.NOT_FOUND, "펀딩 내역을 찾을 수 없습니다."),
     INVALID_STATUS(HttpStatus.BAD_REQUEST,"올바른 펀딩의 상태 값이 아닙니다."),
     ALREADY_REGISTERED(HttpStatus.BAD_REQUEST, "이미 등록된 펀딩 내역이 있습니다.");;
