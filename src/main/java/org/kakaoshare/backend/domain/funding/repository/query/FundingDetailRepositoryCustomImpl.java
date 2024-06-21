ac2bcfd99d071347e00390b9c3b67663062d5a09
kmw2378 <rlarla677@gmail.com>
2024-06-22 00:15:08 +0900

    [refactor]FundingDetailRepositoryCustom 메서드 수정 (#302) ContributedFundingHistoryDto 를 반환하도록 수정

--- src/main/java/org/kakaoshare/backend/domain/funding/repository/query/FundingDetailRepositoryCustomImpl.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/repository/query/FundingDetailRepositoryCustomImpl.java
@@ -4,9 +4,8 @@ import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.kakaoshare.backend.common.vo.date.Date;
+import org.kakaoshare.backend.domain.funding.dto.inquiry.ContributedFundingHistoryDto;
 import org.kakaoshare.backend.domain.funding.dto.inquiry.QContributedFundingHistoryDto;
-import org.kakaoshare.backend.domain.funding.dto.inquiry.response.ContributedFundingHistoryResponse;
-import org.kakaoshare.backend.domain.funding.dto.inquiry.response.QContributedFundingHistoryResponse;
 import org.kakaoshare.backend.domain.member.entity.QMember;
 import org.kakaoshare.backend.domain.product.dto.QProductDto;
 import org.springframework.data.domain.Page;
@@ -26,16 +25,16 @@ public class FundingDetailRepositoryCustomImpl implements FundingDetailRepositor
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Page<ContributedFundingHistoryResponse> findHistoryByCondition(final String providerId, final Date date, final String status, final Pageable pageable) {
+    public Page<ContributedFundingHistoryDto> findHistoryByCondition(final String providerId, final Date date, final String status, final Pageable pageable) {
         final JPAQuery<Long> countQuery = createCountQuery(providerId, date, status, pageable);
-        final JPAQuery<ContributedFundingHistoryResponse> contentQuery = createContentQuery(providerId, date, status, pageable);
+        final JPAQuery<ContributedFundingHistoryDto> contentQuery = createContentQuery(providerId, date, status, pageable);
         return toPage(pageable, contentQuery, countQuery);
     }
 
     @Override
-    public Page<ContributedFundingHistoryResponse> findHistoryByConditionWithoutStatus(final String providerId, final Date date, final Pageable pageable) {
+    public Page<ContributedFundingHistoryDto> findHistoryByConditionWithoutStatus(final String providerId, final Date date, final Pageable pageable) {
         final JPAQuery<Long> countQuery = createCountQuery(providerId, date, pageable);
-        final JPAQuery<ContributedFundingHistoryResponse> contentQuery = createContentQuery(providerId, date, pageable);
+        final JPAQuery<ContributedFundingHistoryDto> contentQuery = createContentQuery(providerId, date, pageable);
         return toPage(pageable, contentQuery, countQuery);
     }
 
@@ -76,22 +75,22 @@ public class FundingDetailRepositoryCustomImpl implements FundingDetailRepositor
                 .limit(pageable.getPageSize());
     }
 
-    private JPAQuery<ContributedFundingHistoryResponse> createContentQuery(final String providerId,
-                                                                           final Date date,
-                                                                           final Pageable pageable) {
+    private JPAQuery<ContributedFundingHistoryDto> createContentQuery(final String providerId,
+                                                                      final Date date,
+                                                                      final Pageable pageable) {
         return createBaseQuery(providerId, date)
-                .select(new QContributedFundingHistoryResponse(getProductDto(), getContributedFundingHistoryDto()))
+                .select(getContributedFundingHistoryDto())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .orderBy(createOrderSpecifiers(fundingDetail, pageable));
     }
 
-    private JPAQuery<ContributedFundingHistoryResponse> createContentQuery(final String providerId,
+    private JPAQuery<ContributedFundingHistoryDto> createContentQuery(final String providerId,
                                                                            final Date date,
                                                                            final String status,
                                                                            final Pageable pageable) {
         return createBaseQuery(providerId, date, status)
-                .select(new QContributedFundingHistoryResponse(getProductDto(), getContributedFundingHistoryDto()))
+                .select(getContributedFundingHistoryDto())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .orderBy(createOrderSpecifiers(fundingDetail, pageable));
