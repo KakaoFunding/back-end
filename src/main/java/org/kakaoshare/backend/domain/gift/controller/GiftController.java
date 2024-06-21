2c0bdf428da63f2125d7ba7581b8b9e11749f391
kmw2378 <rlarla677@gmail.com>
2024-06-22 01:08:28 +0900

    [refactor]giftController.getGiftBox() 메서드 시그니처 수정 (#302)

--- src/main/java/org/kakaoshare/backend/domain/gift/controller/GiftController.java
+++ src/main/java/org/kakaoshare/backend/domain/gift/controller/GiftController.java
@@ -4,16 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.kakaoshare.backend.common.dto.PageResponse;
 import org.kakaoshare.backend.domain.gift.dto.GiftDescriptionResponse;
 import org.kakaoshare.backend.domain.gift.dto.GiftDetailResponse;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.kakaoshare.backend.domain.gift.dto.GiftResponse;
 import org.kakaoshare.backend.domain.gift.entity.GiftStatus;
 import org.kakaoshare.backend.domain.gift.service.GiftService;
 import org.kakaoshare.backend.jwt.util.LoggedInMember;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +37,7 @@ public class GiftController {
     public ResponseEntity<?> getGiftBox(@LoggedInMember final String providerId,
                                         @PageableDefault(size = 20) Pageable pageable,
                                         @RequestParam(name = "status", required = false, defaultValue = "NOT_USED") GiftStatus status) {
-        PageResponse<?> giftSliceResponse = giftService.getMyGiftBox(providerId, pageable, status);
+        PageResponse<?> giftSliceResponse = giftService.getMyGiftBox(providerId, status, pageable);
         return ResponseEntity.ok(giftSliceResponse);
     }
 
