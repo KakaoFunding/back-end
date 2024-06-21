8167da829b366664007109deaf5ae555ba301ab0
hwan2-99 <thh1233123@gmail.com>
2024-06-22 02:22:50 +0900

    [feat] 펀딩예외 메시지 추가 (KakaoFunding#292)

--- src/main/java/org/kakaoshare/backend/domain/funding/exception/FundingErrorCode.java
+++ src/main/java/org/kakaoshare/backend/domain/funding/exception/FundingErrorCode.java
@@ -11,7 +11,8 @@ public enum FundingErrorCode implements ErrorCode {
 
     NOT_FOUND(HttpStatus.NOT_FOUND, "펀딩 내역을 찾을 수 없습니다."),
     INVALID_STATUS(HttpStatus.BAD_REQUEST,"올바른 펀딩의 상태 값이 아닙니다."),
-    ALREADY_REGISTERED(HttpStatus.BAD_REQUEST, "이미 등록된 펀딩 내역이 있습니다.");;
+    GOAL_AMOUNT_EXCEEDS_PRODUCT_PRICE(HttpStatus.BAD_REQUEST, "목표 금액은 상품 금액을 초과할 수 없습니다."),
+    ALREADY_REGISTERED(HttpStatus.BAD_REQUEST, "이미 등록된 펀딩 내역이 있습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
