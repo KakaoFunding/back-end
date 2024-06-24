5133abab544263aa5461a9abdfe5b343227a23b7
hwan2-99 <thh1233123@gmail.com>
2024-06-24 22:20:27 +0900

    [feat] 장바구니 선택 상태 변경 컨트롤러 구현 (KakaoFunding303)

--- src/main/java/org/kakaoshare/backend/domain/cart/controller/CartController.java
+++ src/main/java/org/kakaoshare/backend/domain/cart/controller/CartController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -63,6 +64,12 @@ public class CartController {
     @DeleteMapping()
     public ResponseEntity<CartClearResponse> clearCart(@LoggedInMember String providerId) {
         CartClearResponse response = cartService.clearCartItems(providerId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{cartId}/selection")
+    public ResponseEntity<?> updateCartSelection(@PathVariable Long cartId, @RequestParam boolean isSelected) {
+        CartRegisterResponse response = cartService.updateCartSelection(cartId, isSelected);
         return ResponseEntity.ok(response);
     }
 
