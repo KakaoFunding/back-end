4eca5295a6dead3f893fa3786d5312a31f3b5774
kmw2378 <rlarla677@gmail.com>
2024-07-09 10:41:25 +0900

    [feat]LoggingConfig에 Interceptor 추가 (#322)
    Signed-off-by: kmw2378 <rlarla677@gmail.com>

--- src/main/java/org/kakaoshare/backend/logging/config/LoggingConfig.java
+++ src/main/java/org/kakaoshare/backend/logging/config/LoggingConfig.java
@@ -1,15 +1,32 @@
 package org.kakaoshare.backend.logging.config;
 
+import lombok.RequiredArgsConstructor;
+import org.kakaoshare.backend.logging.interceptor.LoggingInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StopWatch;
 import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class LoggingConfig {
+@RequiredArgsConstructor
+public class LoggingConfig implements WebMvcConfigurer {
+    private static final String METRIC_URL_PREFIX = "/actuator";
+    private static final String FAVICON_URL = "/favicon.ico";
+
+    private final LoggingInterceptor loggingInterceptor;
+
     @Bean
     @RequestScope
     public StopWatch stopWatch() {
         return new StopWatch();
+    }
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(METRIC_URL_PREFIX, FAVICON_URL);
     }
 }
