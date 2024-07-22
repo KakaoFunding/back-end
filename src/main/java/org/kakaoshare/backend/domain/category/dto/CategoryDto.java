8659855a7b984438787242e0ed60cfe32fe985b5
YeaChan05 <qkenrdl05@gmail.com>
2024-07-22 15:01:50 +0900

    [fix] 캐싱 적용 및 쿼리 변경에 따른 생성자 수정 (#329)
    1. 캐싱 적용에 따라 기본 생성자 생성
    2. 쿼리 변경에 따라 기존에 사용하던 로직들 제거

--- src/main/java/org/kakaoshare/backend/domain/category/dto/CategoryDto.java
+++ src/main/java/org/kakaoshare/backend/domain/category/dto/CategoryDto.java
@@ -6,35 +6,31 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.querydsl.core.annotations.QueryProjection;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import lombok.Builder;
 import lombok.Getter;
-import org.kakaoshare.backend.domain.category.entity.Category;
+import lombok.NoArgsConstructor;
 
 @Getter
 @JsonSerialize
+@NoArgsConstructor
 public class CategoryDto {
     public static final long PARENT_ID = -1L;
-    private final Long categoryId;
-    private final String categoryName;
+    private Long categoryId;
+    private String categoryName;
     
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private final Long parentId;
+    private Long parentId;
     
     @JsonInclude(Include.NON_EMPTY)
     private final List<CategoryDto> subCategories = new ArrayList<>();
 
     private final TabType defaultTab=TabType.BRAND;
-    private int level;
 
-    @Builder
     @QueryProjection
     public CategoryDto(Long categoryId, String categoryName, Long parentId, List<CategoryDto> subCategories) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
         this.parentId = parentId;
         this.subCategories.addAll(subCategories);
-        this.level = 1;
     }
 
     @QueryProjection
@@ -42,31 +38,6 @@ public class CategoryDto {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
         this.parentId = parentId;
-        this.level = 2;
-    }
-
-    public static CategoryDto from(final Category category) {
-        CategoryDto categoryDto = CategoryDto
-                .getCategoryDtoBuilder(category)
-                .build();
-
-        giveLevelAndSubCategories(categoryDto, category);
-
-        return categoryDto;
-    }
-
-    private static void giveLevelAndSubCategories(CategoryDto dto, Category category) {
-        dto.level=2;
-        if (!category.isChildEmpty()) {
-            dto.level = 1;
-            dto.getSubCategories().addAll(getChildrenDtos(category));
-        }
-    }
-    
-    private static List<CategoryDto> getChildrenDtos(final Category category) {
-        return category.getChildren().stream()
-                .map(CategoryDto::from)
-                .toList();
     }
     
     @Override
@@ -80,17 +51,5 @@ public class CategoryDto {
                         .toList() +
                 ", defaultTab=" + defaultTab +
                 '}';
-    }
-
-    private static CategoryDtoBuilder getCategoryDtoBuilder(final Category category) {
-        Long parentId = Optional
-                .ofNullable(category.getParent())
-                .map(Category::getCategoryId)
-                .orElse(null);
-
-        return CategoryDto.builder()
-                .categoryId(category.getCategoryId())
-                .categoryName(category.getName())
-                .parentId(parentId);
     }
 }
