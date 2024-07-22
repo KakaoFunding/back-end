e62503d77e01d20c691c3329daf5aeaf070675c3
YeaChan05 <qkenrdl05@gmail.com>
2024-07-22 14:43:43 +0900

    [refactor] 생성자 단순화 (#329)
    사용하지 않는 생성자를 제거 후 클래스에 builder 적용

--- src/main/java/org/kakaoshare/backend/domain/category/dto/CategoryHeaderResponse.java
+++ src/main/java/org/kakaoshare/backend/domain/category/dto/CategoryHeaderResponse.java
@@ -1,5 +1,6 @@
 package org.kakaoshare.backend.domain.category.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,20 +8,15 @@ import java.util.List;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class CategoryHeaderResponse {
     private Long brandCount;
     private Long productCount;
     private Long totalCount;
     private List<SimpleCategoryDto> simpleCategoryDtos;
-    @Builder
-    private CategoryHeaderResponse(final Long brandCount, final Long productCount, Long totalCount, final List<SimpleCategoryDto> simpleCategoryDtos) {
-        this.brandCount = brandCount;
-        this.productCount = productCount;
-        this.totalCount = totalCount;
-        this.simpleCategoryDtos = simpleCategoryDtos;
-    }
-    
+
     public static CategoryHeaderResponse of(final Long brandCount, final Long productCount, final List<SimpleCategoryDto> simpleCategoryDtos){
         return CategoryHeaderResponse
                 .builder()
